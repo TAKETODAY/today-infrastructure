59566e2afb1da3e523158ffdb8b432d402712282
TODAY <2228592280@qq.com>
2019-07-26 00:06:55 +0800

    add Lifecycle test code

---
+++ src/test/java/test/context/LifecycleBean.java
@@ -0,0 +1,113 @@
+/**
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2019 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *   
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+package test.context;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.junit.Test;
+
+import cn.taketoday.context.ApplicationContext;
+import cn.taketoday.context.StandardApplicationContext;
+import cn.taketoday.context.annotation.Singleton;
+import cn.taketoday.context.aware.ApplicationContextAware;
+import cn.taketoday.context.aware.BeanFactoryAware;
+import cn.taketoday.context.aware.BeanNameAware;
+import cn.taketoday.context.aware.EnvironmentAware;
+import cn.taketoday.context.bean.BeanDefinition;
+import cn.taketoday.context.env.Environment;
+import cn.taketoday.context.factory.BeanFactory;
+import cn.taketoday.context.factory.DisposableBean;
+import cn.taketoday.context.factory.InitializingBean;
+import cn.taketoday.context.utils.ClassUtils;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author TODAY <br>
+ *         2019-07-25 22:44
+ */
+@Slf4j
+@Singleton
+public class LifecycleBean //
+        implements DisposableBean, BeanNameAware,
+        InitializingBean, BeanFactoryAware,
+        EnvironmentAware, ApplicationContextAware {
+
+    @Override
+    public void setBeanName(String name) {
+        log.info("setBeanName: {}", name);
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        log.info("setEnvironment: {}", environment);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        log.info("setBeanFactory: {}", beanFactory);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        log.info("setApplicationContext: {}", applicationContext);
+    }
+
+    @PostConstruct
+    public void initData() {
+        log.info("@PostConstruct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("afterPropertiesSet");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("preDestroy");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        log.info("destroy");
+    }
+
+    @Test
+    public void testLifecycle() {
+
+        final Set<Class<?>> beans = new HashSet<>();
+        beans.add(LifecycleBean.class);
+        ClassUtils.setClassCache(beans);
+        try (final ApplicationContext applicationContext = new StandardApplicationContext("info.properties")) {
+
+            applicationContext.loadContext(beans);
+
+            Map<String, BeanDefinition> beanDefinitionsMap = applicationContext.getEnvironment().getBeanDefinitionRegistry().getBeanDefinitions();
+
+            System.out.println(beanDefinitionsMap);
+        }
+    }
+
+}
