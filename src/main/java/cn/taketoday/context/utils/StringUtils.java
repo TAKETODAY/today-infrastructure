51ebd7e46ca3bfc71a0b76a6fbf8c70b48357012
TAKETODAY <2228592280@qq.com>
2019-11-20 21:26:58 +0800

    :art: 格式化代码

--- src/main/java/cn/taketoday/context/utils/StringUtils.java
+++ src/main/java/cn/taketoday/context/utils/StringUtils.java
@@ -149,14 +149,18 @@ public abstract class StringUtils {
                         int pos = 0;
                         while (((i + 2) < numChars) && (c == '%')) {
                             int v = Integer.parseInt(s.substring(i + 1, i + 3), 16);
-                            if (v < 0) throw new IllegalArgumentException("Illegal hex characters in escape (%) pattern - negative value");
+                            if (v < 0) {
+                                throw new IllegalArgumentException("Illegal hex characters in escape (%) pattern - negative value");
+                            }
                             bytes[pos++] = (byte) v;
                             i += 3;
                             if (i < numChars) c = s.charAt(i);
                         }
                         // A trailing, incomplete byte encoding such as
                         // "%x" will cause an exception to be thrown
-                        if ((i < numChars) && (c == '%')) throw new IllegalArgumentException("Incomplete trailing escape (%) pattern");
+                        if ((i < numChars) && (c == '%')) {
+                            throw new IllegalArgumentException("Incomplete trailing escape (%) pattern");
+                        }
                         sb.append(new String(bytes, 0, pos, charset));
                     }
                     catch (NumberFormatException e) {
