e2077ec5553b1901fac848a8e9344ba27d67ab77
TODAY <2228592280@qq.com>
2020-07-29 16:26:18 +0800

    :zap: 优化AutoProxyCreator

--- src/main/java/cn/taketoday/aop/proxy/AutoProxyCreator.java
+++ src/main/java/cn/taketoday/aop/proxy/AutoProxyCreator.java
@@ -1,7 +1,7 @@
 /**
  * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
  * Copyright © TODAY & 2017 - 2020 All Rights Reserved.
- * 
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,30 +21,30 @@ package cn.taketoday.aop.proxy;
 
 import cn.taketoday.context.ApplicationContext;
 import cn.taketoday.context.Ordered;
+import cn.taketoday.context.OrderedSupport;
 import cn.taketoday.context.annotation.Autowired;
-import cn.taketoday.context.annotation.Order;
 import cn.taketoday.context.factory.BeanDefinition;
 import cn.taketoday.context.factory.BeanPostProcessor;
 
 /**
  * Auto create proxy
- * 
+ *
  * @author TODAY <br>
  *         2018-11-10 13:13
  */
-@Order(Ordered.LOWEST_PRECEDENCE - Ordered.HIGHEST_PRECEDENCE)
-public class AutoProxyCreator implements BeanPostProcessor {
+public class AutoProxyCreator extends OrderedSupport implements BeanPostProcessor {
 
-    private final ApplicationContext applicationContext;
+    private final ApplicationContext context;
 
     @Autowired
-    public AutoProxyCreator(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+    public AutoProxyCreator(ApplicationContext context) {
+        super(Ordered.LOWEST_PRECEDENCE - Ordered.HIGHEST_PRECEDENCE);
+        this.context = context;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, BeanDefinition beanDefinition) throws Exception {
-        return new DefaultProxyFactory(new TargetSource(bean, bean.getClass()), applicationContext).getProxy();
+        return new DefaultProxyFactory(new TargetSource(bean, bean.getClass()), context).getProxy();
     }
 
 }
