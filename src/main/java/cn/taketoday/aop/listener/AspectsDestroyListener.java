477bfe108a600774e24ecdd8bfd2f16d0be69bf7
TODAY <2228592280@qq.com>
2020-08-10 10:51:31 +0800

    Update AspectsDestroyListener.java
    :zap: 优化AspectsDestroyListener排序问题

--- src/main/java/cn/taketoday/aop/listener/AspectsDestroyListener.java
+++ src/main/java/cn/taketoday/aop/listener/AspectsDestroyListener.java
@@ -3,7 +3,7 @@
  * Copyright © TODAY & 2017 - 2020 All Rights Reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,7 +13,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *   
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/]
  */
@@ -21,7 +21,7 @@ package cn.taketoday.aop.listener;
 
 import cn.taketoday.aop.advice.AspectsRegistry;
 import cn.taketoday.context.Ordered;
-import cn.taketoday.context.annotation.Order;
+import cn.taketoday.context.OrderedSupport;
 import cn.taketoday.context.event.ContextCloseEvent;
 import cn.taketoday.context.listener.ApplicationListener;
 import cn.taketoday.context.logger.LoggerFactory;
@@ -30,8 +30,12 @@ import cn.taketoday.context.logger.LoggerFactory;
  * @author TODAY <br>
  *         2019-02-14 20:48
  */
-@Order(Ordered.LOWEST_PRECEDENCE)
-public class AspectsDestroyListener implements ApplicationListener<ContextCloseEvent> {
+public class AspectsDestroyListener
+        extends OrderedSupport implements ApplicationListener<ContextCloseEvent> {
+
+    public AspectsDestroyListener() {
+        super(Ordered.LOWEST_PRECEDENCE);
+    }
 
     @Override
     public void onApplicationEvent(ContextCloseEvent event) {
