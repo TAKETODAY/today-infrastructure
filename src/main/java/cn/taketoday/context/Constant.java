fe2933ffa4f3b75badc26f9a655cd5e3df06ef24
TODAY <2228592280@qq.com>
2020-09-16 21:49:21 +0800

    :art: Constant

--- src/main/java/cn/taketoday/context/Constant.java
+++ src/main/java/cn/taketoday/context/Constant.java
@@ -98,82 +98,82 @@ public interface Constant extends Opcodes, Serializable {
     int SWITCH_STYLE_HASHONLY = 2;
 
     //@off
-	/** Bytes per Kilobyte.*/
-	long 	BYTES_PER_KB 			= 1024;
-	/** Bytes per Megabyte. */
-	long 	BYTES_PER_MB 			= BYTES_PER_KB * 1024;
-	/** Bytes per Gigabyte.*/
-	long 	BYTES_PER_GB 			= BYTES_PER_MB * 1024;
-	/** Bytes per Terabyte.*/
-	long 	BYTES_PER_TB 			= BYTES_PER_GB * 1024;
+    /** Bytes per Kilobyte.*/
+    long 	BYTES_PER_KB 			= 1024;
+    /** Bytes per Megabyte. */
+    long 	BYTES_PER_MB 			= BYTES_PER_KB * 1024;
+    /** Bytes per Gigabyte.*/
+    long 	BYTES_PER_GB 			= BYTES_PER_MB * 1024;
+    /** Bytes per Terabyte.*/
+    long 	BYTES_PER_TB 			= BYTES_PER_GB * 1024;
 
-	//@since 2.1.6
-	String 	ENABLE_LAZY_LOADING 	= "enable.lazy.loading";
-	String	ENABLE_FULL_PROTOTYPE	= "enable.full.prototype";
-	String	ENABLE_FULL_LIFECYCLE	= "enable.full.lifecycle";
-	String  EMPTY_STRING_ARRAY[]	= new String[0];
-	String  CONSTRUCTOR_NAME 		= "<init>";
-	String  STATIC_CLASS_INIT 		= "<clinit>";
-	/** Suffix for array class names: {@code "[]"}. */
-	String  ARRAY_SUFFIX 			= "[]";
-	/** Prefix for internal array class names: {@code "["}. */
-	String  INTERNAL_ARRAY_PREFIX 	= "[";
-	/** Prefix for internal non-primitive array class names: {@code "[L"}. */
-	String  NON_PRIMITIVE_ARRAY_PREFIX = "[L";
-	/** The package separator character: {@code '.'}. */
-	char 	PACKAGE_SEPARATOR 		= '.';
-	/** The path separator character: {@code '/'}. */
-	char 	PATH_SEPARATOR 			= '/';
-	char 	WINDOWS_PATH_SEPARATOR 	= '\\';
-	char 	INNER_CLASS_SEPARATOR 	= '$';
-	String	ENV						= "env";
-	String 	EL_PREFIX 				= "${";
-	
-	String 	DEFAULT_DATE_FORMAT		= "yyyy-MM-dd HH:mm:ss.SSS";
+    //@since 2.1.6
+    String 	ENABLE_LAZY_LOADING 	= "enable.lazy.loading";
+    String	ENABLE_FULL_PROTOTYPE	= "enable.full.prototype";
+    String	ENABLE_FULL_LIFECYCLE	= "enable.full.lifecycle";
+    String[]  EMPTY_STRING_ARRAY	= new String[0];
+    String  CONSTRUCTOR_NAME 		= "<init>";
+    String  STATIC_CLASS_INIT 		= "<clinit>";
+    /** Suffix for array class names: {@code "[]"}. */
+    String  ARRAY_SUFFIX 			= "[]";
+    /** Prefix for internal array class names: {@code "["}. */
+    String  INTERNAL_ARRAY_PREFIX 	= "[";
+    /** Prefix for internal non-primitive array class names: {@code "[L"}. */
+    String  NON_PRIMITIVE_ARRAY_PREFIX = "[L";
+    /** The package separator character: {@code '.'}. */
+    char 	PACKAGE_SEPARATOR 		= '.';
+    /** The path separator character: {@code '/'}. */
+    char 	PATH_SEPARATOR 			= '/';
+    char 	WINDOWS_PATH_SEPARATOR 	= '\\';
+    char 	INNER_CLASS_SEPARATOR 	= '$';
+    String	ENV						= "env";
+    String 	EL_PREFIX 				= "${";
 
-	/** The CGLIB class separator: {@code "$$"}. */
-	String 	CGLIB_CLASS_SEPARATOR 	= "$$";
-	char 	CGLIB_CHAR_SEPARATOR 	= INNER_CLASS_SEPARATOR;
+    String 	DEFAULT_DATE_FORMAT		= "yyyy-MM-dd HH:mm:ss.SSS";
 
-	String 	PROTOCOL_JAR 			= "jar";
-	String 	PROTOCOL_FILE 			= "file";
-	String 	JAR_ENTRY_URL_PREFIX 	= "jar:file:";
-	String 	JAR_SEPARATOR 			= "!/";
-	int[] 	EMPTY_INT_ARRAY 		= new int[0];
-	
+    /** The CGLIB class separator: {@code "$$"}. */
+    String 	CGLIB_CLASS_SEPARATOR 	= "$$";
+    char 	CGLIB_CHAR_SEPARATOR 	= INNER_CLASS_SEPARATOR;
 
-	String	KEY_USE_SIMPLE_NAME		= "context.name.simple";
-	String 	KEY_ACTIVE_PROFILES 	= "context.active.profiles";
-	
-	String 	DEFAULT_ENCODING 		= "UTF-8";
-	Charset DEFAULT_CHARSET 		= StandardCharsets.UTF_8;
+    String 	PROTOCOL_JAR 			= "jar";
+    String 	PROTOCOL_FILE 			= "file";
+    String 	JAR_ENTRY_URL_PREFIX 	= "jar:file:";
+    String 	JAR_SEPARATOR 			= "!/";
+    int[] 	EMPTY_INT_ARRAY 		= new int[0];
 
-	String 	GET_BEAN 				= "getBean";
-	String 	ON_APPLICATION_EVENT 	= "onApplicationEvent";
-	String 	PROPERTIES_SUFFIX 		= ".properties";
-	String 	PLACE_HOLDER_PREFIX 	= "#{";
-	char 	PLACE_HOLDER_SUFFIX 	= '}';
-	String 	SPLIT_REGEXP 			= "[;|,]";
 
-	String	BLANK					= "";
-	String	CLASS_FILE_SUFFIX		= ".class";
-	String 	SCOPE 					= "scope";
-	String 	VALUE 					= "value";
-	String 	EQUALS 					= "equals";
-	String 	HASH_CODE 				= "hashCode";
-	String 	TO_STRING 				= "toString";
-	String 	ANNOTATION_TYPE 		= "annotationType";
-	String	DEFAULT					= "default";
-	
-	/**********************************************
-	 * @since 2.1.2
-	 */
-	String 	CLASS_PATH_PREFIX 		= "classpath:";
-	String	INIT_METHODS			    = "initMethods";
-	String	DESTROY_METHODS			  = "destroyMethods";
-	String 	TYPE 					        = "type";
-	/**
-	 **********************************************/ //@on
+    String	KEY_USE_SIMPLE_NAME		= "context.name.simple";
+    String 	KEY_ACTIVE_PROFILES 	= "context.active.profiles";
+
+    String 	DEFAULT_ENCODING 		= "UTF-8";
+    Charset DEFAULT_CHARSET 		= StandardCharsets.UTF_8;
+
+    String 	GET_BEAN 				= "getBean";
+    String 	ON_APPLICATION_EVENT 	= "onApplicationEvent";
+    String 	PROPERTIES_SUFFIX 		= ".properties";
+    String 	PLACE_HOLDER_PREFIX 	= "#{";
+    char 	PLACE_HOLDER_SUFFIX 	= '}';
+    String 	SPLIT_REGEXP 			= "[;|,]";
+
+    String	BLANK					= "";
+    String	CLASS_FILE_SUFFIX		= ".class";
+    String 	SCOPE 					= "scope";
+    String 	VALUE 					= "value";
+    String 	EQUALS 					= "equals";
+    String 	HASH_CODE 				= "hashCode";
+    String 	TO_STRING 				= "toString";
+    String 	ANNOTATION_TYPE 		= "annotationType";
+    String	DEFAULT					= "default";
+
+    /**********************************************
+     * @since 2.1.2
+     */
+    String 	CLASS_PATH_PREFIX 		= "classpath:";
+    String	INIT_METHODS			    = "initMethods";
+    String	DESTROY_METHODS			  = "destroyMethods";
+    String 	TYPE 					        = "type";
+    /**
+     **********************************************/ //@on
 
     /**
      * @since 2.1.7
