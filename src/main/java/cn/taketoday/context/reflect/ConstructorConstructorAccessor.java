726df43ea8e4f385cb3896fc5d9c336e53e9a283
TODAY <2228592280@qq.com>
2020-09-20 23:42:51 +0800

    :bug: 修复private调用错误

---
+++ src/main/java/cn/taketoday/context/reflect/ConstructorConstructorAccessor.java
@@ -0,0 +1,43 @@
+/*
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2020 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.context.reflect;
+
+import java.lang.reflect.Constructor;
+
+import cn.taketoday.context.utils.ReflectionUtils;
+
+/**
+ * @author TODAY
+ * @date 2020/9/20 21:55
+ */
+public class ConstructorConstructorAccessor implements ConstructorAccessor {
+
+    private final Constructor<?> constructor;
+
+    public ConstructorConstructorAccessor(Constructor<?> constructor) {
+        this.constructor = constructor;
+    }
+
+    @Override
+    public Object newInstance(final Object[] args) {
+        return ReflectionUtils.invokeConstructor(constructor, args);
+    }
+}
---
+++ src/main/java/cn/taketoday/context/reflect/MethodMethodAccessor.java
@@ -0,0 +1,44 @@
+/**
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2020 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+package cn.taketoday.context.reflect;
+
+import java.lang.reflect.Method;
+
+import cn.taketoday.context.utils.Assert;
+import cn.taketoday.context.utils.ReflectionUtils;
+
+/**
+ * @author TODAY
+ * @date 2020/9/20 21:49
+ */
+public class MethodMethodAccessor extends MethodInvoker implements MethodAccessor {
+
+    private final Method method;
+
+    public MethodMethodAccessor(final Method method) {
+        Assert.notNull(method, "method must not be null");
+        this.method = method;
+    }
+
+    @Override
+    public Object invoke(final Object obj, final Object[] args) {
+        return ReflectionUtils.invokeMethod(method, obj, args);
+    }
+}
