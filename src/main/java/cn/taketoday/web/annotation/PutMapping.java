95d2e6f69997abaa90f2edc7e42f7f68d525c859
TODAY <2228592280@qq.com>
2020-12-08 21:52:17 +0800

    :zap: 新建注解

---
+++ src/main/java/cn/taketoday/web/annotation/PutMapping.java
@@ -0,0 +1,26 @@
+package cn.taketoday.web.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import cn.taketoday.context.Constant;
+import cn.taketoday.web.RequestMethod;
+
+/**
+ * @author TODAY
+ * @date 2020/12/8 21:47
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@ActionMapping(method = RequestMethod.PUT)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface PutMapping {
+
+  /** urls */
+  String[] value() default Constant.BLANK;
+
+  /** Exclude url on class */
+  boolean exclude() default false;
+
+}
