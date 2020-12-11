274d82407ea55f51a3f1affa8f7b129590b424b7
TODAY <2228592280@qq.com>
2020-12-11 23:40:18 +0800

    :zap: CacheableMappedHandlerRegistry

---
+++ src/main/java/cn/taketoday/web/registry/CacheableMappedHandlerRegistry.java
@@ -0,0 +1,63 @@
+package cn.taketoday.web.registry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.taketoday.cache.Cache;
+import cn.taketoday.cache.ConcurrentMapCache;
+import cn.taketoday.context.EmptyObject;
+
+/**
+ * @author TODAY
+ * @date 2020/12/11 23:32
+ * @since 3.0
+ */
+public class CacheableMappedHandlerRegistry extends MappedHandlerRegistry {
+
+  private Cache patternMatchingCache;
+
+  public CacheableMappedHandlerRegistry() {
+    super(new HashMap<>());
+  }
+
+  public CacheableMappedHandlerRegistry(int initialCapacity) {
+    super(new HashMap<>(initialCapacity));
+  }
+
+  public CacheableMappedHandlerRegistry(Map<String, Object> handlers) {
+    super(handlers, LOWEST_PRECEDENCE);
+  }
+
+  public CacheableMappedHandlerRegistry(Map<String, Object> handlers, int order) {
+    super(handlers, order);
+  }
+
+  @Override
+  protected Object lookupPatternHandler(String handlerKey) {
+    final Cache patternMatchingCache = getPatternMatchingCache();
+    Object handler = patternMatchingCache.get(handlerKey, false);
+    if (handler == null) {
+      handler = super.lookupPatternHandler(handlerKey);
+      patternMatchingCache.put(handlerKey, handler == null ? EmptyObject.INSTANCE : handler);
+    }
+    else if (handler == EmptyObject.INSTANCE) {
+      return null;
+    }
+    return handler;
+  }
+
+  public final Cache getPatternMatchingCache() {
+    if (patternMatchingCache == null) {
+      patternMatchingCache = createPatternMatchingCache();
+    }
+    return patternMatchingCache;
+  }
+
+  protected ConcurrentMapCache createPatternMatchingCache() {
+    return new ConcurrentMapCache("pattern-matching", 128);
+  }
+
+  public void setPatternMatchingCache(final Cache patternMatchingCache) {
+    this.patternMatchingCache = patternMatchingCache;
+  }
+}
