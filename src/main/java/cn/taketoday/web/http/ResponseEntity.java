04f3edbf7bd4e963fda6fdea944b91919851fbb6
TODAY <2228592280@qq.com>
2021-01-28 21:08:56 +0800

    :art: copyright

--- src/main/java/cn/taketoday/web/http/ResponseEntity.java
+++ src/main/java/cn/taketoday/web/http/ResponseEntity.java
@@ -1,3 +1,22 @@
+/**
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
 package cn.taketoday.web.http;
 
 import java.net.URI;
@@ -16,7 +35,6 @@ import cn.taketoday.context.utils.MultiValueMap;
 import cn.taketoday.web.RequestMethod;
 import cn.taketoday.web.resource.CacheControl;
 import jodd.net.HttpMethod;
-import sun.misc.Request;
 
 /**
  * Extension of {@link HttpEntity} that adds a {@link HttpStatus} status code.
