a265ef2cd991e8b2ccb4f67a12f6b7f86da4fc20
TODAY <2228592280@qq.com>
2021-02-04 22:49:03 +0800

    :zap: 优化异常结构

--- src/main/java/cn/taketoday/web/exception/UnauthorizedException.java
+++ src/main/java/cn/taketoday/web/exception/UnauthorizedException.java
@@ -47,8 +47,4 @@ public class UnauthorizedException extends WebNestedRuntimeException {
     super(cause);
   }
 
-  protected UnauthorizedException(String message, Throwable cause, //
-                                  boolean enableSuppression, boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
-  }
 }
