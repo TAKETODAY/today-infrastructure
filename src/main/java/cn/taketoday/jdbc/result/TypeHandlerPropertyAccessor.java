ceeeb20f7a0aef6b632d8305c8b944a8d3756fa4
TODAY <2228592280@qq.com>
2021-02-12 11:58:35 +0800

    :zap: 优化TypeHandlerPropertyAccessor 避免底层驱动数据转换出错

--- src/main/java/cn/taketoday/jdbc/result/TypeHandlerPropertyAccessor.java
+++ src/main/java/cn/taketoday/jdbc/result/TypeHandlerPropertyAccessor.java
@@ -3,7 +3,7 @@ package cn.taketoday.jdbc.result;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import cn.taketoday.context.reflect.PropertyAccessor;
+import cn.taketoday.context.factory.BeanProperty;
 import cn.taketoday.jdbc.type.TypeHandler;
 
 /**
@@ -13,21 +13,45 @@ import cn.taketoday.jdbc.type.TypeHandler;
 public class TypeHandlerPropertyAccessor implements JdbcPropertyAccessor {
 
   final TypeHandler<?> typeHandler;
-  final PropertyAccessor accessor;
+  final BeanProperty beanProperty;
 
-  public TypeHandlerPropertyAccessor(TypeHandler<?> typeHandler, PropertyAccessor accessor) {
+  public TypeHandlerPropertyAccessor(TypeHandler<?> typeHandler, BeanProperty beanProperty) {
     this.typeHandler = typeHandler;
-    this.accessor = accessor;
+    this.beanProperty = beanProperty;
   }
 
   @Override
   public Object get(Object obj) {
-    return accessor.get(obj);
+    return beanProperty.getValue(obj);
   }
 
   @Override
   public void set(Object obj, ResultSet resultSet, int columnIndex) throws SQLException {
-    final Object result = typeHandler.getResult(resultSet, columnIndex);
-    accessor.set(obj, result);
+    final Object result = getResult(resultSet, columnIndex);
+    beanProperty.setValue(obj, result);
+  }
+
+  /**
+   * Get result from {@link ResultSet}.
+   * <p>
+   * Obtain from {@link TypeHandler}, if it fails, use the default acquisition method
+   * </p>
+   *
+   * @param resultSet
+   *         Target result set
+   *
+   * @return data object
+   *
+   * @throws SQLException
+   *         If {@link ResultSet#getObject(int)} failed
+   */
+  Object getResult(ResultSet resultSet, int columnIndex) throws SQLException {
+    try {
+      return typeHandler.getResult(resultSet, columnIndex);
+    }
+    catch (SQLException e) {
+      // maybe data conversion error
+      return resultSet.getObject(columnIndex);
+    }
   }
 }
