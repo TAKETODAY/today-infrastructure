ea0787a038a3adcbd6b0c2fd3c44186dad050ff1
TODAY <2228592280@qq.com>
2021-03-19 21:12:50 +0800

    :zap: 暂时ConversionService

---
+++ src/main/java/cn/taketoday/context/conversion/ConversionService.java
@@ -0,0 +1,56 @@
+/*
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.context.conversion;
+
+/**
+ * Conversion Service
+ *
+ * @author TODAY 2021/3/19 20:59
+ * @since 3.0
+ */
+public interface ConversionService {
+
+  /**
+   * whether this {@link ConversionService} supports to convert source object to
+   * target class object
+   *
+   * @param targetClass
+   *         target class
+   * @param source
+   *         source object
+   *
+   * @return whether this {@link ConversionService} supports to convert source object
+   * to target class object
+   */
+  boolean canConvert(Object source, Class<?> targetClass);
+
+  /**
+   * Convert source to target type
+   *
+   * @param source
+   *         value
+   * @param targetClass
+   *         targetClass
+   *
+   * @return converted object
+   */
+  Object convert(Object source, Class<?> targetClass);
+}
