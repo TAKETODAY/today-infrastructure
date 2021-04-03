1898c42dc31fb0023a53ad8f5fa52f8e48d45b5f
TODAY <2228592280@qq.com>
2021-04-03 13:34:54 +0800

    :zap: 准备适配 WebSocket

---
+++ src/main/java/cn/taketoday/web/socket/BinaryMessage.java
@@ -0,0 +1,29 @@
+/*
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.web.socket;
+
+/**
+ * @author TODAY 2021/4/3 11:57
+ * @since 3.0
+ */
+public class BinaryMessage implements Message {
+
+}
