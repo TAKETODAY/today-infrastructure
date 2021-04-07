95a2b7a0f8625faaea07dbe2a78c03c8f1df679c
TODAY <2228592280@qq.com>
2021-04-07 19:11:01 +0800

    :zap: CompositeValidator to WebValidator

--- src/main/java/cn/taketoday/web/config/WebMvcConfiguration.java
+++ src/main/java/cn/taketoday/web/config/WebMvcConfiguration.java
@@ -33,7 +33,7 @@ import cn.taketoday.web.registry.HandlerRegistry;
 import cn.taketoday.web.registry.ResourceHandlerRegistry;
 import cn.taketoday.web.registry.ViewControllerHandlerRegistry;
 import cn.taketoday.web.resolver.ParameterResolver;
-import cn.taketoday.web.validation.CompositeValidator;
+import cn.taketoday.web.validation.WebValidator;
 import cn.taketoday.web.view.ResultHandler;
 import cn.taketoday.web.view.template.AbstractTemplateViewResolver;
 import cn.taketoday.web.view.template.TemplateViewResolver;
@@ -161,13 +161,13 @@ public interface WebMvcConfiguration {
   default void configureExceptionHandlers(List<HandlerExceptionHandler> handlers) {}
 
   /**
-   * Configure {@link CompositeValidator}
+   * Configure {@link WebValidator}
    *
    * @param validator
    *         list of validators
    *
    * @since 3.0
    */
-  default void configureValidators(CompositeValidator validator) {}
+  default void configureValidators(WebValidator validator) {}
 
 }
