1edc953dfaf40d0c3784a22ac229b587ff18aebc
TODAY <2228592280@qq.com>
2021-04-13 15:21:30 +0800

    :zap: StringUtils

--- src/main/java/cn/taketoday/context/utils/StringUtils.java
+++ src/main/java/cn/taketoday/context/utils/StringUtils.java
@@ -28,12 +28,9 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Random;
 import java.util.StringTokenizer;
 import java.util.TimeZone;
@@ -306,71 +303,6 @@ public abstract class StringUtils {
       needToChange = true;
     }
     return (needToChange ? out.toString() : s);
-  }
-
-  /**
-   * Parse Parameters
-   *
-   * @param s
-   *         Input {@link String}
-   *
-   * @return Map of list parameters
-   */
-  public static Map<String, List<String>> parseParameters(final String s) {
-
-    if (isEmpty(s)) {
-      return Collections.emptyMap();
-    }
-
-    final Map<String, List<String>> params = new HashMap<>();
-    int nameStart = 0;
-    int valueStart = -1;
-    int i;
-    final int len = s.length();
-    loop:
-    for (i = 0; i < len; i++) {
-      switch (s.charAt(i)) {
-        case '=':
-          if (nameStart == i) {
-            nameStart = i + 1;
-          }
-          else if (valueStart < nameStart) {
-            valueStart = i + 1;
-          }
-          break;
-        case '&':
-        case ';':
-          addParam(s, nameStart, valueStart, i, params);
-          nameStart = i + 1;
-          break;
-        case '#':
-          break loop;
-        default:
-          // continue
-      }
-    }
-    addParam(s, nameStart, valueStart, i, params);
-    return params;
-  }
-
-  private static void addParam(String s,
-                               int nameStart,
-                               int valueStart,
-                               int valueEnd,
-                               Map<String, List<String>> params) {
-
-    if (nameStart < valueEnd) {
-      if (valueStart <= nameStart) {
-        valueStart = valueEnd + 1;
-      }
-      String name = s.substring(nameStart, valueStart - 1); //FIXME
-      String value = s.substring(valueStart, valueEnd);
-      List<String> values = params.get(name);
-      if (values == null) {
-        params.put(name, values = new ArrayList<>(2));
-      }
-      values.add(value);
-    }
   }
 
   /**
