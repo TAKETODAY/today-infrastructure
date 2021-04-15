a81003ca80409efa3bfaffd307f6b84d59fb09d8
TODAY <2228592280@qq.com>
2021-04-15 13:46:26 +0800

    :zap: 修复 tokenizeToStringArray

--- src/main/java/cn/taketoday/context/utils/StringUtils.java
+++ src/main/java/cn/taketoday/context/utils/StringUtils.java
@@ -423,39 +423,63 @@ public abstract class StringUtils {
   }
 
   /**
-   * Use StringTokenizer to split string to string array
+   * Tokenize the given {@code String} into a {@code String} array via a
+   * {@link StringTokenizer}.
+   * <p>Trims tokens and omits empty tokens.
+   * <p>The given {@code delimiters} string can consist of any number of
+   * delimiter characters. Each of those characters can be used to separate
+   * tokens. A delimiter is always a single character; for multi-character
+   * delimiters, consider using {@link #delimitedListToStringArray}.
    *
    * @param str
-   *         Input string
-   * @param delimiter
-   *         Input delimiter
+   *         the {@code String} to tokenize (potentially {@code null} or empty)
+   * @param delimiters
+   *         the delimiter characters, assembled as a {@code String}
+   *         (each of the characters is individually considered as a delimiter)
    *
-   * @return Returns the splitted string array
+   * @return an array of the tokens
+   *
+   * @see java.util.StringTokenizer
+   * @see String#trim()
+   * @see #delimitedListToStringArray
    */
-  public static String[] tokenizeToStringArray(final String str, final String delimiter) {
-
-    if (str == null) {
-      return Constant.EMPTY_STRING_ARRAY;
-    }
-
-    StringTokenizer st = new StringTokenizer(str, delimiter);
-    List<String> tokens = new ArrayList<>(4);
-    while (st.hasMoreTokens()) {
-      tokens.add(st.nextToken());
-    }
-    return toStringArray(tokens);
+  public static String[] tokenizeToStringArray(final String str, final String delimiters) {
+    return tokenizeToStringArray(str, delimiters, true, true);
   }
 
-  public static String[] tokenizeToStringArray(String str,
-                                               String delimiters,
-                                               boolean trimTokens,
-                                               boolean ignoreEmptyTokens) {
-
+  /**
+   * Tokenize the given {@code String} into a {@code String} array via a
+   * {@link StringTokenizer}.
+   * <p>The given {@code delimiters} string can consist of any number of
+   * delimiter characters. Each of those characters can be used to separate
+   * tokens. A delimiter is always a single character; for multi-character
+   * delimiters, consider using {@link #delimitedListToStringArray}.
+   *
+   * @param str
+   *         the {@code String} to tokenize (potentially {@code null} or empty)
+   * @param delimiters
+   *         the delimiter characters, assembled as a {@code String}
+   *         (each of the characters is individually considered as a delimiter)
+   * @param trimTokens
+   *         trim the tokens via {@link String#trim()}
+   * @param ignoreEmptyTokens
+   *         omit empty tokens from the result array
+   *         (only applies to tokens that are empty after trimming; StringTokenizer
+   *         will not consider subsequent delimiters as token in the first place).
+   *
+   * @return an array of the tokens
+   *
+   * @see java.util.StringTokenizer
+   * @see String#trim()
+   * @see #delimitedListToStringArray
+   */
+  public static String[] tokenizeToStringArray(
+          String str, String delimiters, boolean trimTokens, boolean ignoreEmptyTokens) {
     if (str == null) {
       return Constant.EMPTY_STRING_ARRAY;
     }
     StringTokenizer st = new StringTokenizer(str, delimiters);
-    ArrayList<String> tokens = new ArrayList<>(4);
+    ArrayList<String> tokens = new ArrayList<>();
     while (st.hasMoreTokens()) {
       String token = st.nextToken();
       if (trimTokens) {
@@ -1459,7 +1483,7 @@ else */
    */
   public static TimeZone parseTimeZoneString(String timeZoneString) {
     TimeZone timeZone = TimeZone.getTimeZone(timeZoneString);
-    if ("GMT".equals(timeZone.getID()) && !timeZoneString.startsWith("GMT")) {
+    if ("GMT" .equals(timeZone.getID()) && !timeZoneString.startsWith("GMT")) {
       // We don't want that GMT fallback...
       throw new IllegalArgumentException("Invalid time zone specification '" + timeZoneString + "'");
     }
