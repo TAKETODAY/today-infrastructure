0e90afb59ba59bf453eda386b424f00482d12f66
TODAY <2228592280@qq.com>
2021-07-09 23:57:10 +0800

    :fix: WebServerAutoConfiguration

--- src/main/java/cn/taketoday/framework/config/WebServerAutoConfiguration.java
+++ src/main/java/cn/taketoday/framework/config/WebServerAutoConfiguration.java
@@ -3,22 +3,24 @@ package cn.taketoday.framework.config;
 import cn.taketoday.context.annotation.Configuration;
 import cn.taketoday.context.annotation.IgnoreDuplicates;
 import cn.taketoday.context.annotation.MissingBean;
+import cn.taketoday.context.annotation.Props;
 import cn.taketoday.context.utils.ClassUtils;
+import cn.taketoday.framework.server.AbstractServletWebServer;
 import cn.taketoday.framework.server.JettyServer;
 import cn.taketoday.framework.server.TomcatServer;
 import cn.taketoday.framework.server.UndertowServer;
-import cn.taketoday.framework.server.WebServer;
 
 @Configuration
 @IgnoreDuplicates
 public class WebServerAutoConfiguration {
 
   @MissingBean
-  WebServer webServer() {
-    if(ClassUtils.isPresent("org.eclipse.jetty.server.Server")) {
+  @Props(prefix = "server.") // @since 1.0.3
+  static AbstractServletWebServer webServer() {
+    if (ClassUtils.isPresent("org.eclipse.jetty.server.Server")) {
       return new JettyServer();
     }
-    else if(ClassUtils.isPresent("io.undertow.Undertow")) {
+    else if (ClassUtils.isPresent("io.undertow.Undertow")) {
       return new UndertowServer();
     }
     return new TomcatServer();
