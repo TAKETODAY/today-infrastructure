dcc1d7c31914c554e7b0a617d504a2b4b8618413
yanghaijian <taketoday@foxmail.com>
2021-09-27 14:17:23 +0800

    :zap: ClassUtils#getDefaultClassLoader

--- src/main/java/cn/taketoday/util/ClassUtils.java
+++ src/main/java/cn/taketoday/util/ClassUtils.java
@@ -206,9 +206,51 @@ public abstract class ClassUtils {
 
   /**
    * default class loader
+   * @deprecated use {@link #getDefaultClassLoader()}
    */
+  @Deprecated
   public static ClassLoader getClassLoader() {
     return classLoader;
+  }
+
+	/**
+	 * Return the default ClassLoader to use: typically the thread context
+	 * ClassLoader, if available; the ClassLoader that loaded the ClassUtils
+	 * class will be used as fallback.
+	 * <p>Call this method if you intend to use the thread context ClassLoader
+	 * in a scenario where you clearly prefer a non-null ClassLoader reference:
+	 * for example, for class path resource loading (but not necessarily for
+	 * {@code Class.forName}, which accepts a {@code null} ClassLoader
+	 * reference as well).
+	 * @return the default ClassLoader (only {@code null} if even the system
+	 * ClassLoader isn't accessible)
+	 * @see Thread#getContextClassLoader()
+	 * @see ClassLoader#getSystemClassLoader()
+   * @since 4.0
+	 */
+	@Nullable
+	public static ClassLoader getDefaultClassLoader() {
+    ClassLoader cl = null;
+    try {
+      cl = Thread.currentThread().getContextClassLoader();
+    }
+    catch (Throwable ex) {
+      // Cannot access thread context ClassLoader - falling back...
+    }
+    if (cl == null) {
+      // No thread context class loader -> use class loader of this class.
+      cl = ClassUtils.class.getClassLoader();
+      if (cl == null) {
+        // getClassLoader() returning null indicates the bootstrap ClassLoader
+        try {
+          cl = ClassLoader.getSystemClassLoader();
+        }
+        catch (Throwable ex) {
+          // Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+        }
+      }
+    }
+    return cl;
   }
 
   // ------------------------------------------ Class Scan
@@ -289,7 +331,7 @@ public abstract class ClassUtils {
     }
 
     if (classLoader == null) {
-      classLoader = getClassLoader();
+      classLoader = getDefaultClassLoader();
     }
     try {
       return Class.forName(name, false, classLoader);
