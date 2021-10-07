7a86e5a2e0c2bd4a66863177a308168236f30e7d
TODAY <2228592280@qq.com>
2021-10-07 19:33:17 +0800

    :hammer: 重构 DefaultApplicationEventPublisher 优化 结构

--- src/main/java/cn/taketoday/context/event/DefaultApplicationEventPublisher.java
+++ src/main/java/cn/taketoday/context/event/DefaultApplicationEventPublisher.java
@@ -20,27 +20,46 @@
 
 package cn.taketoday.context.event;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 
+import cn.taketoday.beans.factory.BeanFactory;
+import cn.taketoday.context.aware.BeanFactoryAware;
 import cn.taketoday.core.Assert;
 import cn.taketoday.core.DefaultMultiValueMap;
 import cn.taketoday.core.GenericTypeResolver;
-import cn.taketoday.core.annotation.AnnotationAwareOrderComparator;
+import cn.taketoday.core.Nullable;
 import cn.taketoday.logger.Logger;
 import cn.taketoday.logger.LoggerFactory;
+import cn.taketoday.util.ClassUtils;
 import cn.taketoday.util.CollectionUtils;
+import cn.taketoday.util.ObjectUtils;
 
 /**
  * @author TODAY 2021/10/7 15:20
  * @since 4.0
  */
 @SuppressWarnings({ "rawtypes", "unchecked" })
-public class DefaultApplicationEventPublisher implements ApplicationEventPublisher {
+public class DefaultApplicationEventPublisher implements ApplicationEventPublisher, BeanFactoryAware {
   private static final Logger log = LoggerFactory.getLogger(DefaultApplicationEventPublisher.class);
 
   /** application listeners **/
   private final DefaultMultiValueMap<Class<?>, ApplicationListener>
-          applicationListeners = new DefaultMultiValueMap<>(32);
+          applicationListenerCache = new DefaultMultiValueMap<>(16);
+
+  private final LinkedHashSet<String> listenerBeanNames = new LinkedHashSet<>();
+  private final ArrayList<ApplicationListener> applicationListeners = new ArrayList<>();
+
+  @Nullable
+  private BeanFactory beanFactory;
+
+  public DefaultApplicationEventPublisher() { }
+
+  public DefaultApplicationEventPublisher(@Nullable BeanFactory beanFactory) {
+    this.beanFactory = beanFactory;
+  }
 
   @Override
   public void publishEvent(Object event) {
@@ -48,7 +67,7 @@ public class DefaultApplicationEventPublisher implements ApplicationEventPublish
       log.debug("Publish event: [{}]", event);
     }
 
-    List<ApplicationListener> listeners = applicationListeners.get(event.getClass());
+    List<ApplicationListener> listeners = getApplicationListeners(event);
     if (CollectionUtils.isNotEmpty(listeners)) {
       for (ApplicationListener applicationListener : listeners) {
         applicationListener.onApplicationEvent(event);
@@ -56,47 +75,80 @@ public class DefaultApplicationEventPublisher implements ApplicationEventPublish
     }
   }
 
-  @Override
-  public void addApplicationListener(Class<?> listener) {
+  private List<ApplicationListener> getApplicationListeners(Object event) {
+    Class<?> eventClass = event.getClass();
+    List<ApplicationListener> listenerList = applicationListenerCache.get(eventClass);
+    if (listenerList == null) {
+      listenerList = new ArrayList<>();
+      for (ApplicationListener listener : this.applicationListeners) {
+        if (listener instanceof EventProvider) { // @since 2.1.7
+          Class<?>[] supportedEvent = ((EventProvider) listener).getSupportedEvent();
+          if (ObjectUtils.containsElement(supportedEvent, eventClass)) {
+            listenerList.add(listener);
+          }
+        }
+        else {
+          Class<?> supportedEvent = GenericTypeResolver.resolveTypeArgument(listener.getClass(), ApplicationListener.class);
+          if (ClassUtils.isAssignable(supportedEvent, eventClass)) {
+            listenerList.add(listener);
+          }
+        }
+      }
 
-  }
+      if (CollectionUtils.isNotEmpty(listenerBeanNames)) {
+        Assert.state(beanFactory != null, "No BeanFactory");
+        for (String listenerBeanName : listenerBeanNames) {
 
-  @Override
-  public void removeAllListeners() {
-    applicationListeners.clear();
+          ApplicationListener listener = beanFactory.getBean(listenerBeanName, ApplicationListener.class);
+          listenerList.add(listener);
+        }
+      }
+
+      if (listenerList.isEmpty()) {
+        listenerList = Collections.emptyList();
+      }
+      applicationListenerCache.putIfAbsent(eventClass, listenerList);
+    }
+    return listenerList;
   }
 
   @Override
   public void addApplicationListener(ApplicationListener<?> listener) {
     Assert.notNull(listener, "listener can't be null");
-    if (listener instanceof ApplicationEventCapable) { // @since 2.1.7
-      for (Class<?> type : ((ApplicationEventCapable) listener).getApplicationEvent()) {
-        addApplicationListener(listener, type);
-      }
-    }
-    else {
-      Class<?> eventType = GenericTypeResolver.resolveTypeArgument(listener.getClass(), ApplicationListener.class);
-      addApplicationListener(listener, eventType);
-    }
+    applicationListeners.add(listener);
   }
 
-  /**
-   * Register to registry
-   *
-   * @param listener
-   *         The instance of application listener
-   * @param eventType
-   *         The event type
-   */
-  protected void addApplicationListener(ApplicationListener listener, Class<?> eventType) {
-    List<ApplicationListener> listeners = applicationListeners.get(eventType);
-    if (listeners == null) {
-      applicationListeners.add(eventType, listener);
-    }
-    else if (!listeners.contains(listener)) {
-      listeners.add(listener);
-      AnnotationAwareOrderComparator.sort(listeners);
-    }
+  @Override
+  public void addApplicationListener(String listenerBeanName) {
+    listenerBeanNames.add(listenerBeanName);
+  }
+
+  @Override
+  public void removeAllListeners() {
+    applicationListenerCache.clear();
+    listenerBeanNames.clear();
+    applicationListeners.clear();
+  }
+
+  @Override
+  public void removeApplicationListener(String listenerBeanName) {
+    applicationListenerCache.clear();
+    listenerBeanNames.remove(listenerBeanName);
+  }
+
+  @Override
+  public void removeApplicationListener(ApplicationListener<?> listener) {
+    applicationListenerCache.clear();
+    applicationListeners.remove(listener);
+  }
+
+  public void setBeanFactory(@Nullable BeanFactory beanFactory) {
+    this.beanFactory = beanFactory;
+  }
+
+  @Nullable
+  public BeanFactory getBeanFactory() {
+    return beanFactory;
   }
 
 }
