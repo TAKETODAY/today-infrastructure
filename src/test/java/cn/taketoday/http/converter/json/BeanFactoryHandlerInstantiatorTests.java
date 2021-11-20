db27c943c2b25ea7ec3ddd5a3feebec0054a06df
TODAY <2228592280@qq.com>
2021-11-20 09:36:10 +0800

    :art: 冲突解决

--- src/test/java/cn/taketoday/http/converter/json/BeanFactoryHandlerInstantiatorTests.java
+++ src/test/java/cn/taketoday/http/converter/json/BeanFactoryHandlerInstantiatorTests.java
@@ -56,7 +56,8 @@ import java.util.Map;
 
 import cn.taketoday.beans.factory.BeanDefinition;
 import cn.taketoday.beans.factory.StandardBeanFactory;
-import cn.taketoday.beans.dependency.StandardDependenciesBeanPostProcessor;
+import cn.taketoday.context.StandardApplicationContext;
+import cn.taketoday.context.autowire.AutowiredDependenciesBeanPostProcessor;
 import cn.taketoday.lang.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -74,8 +75,9 @@ public class BeanFactoryHandlerInstantiatorTests {
 
   @BeforeEach
   public void setup() {
-    StandardBeanFactory beanFactory = new StandardBeanFactory();
-    StandardDependenciesBeanPostProcessor bpp = new StandardDependenciesBeanPostProcessor(beanFactory);
+    StandardApplicationContext context = new StandardApplicationContext();
+    AutowiredDependenciesBeanPostProcessor bpp = new AutowiredDependenciesBeanPostProcessor(context);
+    StandardBeanFactory beanFactory = context.getBeanFactory();
     beanFactory.addBeanPostProcessor(bpp);
     beanFactory.registerBeanDefinition(new BeanDefinition("capitalizer", Capitalizer.class));
     instantiator = new BeanFactoryHandlerInstantiator(beanFactory);
