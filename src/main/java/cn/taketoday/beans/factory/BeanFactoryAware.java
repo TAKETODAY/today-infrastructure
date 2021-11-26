de504ef9e8e18110d95cd16c9d43aa80734c79c3
TODAY <2228592280@qq.com>
2021-11-26 15:09:26 +0800

    :memo: BeanFactoryAware

--- src/main/java/cn/taketoday/beans/factory/BeanFactoryAware.java
+++ src/main/java/cn/taketoday/beans/factory/BeanFactoryAware.java
@@ -19,13 +19,31 @@
  */
 package cn.taketoday.beans.factory;
 
+import cn.taketoday.beans.InitializingBean;
+
 /**
- * {@link BeanFactory} Injection
+ * Interface to be implemented by beans that wish to be aware of their
+ * owning {@link BeanFactory}.
  *
- * @author TODAY <br>
- * 2018-08-03 15:54
+ * <p>For example, beans can look up collaborating (same factory) beans via the factory
+ * (Dependency Lookup). Note that most beans will choose to receive references
+ * to collaborating beans via corresponding bean properties or constructor
+ * arguments (Dependency Injection).
+ *
+ * @author TODAY 2018-08-03 15:54
  */
 public interface BeanFactoryAware extends Aware {
 
-  void setBeanFactory(BeanFactory beanFactory);
+  /**
+   * Callback that supplies the owning factory to a bean instance.
+   * <p>Invoked after the population of normal bean properties
+   * but before an initialization callback such as
+   * {@link InitializingBean#afterPropertiesSet()} or a custom init-method.
+   *
+   * @param beanFactory owning BeanFactory (never {@code null}).
+   * The bean can immediately call methods on the factory.
+   * @throws BeansException in case of initialization errors
+   * @see BeanInitializationException
+   */
+  void setBeanFactory(BeanFactory beanFactory) throws BeansException;
 }
