5c883393723a3da61d5af3d46288bb3113f38908
TODAY <2228592280@qq.com>
2021-11-26 22:46:30 +0800

    :memo: FactoryBean

--- src/main/java/cn/taketoday/beans/factory/FactoryBean.java
+++ src/main/java/cn/taketoday/beans/factory/FactoryBean.java
@@ -34,13 +34,13 @@ import cn.taketoday.lang.Nullable;
  * ,and initialization by this factory.
  *
  * <p>
- * The implementation of FactoryBean is a factory; its instance will cached in
- * {@link BeanFactory} as a singleton bean.
+ * <b>NOTE:</b> The implementation of FactoryBean is a factory; its instance will cached in
+ * {@link BeanFactory} as a singleton bean when its define as a singleton.
  *
- * @author TODAY <br>
- * 2018-08-03 17:38
+ * @author TODAY 2018-08-03 17:38
  */
 public interface FactoryBean<T> {
+
   /**
    * Return an instance (possibly shared or independent) of the object
    * managed by this factory.
@@ -49,22 +49,48 @@ public interface FactoryBean<T> {
    * <p>If this FactoryBean is not fully initialized yet at the time of
    * the call (for example because it is involved in a circular reference),
    * throw a corresponding {@link FactoryBeanNotInitializedException}.
-   * <p>As of Spring 2.0, FactoryBeans are allowed to return {@code null}
+   * <p>FactoryBeans are allowed to return {@code null}
    * objects. The factory will consider this as normal value to be used; it
    * will not throw a FactoryBeanNotInitializedException in this case anymore.
    * FactoryBean implementations are encouraged to throw
    * FactoryBeanNotInitializedException themselves now, as appropriate.
+   * <p>
+   * <b>NOTE:</b> is FactoryBean is a Prototype bean that {@code getObject()} will
+   * let {@link #isSingleton()} invalid
+   * </p>
    *
    * @return an instance of the bean (can be {@code null})
    * @throws Exception in case of creation errors
    * @see FactoryBeanNotInitializedException
    * @since 4.0
    */
-  @Nullable
   T getObject() throws Exception;
 
   /**
-   * /**
+   * Return the type of object that this FactoryBean creates,
+   * or {@code null} if not known in advance.
+   * <p>This allows one to check for specific types of beans without
+   * instantiating objects, for example on autowiring.
+   * <p>In the case of implementations that are creating a singleton object,
+   * this method should try to avoid singleton creation as far as possible;
+   * it should rather estimate the type in advance.
+   * For prototypes, returning a meaningful type here is advisable too.
+   * <p>This method can be called <i>before</i> this FactoryBean has
+   * been fully initialized. It must not rely on state created during
+   * initialization; of course, it can still use such state if available.
+   * <p><b>NOTE:</b> Autowiring will simply ignore FactoryBeans that return
+   * {@code null} here. Therefore it is highly recommended to implement
+   * this method properly, using the current state of the FactoryBean.
+   *
+   * @return the type of object that this FactoryBean creates,
+   * or {@code null} if not known at the time of the call
+   * @see BeanFactory#getBeansOfType
+   * @since 2.1.2
+   */
+  @Nullable
+  Class<?> getObjectType();
+
+  /**
    * Is the object managed by this factory a singleton? That is,
    * will {@link #getObject()} always return the same object
    * (a reference that can be cached)?
@@ -89,11 +115,7 @@ public interface FactoryBean<T> {
    * @return whether the exposed object is a singleton
    * @see #getObject()
    * @see SmartFactoryBean#isPrototype()
-   * @since 2.1.2
    */
-  @Nullable
-  Class<?> getObjectType();
-
   default boolean isSingleton() {
     return true;
   }
