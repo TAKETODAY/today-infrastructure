b4520fb66b1a060b4b667d84587269e5ca664c3b
TODAY <2228592280@qq.com>
2021-11-28 14:04:54 +0800

    :zap: FactoryBeanTests

--- src/test/java/cn/taketoday/beans/factory/FactoryBeanTests.java
+++ src/test/java/cn/taketoday/beans/factory/FactoryBeanTests.java
@@ -19,16 +19,24 @@
  */
 package cn.taketoday.beans.factory;
 
-import cn.taketoday.lang.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import cn.taketoday.beans.InitializingBean;
 import cn.taketoday.context.StandardApplicationContext;
 import cn.taketoday.context.annotation.Import;
 import cn.taketoday.context.aware.ApplicationContextSupport;
+import cn.taketoday.context.loader.AnnotatedBeanDefinitionReader;
+import cn.taketoday.lang.Assert;
+import cn.taketoday.lang.Component;
+import cn.taketoday.lang.Configuration;
 import cn.taketoday.lang.Singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Today <br>
@@ -145,6 +153,271 @@ class FactoryBeanTests {
 
       Assertions.assertNotNull(applicationContext.getBean("$testFactoryBean"));
       Assertions.assertTrue(applicationContext.getBean("$testFactoryBean") instanceof TESTFactoryBean);
+    }
+  }
+
+  //
+
+  @Test
+  public void testFactoryBeanReturnsNull() throws Exception {
+    StandardBeanFactory factory = new StandardBeanFactory();
+    new AnnotatedBeanDefinitionReader(factory, false)
+            .registerBean("factoryBean", NullReturningFactoryBean.class);
+    assertThat(factory.getBean("factoryBean")).isNull();
+  }
+
+  @Test
+  public void testFactoryBeansWithAutowiring() throws Exception {
+    StandardBeanFactory factory = new StandardBeanFactory();
+    AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(factory, false);
+
+    Alpha alpha1 = new Alpha();
+    Gamma gamma1 = new Gamma();
+    // gammaFactory betaFactory getGamma
+    BeanDefinition gammaFactoryDef = new BeanDefinition();
+    gammaFactoryDef.setName("gammaFactory");
+    gammaFactoryDef.setFactoryMethodName("getGamma");
+    gammaFactoryDef.setFactoryBeanName("betaFactory"); // is FactoryBean so its real bean is Beta
+
+    reader.registerBean("alpha", alpha1);
+    reader.register(gammaFactoryDef);
+    reader.registerBean("betaFactory", BetaFactoryBean.class, definition -> {
+      definition.addPropertyValue("beta", BeanReference.from("beta"));
+    });
+    reader.registerBean("beta", Beta.class, definition -> {
+      definition.addPropertyValue("name", "yourName");
+    });
+
+    assertThat(factory.getType("betaFactory")).isNull();
+
+    Alpha alpha = (Alpha) factory.getBean("alpha");
+    Beta beta = (Beta) factory.getBean("beta");
+    Gamma gamma = (Gamma) factory.getBean("gamma");
+    Gamma gamma2 = (Gamma) factory.getBean("gammaFactory");
+
+    assertThat(alpha.getBeta()).isSameAs(beta);
+    assertThat(beta.getGamma()).isSameAs(gamma);
+    assertThat(beta.getGamma()).isSameAs(gamma2);
+    assertThat(beta.getName()).isEqualTo("yourName");
+  }
+
+  @Test
+  public void testCircularReferenceWithPostProcessor() {
+    StandardBeanFactory factory = new StandardBeanFactory();
+    AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(factory, false);
+
+    reader.registerBean(BeanImpl2.class, definition -> {
+      definition.addPropertyValue("impl1", BeanReference.from(BeanImpl2.class, false));
+    });
+
+    reader.registerBean(BeanImpl1.class, definition -> {
+      definition.addPropertyValue("impl2", BeanReference.from(BeanImpl2.class, false));
+    });
+
+    CountingPostProcessor counter = new CountingPostProcessor();
+    factory.addBeanPostProcessor(counter);
+
+    BeanImpl1 impl1 = factory.getBean(BeanImpl1.class);
+    assertThat(impl1).isNotNull();
+    assertThat(impl1.getImpl2()).isNotNull();
+    assertThat(impl1.getImpl2()).isNotNull();
+    assertThat(impl1.getImpl2().getImpl1()).isSameAs(impl1);
+    assertThat(counter.getCount("bean1")).isEqualTo(1);
+    assertThat(counter.getCount("bean2")).isEqualTo(1);
+  }
+
+  public static class NullReturningFactoryBean implements FactoryBean<Object> {
+
+    @Override
+    public Object getObject() {
+      return null;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+      return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+      return true;
+    }
+  }
+
+  public static class Alpha implements InitializingBean {
+
+    private Beta beta;
+
+    public void setBeta(Beta beta) {
+      this.beta = beta;
+    }
+
+    public Beta getBeta() {
+      return beta;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+      Assert.notNull(beta, "'beta' property is required");
+    }
+  }
+
+  public static class Beta implements InitializingBean {
+
+    private Gamma gamma;
+
+    private String name;
+
+    public void setGamma(Gamma gamma) {
+      this.gamma = gamma;
+    }
+
+    public Gamma getGamma() {
+      return gamma;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+      Assert.notNull(gamma, "'gamma' property is required");
+    }
+  }
+
+  public static class Gamma { }
+
+  @Component
+  public static class BetaFactoryBean implements FactoryBean<Object> {
+
+    public BetaFactoryBean(Alpha alpha) { }
+
+    private Beta beta;
+
+    public void setBeta(Beta beta) {
+      this.beta = beta;
+    }
+
+    @Override
+    public Object getObject() {
+      return this.beta;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+      return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+      return true;
+    }
+  }
+
+  public static class PassThroughFactoryBean<T> implements FactoryBean<T>, BeanFactoryAware {
+
+    private Class<T> type;
+
+    private String instanceName;
+
+    private BeanFactory beanFactory;
+
+    private T instance;
+
+    public PassThroughFactoryBean(Class<T> type) {
+      this.type = type;
+    }
+
+    public void setInstanceName(String instanceName) {
+      this.instanceName = instanceName;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+      this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public T getObject() {
+      if (instance == null) {
+        instance = beanFactory.getBean(instanceName, type);
+      }
+      return instance;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+      return type;
+    }
+
+    @Override
+    public boolean isSingleton() {
+      return true;
+    }
+  }
+
+  public static class CountingPostProcessor implements InitializationBeanPostProcessor {
+
+    private final Map<String, AtomicInteger> count = new HashMap<>();
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+      return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+      if (bean instanceof FactoryBean) {
+        return bean;
+      }
+      AtomicInteger c = count.get(beanName);
+      if (c == null) {
+        c = new AtomicInteger();
+        count.put(beanName, c);
+      }
+      c.incrementAndGet();
+      return bean;
+    }
+
+    public int getCount(String beanName) {
+      AtomicInteger c = count.get(beanName);
+      if (c != null) {
+        return c.intValue();
+      }
+      else {
+        return 0;
+      }
+    }
+  }
+
+  public static class BeanImpl1 {
+
+    private BeanImpl2 impl2;
+
+    public BeanImpl2 getImpl2() {
+      return impl2;
+    }
+
+    public void setImpl2(BeanImpl2 impl2) {
+      this.impl2 = impl2;
+    }
+  }
+
+  public static class BeanImpl2 {
+
+    private BeanImpl1 impl1;
+
+    public BeanImpl1 getImpl1() {
+      return impl1;
+    }
+
+    public void setImpl1(BeanImpl1 impl1) {
+      this.impl1 = impl1;
     }
   }
 
