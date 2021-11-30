4b743842c3d46c7b6403c79192e775f397f453ed
TODAY <2228592280@qq.com>
2021-11-30 17:25:39 +0800

    :zap: FactoryBeans

---
+++ src/main/java/cn/taketoday/beans/factory/support/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Support package for the FactoryBeans. from Spring.
+ */
+@NonNullApi
+@NonNullFields
+package cn.taketoday.beans.factory.support;
+
+import cn.taketoday.lang.NonNullApi;
+import cn.taketoday.lang.NonNullFields;
