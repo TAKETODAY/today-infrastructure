063e5082891a75469136ff9982750a6445a0fe43
TODAY <2228592280@qq.com>
2022-01-10 09:49:41 +0800

    :zap: pom h2.version

--- src/test/java/cn/taketoday/jdbc/support/incrementer/H2SequenceMaxValueIncrementerTests.java
+++ src/test/java/cn/taketoday/jdbc/support/incrementer/H2SequenceMaxValueIncrementerTests.java
@@ -1,9 +1,17 @@
 package cn.taketoday.jdbc.support.incrementer;
 
+import org.h2.engine.Mode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
+import cn.taketoday.jdbc.core.JdbcTemplate;
+import cn.taketoday.jdbc.datasource.SimpleDriverDataSource;
+import cn.taketoday.jdbc.datasource.embedded.EmbeddedDatabase;
 import cn.taketoday.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import cn.taketoday.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -15,16 +23,48 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class H2SequenceMaxValueIncrementerTests {
 
+  /**
+   * Tests that the incrementer works when using the JDBC connection URL used
+   * in the {@code H2EmbeddedDatabaseConfigurer} which is used transparently
+   * when using Spring's {@link EmbeddedDatabaseBuilder}.
+   *
+   * <p>In other words, this tests compatibility with the default H2
+   * <em>compatibility mode</em>.
+   */
   @Test
-  void testH2SequenceMaxValueIncrementer() {
-    DataSource dataSource = new EmbeddedDatabaseBuilder()
+  void incrementsSequenceUsingH2EmbeddedDatabaseConfigurer() {
+    EmbeddedDatabase database = new EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
+            .generateUniqueName(true)
             .addScript("classpath:/cn/taketoday/jdbc/support/incrementer/schema.sql")
             .build();
-    H2SequenceMaxValueIncrementer incrementer = new H2SequenceMaxValueIncrementer(dataSource, "SEQ");
 
-    assertThat(incrementer.nextIntValue()).isEqualTo(1);
-    assertThat(incrementer.nextStringValue()).isEqualTo("2");
+    assertIncrements(database);
+
+    database.shutdown();
   }
 
+  /**
+   * Tests that the incrementer works when using all supported H2 <em>compatibility modes</em>.
+   */
+  @ParameterizedTest
+  @EnumSource(Mode.ModeEnum.class)
+  void incrementsSequenceWithExplicitH2CompatibilityMode(Mode.ModeEnum mode) {
+    String connectionUrl = String.format("jdbc:h2:mem:%s;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false;MODE=%s", UUID.randomUUID().toString(), mode);
+    DataSource dataSource = new SimpleDriverDataSource(new org.h2.Driver(), connectionUrl, "sa", "");
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    jdbcTemplate.execute("CREATE SEQUENCE SEQ");
+
+    assertIncrements(dataSource);
+
+    jdbcTemplate.execute("SHUTDOWN");
+  }
+
+  private void assertIncrements(DataSource dataSource) {
+    assertThat(new JdbcTemplate(dataSource).queryForObject("values next value for SEQ", int.class)).isEqualTo(1);
+
+    H2SequenceMaxValueIncrementer incrementer = new H2SequenceMaxValueIncrementer(dataSource, "SEQ");
+    assertThat(incrementer.nextIntValue()).isEqualTo(2);
+    assertThat(incrementer.nextStringValue()).isEqualTo("3");
+  }
 }
