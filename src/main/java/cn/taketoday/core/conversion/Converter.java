f112ef08d4ca90deabefdb62e6a7e2ad10b90262
TODAY <2228592280@qq.com>
2022-01-11 14:37:03 +0800

    :zap: Introduce Converter.andThen(...)

--- src/main/java/cn/taketoday/core/conversion/Converter.java
+++ src/main/java/cn/taketoday/core/conversion/Converter.java
@@ -19,9 +19,15 @@
  */
 package cn.taketoday.core.conversion;
 
+import cn.taketoday.lang.Assert;
+
 /**
- * A {@link Converter}
+ * A converter converts a source object of type {@code S} to a target of type {@code T}.
  *
+ * <p>Implementations of this interface are thread-safe and can be shared.
+ *
+ * @param <S> the source type
+ * @param <T> the target type
  * @author TODAY 2018-07-07 21:33:52
  */
 @FunctionalInterface
@@ -30,10 +36,31 @@ public interface Converter<S, T> {
   /**
    * Convert source object to target class {@link T}
    *
-   * @param source source instance
-   * @return A converted instance
-   * @throws ConversionException can't convert source to T
+   * @param source the source object to convert, which must be an instance of {@code S} (never {@code null})
+   * @return A converted instance which must be an instance of {@code T} (potentially {@code null})
+   * @throws IllegalArgumentException if the source cannot be converted to the desired target type
    */
   T convert(S source);
+
+  /**
+   * Construct a composed {@link Converter} that first applies this {@link Converter}
+   * to its input, and then applies the {@code after} {@link Converter} to the
+   * result.
+   *
+   * @param after the {@link Converter} to apply after this {@link Converter}
+   * is applied
+   * @param <U> the type of output of both the {@code after} {@link Converter}
+   * and the composed {@link Converter}
+   * @return a composed {@link Converter} that first applies this {@link Converter}
+   * and then applies the {@code after} {@link Converter}
+   * @since 4.0
+   */
+  default <U> Converter<S, U> andThen(Converter<? super T, ? extends U> after) {
+    Assert.notNull(after, "After Converter must not be null");
+    return (S s) -> {
+      T initialResult = convert(s);
+      return initialResult != null ? after.convert(initialResult) : null;
+    };
+  }
 
 }
