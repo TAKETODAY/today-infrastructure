23b3cd72131719d87b81b2f3b5c5a0bafdfb8bd6
TODAY <2228592280@qq.com>
2022-01-19 22:06:18 +0800

    :zap: AbstractNamedValueParameterResolvingStrategy

---
+++ src/main/java/cn/taketoday/web/resolver/AbstractNamedValueParameterResolvingStrategy.java
@@ -0,0 +1,268 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.web.resolver;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import cn.taketoday.beans.factory.support.BeanExpressionContext;
+import cn.taketoday.beans.factory.support.BeanExpressionResolver;
+import cn.taketoday.beans.factory.support.ConfigurableBeanFactory;
+import cn.taketoday.core.MethodParameter;
+import cn.taketoday.lang.Constant;
+import cn.taketoday.lang.Nullable;
+import cn.taketoday.web.RequestContext;
+import cn.taketoday.web.handler.method.NamedValueInfo;
+import cn.taketoday.web.handler.method.ResolvableMethodParameter;
+import cn.taketoday.web.scope.RequestScope;
+
+/**
+ * Abstract base class for resolving method arguments from a named value.
+ * Request parameters, request headers, and path variables are examples of named
+ * values. Each may have a name, a required flag, and a default value.
+ *
+ * <p>Subclasses define how to do the following:
+ * <ul>
+ * <li>Obtain named value information for a method parameter
+ * <li>Resolve names into argument values
+ * <li>Handle missing argument values when argument values are required
+ * <li>Optionally handle a resolved value
+ * </ul>
+ *
+ * <p>A default value string can contain ${...} placeholders and Spring Expression
+ * Language #{...} expressions. For this to work a
+ * {@link ConfigurableBeanFactory} must be supplied to the class constructor.
+ *
+ * @author Arjen Poutsma
+ * @author Rossen Stoyanchev
+ * @author Juergen Hoeller
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/1/19 21:26
+ */
+public abstract class AbstractNamedValueParameterResolvingStrategy implements ParameterResolvingStrategy {
+
+  @Nullable
+  private final ConfigurableBeanFactory configurableBeanFactory;
+
+  @Nullable
+  private final BeanExpressionContext expressionContext;
+
+  private final Map<MethodParameter, NamedValueInfo> namedValueInfoCache = new ConcurrentHashMap<>(256);
+
+  public AbstractNamedValueParameterResolvingStrategy() {
+    this.configurableBeanFactory = null;
+    this.expressionContext = null;
+  }
+
+  /**
+   * Create a new {@link AbstractNamedValueParameterResolvingStrategy} instance.
+   *
+   * @param beanFactory a bean factory to use for resolving ${...} placeholder
+   * and #{...} EL expressions in default values, or {@code null} if default
+   * values are not expected to contain expressions
+   */
+  public AbstractNamedValueParameterResolvingStrategy(@Nullable ConfigurableBeanFactory beanFactory) {
+    this.configurableBeanFactory = beanFactory;
+    this.expressionContext =
+            beanFactory != null ? new BeanExpressionContext(beanFactory, new RequestScope()) : null;
+  }
+
+  @Nullable
+  @Override
+  public final Object resolveParameter(
+          RequestContext context, ResolvableMethodParameter parameter) throws Throwable {
+
+    MethodParameter methodParameter = parameter.getParameter();
+    NamedValueInfo namedValueInfo = getNamedValueInfo(parameter);
+    MethodParameter nestedParameter = methodParameter.nestedIfOptional();
+
+    Object resolvedName = resolveEmbeddedValuesAndExpressions(namedValueInfo.name);
+    if (resolvedName == null) {
+      throw new IllegalArgumentException(
+              "Specified name must not resolve to null: [" + namedValueInfo.name + "]");
+    }
+
+    Object arg = resolveName(resolvedName.toString(), nestedParameter, context);
+    if (arg == null) {
+      if (namedValueInfo.defaultValue != null) {
+        arg = resolveEmbeddedValuesAndExpressions(namedValueInfo.defaultValue);
+      }
+      else if (namedValueInfo.required && !nestedParameter.isOptional()) {
+        handleMissingValue(namedValueInfo.name, nestedParameter, context);
+      }
+      arg = handleNullValue(namedValueInfo.name, arg, nestedParameter.getNestedParameterType());
+    }
+    else if ("".equals(arg) && namedValueInfo.defaultValue != null) {
+      arg = resolveEmbeddedValuesAndExpressions(namedValueInfo.defaultValue);
+    }
+
+    handleResolvedValue(arg, namedValueInfo.name, methodParameter, context);
+
+    return arg;
+  }
+
+  /**
+   * Obtain the named value for the given method parameter.
+   */
+  private NamedValueInfo getNamedValueInfo(ResolvableMethodParameter parameter) {
+    NamedValueInfo namedValueInfo = parameter.getNamedValueInfo();
+    if (namedValueInfo == null) {
+      namedValueInfo = createNamedValueInfo(parameter.getParameter());
+      namedValueInfo = updateNamedValueInfo(parameter.getParameter(), namedValueInfo);
+      parameter.setNamedValueInfo(namedValueInfo);
+    }
+    return namedValueInfo;
+  }
+
+  /**
+   * Create the {@link NamedValueInfo} object for the given
+   * method parameter. Implementations typically
+   * retrieve the method annotation by means of
+   * {@link MethodParameter#getParameterAnnotation(Class)}.
+   *
+   * @param parameter the method parameter
+   * @return the named value information
+   */
+  protected abstract NamedValueInfo createNamedValueInfo(MethodParameter parameter);
+
+  /**
+   * Create a new NamedValueInfo based on the given NamedValueInfo with sanitized values.
+   */
+  private NamedValueInfo updateNamedValueInfo(MethodParameter parameter, NamedValueInfo info) {
+    String name = info.name;
+    if (info.name.isEmpty()) {
+      name = parameter.getParameterName();
+      if (name == null) {
+        throw new IllegalArgumentException(
+                "Name for argument of type [" + parameter.getNestedParameterType().getName() +
+                        "] not specified, and parameter name information not found in class file either.");
+      }
+    }
+    String defaultValue = Constant.DEFAULT_NONE.equals(info.defaultValue) ? null : info.defaultValue;
+    return new NamedValueInfo(name, info.required, defaultValue);
+  }
+
+  /**
+   * Resolve the given annotation-specified value,
+   * potentially containing placeholders and expressions.
+   */
+  @Nullable
+  private Object resolveEmbeddedValuesAndExpressions(String value) {
+    if (this.configurableBeanFactory == null || this.expressionContext == null) {
+      return value;
+    }
+    String placeholdersResolved = this.configurableBeanFactory.resolveEmbeddedValue(value);
+    BeanExpressionResolver exprResolver = this.configurableBeanFactory.getBeanExpressionResolver();
+    if (exprResolver == null) {
+      return value;
+    }
+    return exprResolver.evaluate(placeholdersResolved, this.expressionContext);
+  }
+
+  /**
+   * Resolve the given parameter type and value name into an argument value.
+   *
+   * @param name the name of the value being resolved
+   * @param parameter the method parameter to resolve to an argument value
+   * (pre-nested in case of a {@link java.util.Optional} declaration)
+   * @param context the current request context
+   * @return the resolved argument (may be {@code null})
+   * @throws Exception in case of errors
+   */
+  @Nullable
+  protected abstract Object resolveName(String name, MethodParameter parameter, RequestContext context)
+          throws Exception;
+
+  /**
+   * Invoked when a named value is required, but
+   * {@link #resolveName(String, MethodParameter, RequestContext)}
+   * returned {@code null} and there is no default value.
+   * Subclasses typically throw an exception in this case.
+   *
+   * @param name the name for the value
+   * @param parameter the method parameter
+   * @param request the current request
+   */
+  protected void handleMissingValue(String name, MethodParameter parameter, RequestContext request)
+          throws Exception {
+
+    handleMissingValue(name, parameter);
+  }
+
+  /**
+   * Invoked when a named value is required, but
+   * {@link #resolveName(String, MethodParameter, RequestContext)}
+   * returned {@code null} and there is no default value.
+   * Subclasses typically throw an exception in this case.
+   *
+   * @param name the name for the value
+   * @param parameter the method parameter
+   */
+  protected void handleMissingValue(String name, MethodParameter parameter) {
+    throw new MissingParameterException(parameter);
+  }
+
+  /**
+   * Invoked when a named value is present but becomes {@code null} after conversion.
+   *
+   * @param name the name for the value
+   * @param parameter the method parameter
+   * @param request the current request
+   */
+  protected void handleMissingValueAfterConversion(
+          String name, MethodParameter parameter, RequestContext request) throws Exception {
+
+    handleMissingValue(name, parameter, request);
+  }
+
+  /**
+   * A {@code null} results in a {@code false} value for
+   * {@code boolean}s or an exception for other primitives.
+   */
+  @Nullable
+  private Object handleNullValue(String name, @Nullable Object value, Class<?> paramType) {
+    if (value == null) {
+      if (Boolean.TYPE.equals(paramType)) {
+        return Boolean.FALSE;
+      }
+      else if (paramType.isPrimitive()) {
+        throw new IllegalStateException("Optional " + paramType.getSimpleName() + " parameter '" + name +
+                "' is present but cannot be translated into a null value due to being declared as a " +
+                "primitive type. Consider declaring it as object wrapper for the corresponding primitive type.");
+      }
+    }
+    return value;
+  }
+
+  /**
+   * Invoked after a value is resolved.
+   *
+   * @param arg the resolved argument value
+   * @param name the argument name
+   * @param parameter the argument parameter type
+   * @param webRequest the current request
+   */
+  protected void handleResolvedValue(
+          @Nullable Object arg, String name, MethodParameter parameter, RequestContext webRequest) {
+
+  }
+
+}
