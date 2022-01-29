09155900be3ae467389d3b84373184fbdb77d677
TODAY <2228592280@qq.com>
2022-01-29 09:44:41 +0800

    :zap: Improve Javadoc for DatabasePopulator

--- src/main/java/cn/taketoday/jdbc/datasource/init/DatabasePopulator.java
+++ src/main/java/cn/taketoday/jdbc/datasource/init/DatabasePopulator.java
@@ -44,16 +44,18 @@ public interface DatabasePopulator {
   /**
    * Populate, initialize, or clean up the database using the provided JDBC
    * connection.
+   * <p><strong>Warning</strong>: Concrete implementations should not close
+   * the provided {@link Connection}.
    * <p>Concrete implementations <em>may</em> throw an {@link SQLException} if
    * an error is encountered but are <em>strongly encouraged</em> to throw a
-   * specific {@link ScriptException} instead. For example,
-   * {@link ResourceDatabasePopulator} and {@link DatabasePopulator} wrap
+   * specific {@link ScriptException} instead. For example, Spring's
+   * {@link ResourceDatabasePopulator} and {@link DatabasePopulator#execute} wrap
    * all {@code SQLExceptions} in {@code ScriptExceptions}.
    *
-   * @param connection the JDBC connection to use to populate the db; already
-   * configured and ready to use; never {@code null}
+   * @param connection the JDBC connection to use; already configured and
+   * ready to use; never {@code null}
    * @throws SQLException if an unrecoverable data access exception occurs
-   * during database population
+   * while interacting with the database
    * @throws ScriptException in all other error cases
    * @see DatabasePopulator#execute
    */
