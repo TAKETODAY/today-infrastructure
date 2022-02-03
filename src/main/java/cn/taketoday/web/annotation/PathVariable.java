49b9ef83d45273882a87b85bf72b86ffc9250a6a
TODAY <2228592280@qq.com>
2022-02-03 20:21:02 +0800

    :zap: PathVariable

--- src/main/java/cn/taketoday/web/annotation/PathVariable.java
+++ src/main/java/cn/taketoday/web/annotation/PathVariable.java
@@ -24,6 +24,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import cn.taketoday.core.annotation.AliasFor;
 import cn.taketoday.lang.Constant;
 
 /**
@@ -63,7 +64,29 @@ import cn.taketoday.lang.Constant;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PathVariable {
 
-  /** Request parameter name in path */
+  /**
+   * Alias for {@link #name}.
+   */
+  @AliasFor("name")
   String value() default Constant.BLANK;
+
+  /**
+   * The name of the path variable to bind to.
+   *
+   * @since 4.0
+   */
+  @AliasFor("value")
+  String name() default Constant.BLANK;
+
+  /**
+   * Whether the path variable is required.
+   * <p>Defaults to {@code true}, leading to an exception being thrown if the path
+   * variable is missing in the incoming request. Switch this to {@code false} if
+   * you prefer a {@code null} or Java 8 {@code java.util.Optional} in this case.
+   * e.g. on a {@code ModelAttribute} method which serves for different requests.
+   *
+   * @since 4.0
+   */
+  boolean required() default true;
 
 }
