03659a738637cdf6a457bd179e7fce8ccad40bd5
TODAY <2228592280@qq.com>
2022-02-12 19:56:03 +0800

    :zap: ReturnValueHandlerManager tests

---
+++ src/test/java/cn/taketoday/web/handler/ReturnValueHandlerManagerTests.java
@@ -0,0 +1,61 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.web.handler;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import cn.taketoday.http.converter.StringHttpMessageConverter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/2/12 19:34
+ */
+class ReturnValueHandlerManagerTests {
+
+  @Test
+  void returnValueHandlerManager() {
+    ReturnValueHandlerManager manager = new ReturnValueHandlerManager();
+    assertThat(manager.getMessageConverters()).isNotEmpty().hasSize(3);
+
+    ReturnValueHandlerManager manager1 = new ReturnValueHandlerManager(manager.getMessageConverters());
+
+    assertThat(manager1.getMessageConverters()).isNotEmpty().hasSize(3);
+    assertThat(manager1).isNotEqualTo(manager);
+
+    manager.setMessageConverters(List.of(new StringHttpMessageConverter(StandardCharsets.US_ASCII)));
+    assertThat(manager.getMessageConverters()).isNotEmpty().hasSize(1);
+
+    assertThat(manager.toString()).isNotEmpty();
+    manager.hashCode();
+  }
+
+  @Test
+  void addHandler() {
+    ReturnValueHandlerManager manager = new ReturnValueHandlerManager();
+    manager.addHandlers(new HttpStatusReturnValueHandler());
+
+  }
+}
