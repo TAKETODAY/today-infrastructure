e42a00c6dccbed15ee050362ca62b00a3a9f10a1
TODAY <2228592280@qq.com>
2022-02-18 10:57:03 +0800

    :zap: Instantiator

---
+++ src/main/java/cn/taketoday/util/Instantiator.java
@@ -0,0 +1,241 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.util;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import cn.taketoday.core.annotation.AnnotationAwareOrderComparator;
+import cn.taketoday.lang.Assert;
+
+/**
+ * Simple factory used to instantiate objects by injecting available parameters.
+ *
+ * @param <T> the type to instantiate
+ * @author Phillip Webb
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/2/18 10:53
+ */
+public class Instantiator<T> {
+
+  private static final Comparator<Constructor<?>> CONSTRUCTOR_COMPARATOR =
+          Comparator.<Constructor<?>>comparingInt(Constructor::getParameterCount).reversed();
+
+  private final Class<?> type;
+
+  private final Map<Class<?>, Function<Class<?>, Object>> availableParameters;
+
+  /**
+   * Create a new {@link Instantiator} instance for the given type.
+   *
+   * @param type the type to instantiate
+   * @param availableParameters consumer used to register available parameters
+   */
+  public Instantiator(Class<?> type, Consumer<AvailableParameters> availableParameters) {
+    this.type = type;
+    this.availableParameters = getAvailableParameters(availableParameters);
+  }
+
+  private Map<Class<?>, Function<Class<?>, Object>> getAvailableParameters(
+          Consumer<AvailableParameters> availableParameters) {
+    LinkedHashMap<Class<?>, Function<Class<?>, Object>> result = new LinkedHashMap<>();
+    availableParameters.accept(new AvailableParameters() {
+
+      @Override
+      public void add(Class<?> type, Object instance) {
+        result.put(type, (factoryType) -> instance);
+      }
+
+      @Override
+      public void add(Class<?> type, Function<Class<?>, Object> factory) {
+        result.put(type, factory);
+      }
+
+    });
+    return Collections.unmodifiableMap(result);
+  }
+
+  /**
+   * Instantiate the given set of class name, injecting constructor arguments as
+   * necessary.
+   *
+   * @param names the class names to instantiate
+   * @return a list of instantiated instances
+   */
+  public List<T> instantiate(Collection<String> names) {
+    return instantiate(null, names);
+  }
+
+  /**
+   * Instantiate the given set of class name, injecting constructor arguments as
+   * necessary.
+   *
+   * @param classLoader the source classloader
+   * @param names the class names to instantiate
+   * @return a list of instantiated instances
+   */
+  public List<T> instantiate(ClassLoader classLoader, Collection<String> names) {
+    Assert.notNull(names, "Names must not be null");
+    return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name)));
+  }
+
+  /**
+   * Instantiate the given set of classes, injecting constructor arguments as necessary.
+   *
+   * @param types the types to instantiate
+   * @return a list of instantiated instances
+   */
+  public List<T> instantiateTypes(Collection<Class<?>> types) {
+    Assert.notNull(types, "Types must not be null");
+    return instantiate(types.stream().map(TypeSupplier::forType));
+  }
+
+  private List<T> instantiate(Stream<TypeSupplier> typeSuppliers) {
+    List<T> instances = typeSuppliers.map(this::instantiate).collect(Collectors.toList());
+    AnnotationAwareOrderComparator.sort(instances);
+    return Collections.unmodifiableList(instances);
+  }
+
+  private T instantiate(TypeSupplier typeSupplier) {
+    try {
+      Class<?> type = typeSupplier.get();
+      Assert.isAssignable(this.type, type);
+      return instantiate(type);
+    }
+    catch (Throwable ex) {
+      throw new IllegalArgumentException(
+              "Unable to instantiate " + this.type.getName() + " [" + typeSupplier.getName() + "]", ex);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private T instantiate(Class<?> type) throws Exception {
+    Constructor<?>[] constructors = type.getDeclaredConstructors();
+    Arrays.sort(constructors, CONSTRUCTOR_COMPARATOR);
+    for (Constructor<?> constructor : constructors) {
+      Object[] args = getArgs(constructor.getParameterTypes());
+      if (args != null) {
+        ReflectionUtils.makeAccessible(constructor);
+        return (T) constructor.newInstance(args);
+      }
+    }
+    throw new IllegalAccessException("Unable to find suitable constructor");
+  }
+
+  private Object[] getArgs(Class<?>[] parameterTypes) {
+    Object[] args = new Object[parameterTypes.length];
+    for (int i = 0; i < parameterTypes.length; i++) {
+      Function<Class<?>, Object> parameter = getAvailableParameter(parameterTypes[i]);
+      if (parameter == null) {
+        return null;
+      }
+      args[i] = parameter.apply(this.type);
+    }
+    return args;
+  }
+
+  private Function<Class<?>, Object> getAvailableParameter(Class<?> parameterType) {
+    for (Map.Entry<Class<?>, Function<Class<?>, Object>> entry : this.availableParameters.entrySet()) {
+      if (entry.getKey().isAssignableFrom(parameterType)) {
+        return entry.getValue();
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Callback used to register available parameters.
+   */
+  public interface AvailableParameters {
+
+    /**
+     * Add a parameter with an instance value.
+     *
+     * @param type the parameter type
+     * @param instance the instance that should be injected
+     */
+    void add(Class<?> type, Object instance);
+
+    /**
+     * Add a parameter with an instance factory.
+     *
+     * @param type the parameter type
+     * @param factory the factory used to create the instance that should be injected
+     */
+    void add(Class<?> type, Function<Class<?>, Object> factory);
+
+  }
+
+  /**
+   * {@link Supplier} that provides a class type.
+   */
+  private abstract static class TypeSupplier {
+
+    abstract String getName();
+
+    abstract Class<?> get() throws ClassNotFoundException;
+
+    static TypeSupplier forName(ClassLoader classLoader, String name) {
+      return new TypeSupplier() {
+
+        @Override
+        public String getName() {
+          return name;
+        }
+
+        @Override
+        public Class<?> get() throws ClassNotFoundException {
+          return ClassUtils.forName(name, classLoader);
+        }
+
+      };
+    }
+
+    static TypeSupplier forType(Class<?> type) {
+      return new TypeSupplier() {
+
+        @Override
+        public String getName() {
+          return type.getName();
+        }
+
+        @Override
+        public Class<?> get() {
+          return type;
+        }
+
+      };
+    }
+
+  }
+
+}
---
+++ src/test/java/cn/taketoday/util/InstantiatorTests.java
@@ -0,0 +1,188 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import cn.taketoday.core.Order;
+import cn.taketoday.core.Ordered;
+import cn.taketoday.core.OverridingClassLoader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/2/18 10:55
+ */
+class InstantiatorTests {
+
+  private ParamA paramA = new ParamA();
+
+  private ParamB paramB = new ParamB();
+
+  private ParamC paramC;
+
+  @Test
+  void instantiateWhenOnlyDefaultConstructorCreatesInstance() {
+    WithDefaultConstructor instance = createInstance(WithDefaultConstructor.class);
+    assertThat(instance).isInstanceOf(WithDefaultConstructor.class);
+  }
+
+  @Test
+  void instantiateWhenMultipleConstructorPicksMostArguments() {
+    WithMultipleConstructors instance = createInstance(WithMultipleConstructors.class);
+    assertThat(instance).isInstanceOf(WithMultipleConstructors.class);
+  }
+
+  @Test
+  void instantiateWhenAdditionalConstructorPicksMostSuitable() {
+    WithAdditionalConstructor instance = createInstance(WithAdditionalConstructor.class);
+    assertThat(instance).isInstanceOf(WithAdditionalConstructor.class);
+
+  }
+
+  @Test
+  void instantiateOrdersInstances() {
+    List<Object> instances = createInstantiator(Object.class).instantiate(
+            Arrays.asList(WithMultipleConstructors.class.getName(), WithAdditionalConstructor.class.getName()));
+    assertThat(instances).hasSize(2);
+    assertThat(instances.get(0)).isInstanceOf(WithAdditionalConstructor.class);
+    assertThat(instances.get(1)).isInstanceOf(WithMultipleConstructors.class);
+  }
+
+  @Test
+  void instantiateWithFactory() {
+    assertThat(this.paramC).isNull();
+    WithFactory instance = createInstance(WithFactory.class);
+    assertThat(instance.getParamC()).isEqualTo(this.paramC);
+  }
+
+  @Test
+  void instantiateTypesCreatesInstance() {
+    WithDefaultConstructor instance = createInstantiator(WithDefaultConstructor.class)
+            .instantiateTypes(Collections.singleton(WithDefaultConstructor.class)).get(0);
+    assertThat(instance).isInstanceOf(WithDefaultConstructor.class);
+  }
+
+  @Test
+  void instantiateWithClassLoaderCreatesInstance() {
+    OverridingClassLoader classLoader = new OverridingClassLoader(getClass().getClassLoader()) {
+
+      @Override
+      protected boolean isEligibleForOverriding(String className) {
+        return super.isEligibleForOverriding(className)
+                && className.equals(WithDefaultConstructorSubclass.class.getName());
+      }
+
+    };
+    WithDefaultConstructor instance = createInstantiator(WithDefaultConstructor.class)
+            .instantiate(classLoader, Collections.singleton(WithDefaultConstructorSubclass.class.getName())).get(0);
+    assertThat(instance.getClass().getClassLoader()).isSameAs(classLoader);
+  }
+
+  @Test
+  void createWhenWrongTypeThrowsException() {
+    assertThatIllegalArgumentException()
+            .isThrownBy(() -> createInstantiator(WithDefaultConstructor.class)
+                    .instantiate(Collections.singleton(WithAdditionalConstructor.class.getName())))
+            .withMessageContaining("Unable to instantiate");
+  }
+
+  private <T> T createInstance(Class<T> type) {
+    return createInstantiator(type).instantiate(Collections.singleton(type.getName())).get(0);
+  }
+
+  private <T> Instantiator<T> createInstantiator(Class<T> type) {
+    return new Instantiator<>(type, (availableParameters) -> {
+      availableParameters.add(ParamA.class, this.paramA);
+      availableParameters.add(ParamB.class, this.paramB);
+      availableParameters.add(ParamC.class, ParamC::new);
+    });
+  }
+
+  static class WithDefaultConstructorSubclass extends WithDefaultConstructor {
+
+  }
+
+  @Order(Ordered.LOWEST_PRECEDENCE)
+  static class WithMultipleConstructors {
+
+    WithMultipleConstructors() {
+      throw new IllegalStateException();
+    }
+
+    WithMultipleConstructors(ParamA paramA) {
+      throw new IllegalStateException();
+    }
+
+    WithMultipleConstructors(ParamA paramA, ParamB paramB) {
+    }
+
+  }
+
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  static class WithAdditionalConstructor {
+
+    WithAdditionalConstructor(ParamA paramA, ParamB paramB) {
+    }
+
+    WithAdditionalConstructor(ParamA paramA, ParamB paramB, String extra) {
+      throw new IllegalStateException();
+    }
+
+  }
+
+  static class WithFactory {
+
+    private ParamC paramC;
+
+    WithFactory(ParamC paramC) {
+      this.paramC = paramC;
+    }
+
+    ParamC getParamC() {
+      return this.paramC;
+    }
+
+  }
+
+  class ParamA {
+
+  }
+
+  class ParamB {
+
+  }
+
+  class ParamC {
+
+    ParamC(Class<?> type) {
+      InstantiatorTests.this.paramC = this;
+    }
+
+  }
+
+}
