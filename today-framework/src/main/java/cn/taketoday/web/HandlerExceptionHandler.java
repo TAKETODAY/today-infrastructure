3334d29ffcf7ab60a35d49ff5b455661015e80bb
Harry Yang <2228592280@qq.com>
2022-06-11 13:25:22 +0800

    :zap: HandlerExceptionHandler#find

--- today-framework/src/main/java/cn/taketoday/web/HandlerExceptionHandler.java
+++ today-framework/src/main/java/cn/taketoday/web/HandlerExceptionHandler.java
@@ -19,7 +19,16 @@
  */
 package cn.taketoday.web;
 
+import java.util.ArrayList;
+
+import cn.taketoday.beans.factory.BeanFactoryUtils;
+import cn.taketoday.context.ApplicationContext;
+import cn.taketoday.core.annotation.AnnotationAwareOrderComparator;
 import cn.taketoday.lang.Nullable;
+import cn.taketoday.util.CollectionUtils;
+import cn.taketoday.web.handler.CompositeHandlerExceptionHandler;
+import cn.taketoday.web.handler.SimpleHandlerExceptionHandler;
+import cn.taketoday.web.handler.method.ExceptionHandlerAnnotationExceptionHandler;
 
 /**
  * Handle Exception from handler
@@ -28,6 +37,13 @@ import cn.taketoday.lang.Nullable;
  * @since 2020-03-29 20:52
  */
 public interface HandlerExceptionHandler {
+  /**
+   * Well-known name for the HandlerExceptionHandler object in the bean factory for this namespace.
+   * Only used when "detectAllHandlerExceptionHandlers" is turned off.
+   *
+   * @see cn.taketoday.web.handler.DispatcherHandler#setDetectAllHandlerExceptionHandlers(boolean)
+   */
+  String HANDLER_EXCEPTION_HANDLER_BEAN_NAME = "handlerExceptionHandler";
 
   /**
    * This value indicates that the handler did not return a value, or the result
@@ -47,5 +63,45 @@ public interface HandlerExceptionHandler {
   @Nullable
   Object handleException(RequestContext context, Throwable exception, @Nullable Object handler)
           throws Exception;
+
+  // static factory method
+
+  static HandlerExceptionHandler find(ApplicationContext context) {
+    return find(context, true);
+  }
+
+  static HandlerExceptionHandler find(ApplicationContext context, boolean detectAllHandlerExceptionHandlers) {
+    if (detectAllHandlerExceptionHandlers) {
+      // Find all HandlerAdapters in the ApplicationContext, including ancestor contexts.
+      var matchingBeans = BeanFactoryUtils.beansOfTypeIncludingAncestors(
+              context, HandlerExceptionHandler.class, true, false);
+      if (!matchingBeans.isEmpty()) {
+        var handlers = new ArrayList<>(matchingBeans.values());
+        // at least one exception-handler
+        if (handlers.size() == 1) {
+          return handlers.get(0);
+        }
+        else {
+          // We keep HandlerExceptionHandlers in sorted order.
+          AnnotationAwareOrderComparator.sort(handlers);
+          return new CompositeHandlerExceptionHandler(handlers);
+        }
+      }
+    }
+    else {
+      var exceptionHandler = BeanFactoryUtils.find(
+              context, HANDLER_EXCEPTION_HANDLER_BEAN_NAME, HandlerExceptionHandler.class);
+      if (exceptionHandler != null) {
+        return exceptionHandler;
+      }
+    }
+
+    var exceptionHandler = new ExceptionHandlerAnnotationExceptionHandler();
+    exceptionHandler.setApplicationContext(context);
+    exceptionHandler.afterPropertiesSet();
+    return new CompositeHandlerExceptionHandler(CollectionUtils.newArrayList(
+            exceptionHandler, new SimpleHandlerExceptionHandler()
+    ));
+  }
 
 }
