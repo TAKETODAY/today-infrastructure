010ab2dbb67459db74886aff23a17001dc18885b
Harry Yang <2228592280@qq.com>
2022-07-22 11:00:59 +0800

    :zap: Polish

--- today-framework/src/test/java/cn/taketoday/jdbc/datasource/DataSourceUtilsTests.java
+++ today-framework/src/test/java/cn/taketoday/jdbc/datasource/DataSourceUtilsTests.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 
 import cn.taketoday.jdbc.CannotGetJdbcConnectionException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,25 +19,27 @@ import static org.mockito.Mockito.when;
  */
 class DataSourceUtilsTests {
 
-	@Test
-	void testConnectionNotAcquiredExceptionIsPropagated() throws SQLException {
-		DataSource dataSource = mock(DataSource.class);
-		when(dataSource.getConnection()).thenReturn(null);
-		assertThatThrownBy(() -> DataSourceUtils.getConnection(dataSource))
-				.isInstanceOf(CannotGetJdbcConnectionException.class)
-				.hasMessageStartingWith("Failed to obtain JDBC Connection")
-				.hasCauseInstanceOf(IllegalStateException.class);
-	}
+  @Test
+  void testConnectionNotAcquiredExceptionIsPropagated() throws SQLException {
+    DataSource dataSource = mock(DataSource.class);
+    when(dataSource.getConnection()).thenReturn(null);
+    assertThatThrownBy(() -> DataSourceUtils.getConnection(dataSource))
+            .isInstanceOf(CannotGetJdbcConnectionException.class)
+            .hasMessageStartingWith("Failed to obtain JDBC Connection")
+            .hasCauseInstanceOf(IllegalStateException.class);
+  }
 
-	@Test
-	void testConnectionSQLExceptionIsPropagated() throws SQLException {
-		DataSource dataSource = mock(DataSource.class);
-		when(dataSource.getConnection()).thenThrow(new SQLException("my dummy exception"));
-		assertThatThrownBy(() -> DataSourceUtils.getConnection(dataSource))
-				.isInstanceOf(CannotGetJdbcConnectionException.class)
-				.hasMessageStartingWith("Failed to obtain JDBC Connection")
-				.cause().isInstanceOf(SQLException.class)
-				.hasMessage("my dummy exception");
-	}
+  @Test
+  void testConnectionSQLExceptionIsPropagated() throws SQLException {
+    DataSource dataSource = mock(DataSource.class);
+    when(dataSource.getConnection()).thenThrow(new SQLException("my dummy exception"));
+    assertThatThrownBy(() -> DataSourceUtils.getConnection(dataSource))
+            .isInstanceOf(CannotGetJdbcConnectionException.class)
+            .hasMessageStartingWith("Failed to obtain JDBC Connection")
+            .satisfies(e -> {
+              assertThat(e).isInstanceOf(SQLException.class);
+            })
+            .hasMessage("my dummy exception");
+  }
 
 }
