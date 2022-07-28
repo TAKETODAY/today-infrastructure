986570beb2d6517d644dc854949ee7eeb903cb40
Harry Yang <2228592280@qq.com>
2022-07-28 13:05:02 +0800

    :bug: 优化 xml 工具

--- today-core/src/main/java/cn/taketoday/util/xml/package-info.java
+++ today-core/src/main/java/cn/taketoday/util/xml/package-info.java
@@ -20,7 +20,7 @@
 
 /**
  * Miscellaneous utility classes for XML parsing and transformation,
- * such as error handlers that log warnings via Commons Logging.
+ * such as error handlers that log warnings via Logging.
  */
 @NonNullApi
 @NonNullFields
