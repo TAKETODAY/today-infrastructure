eb617b350bcc6e83c70fa2f3333cb12a7fddbcdc
Harry Yang <2228592280@qq.com>
2022-08-01 12:45:01 +0800

    :art: DataSourceUtils

--- today-jdbc/src/main/java/cn/taketoday/jdbc/datasource/DataSourceUtils.java
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/datasource/DataSourceUtils.java
@@ -406,7 +406,7 @@ public abstract class DataSourceUtils {
    * @see SmartDataSource#shouldClose(Connection)
    */
   public static void doCloseConnection(Connection con, @Nullable DataSource dataSource) throws SQLException {
-    if (!(dataSource instanceof SmartDataSource) || ((SmartDataSource) dataSource).shouldClose(con)) {
+    if (!(dataSource instanceof SmartDataSource smartDataSource) || smartDataSource.shouldClose(con)) {
       con.close();
     }
   }
@@ -442,8 +442,8 @@ public abstract class DataSourceUtils {
    */
   public static Connection getTargetConnection(Connection con) {
     Connection conToUse = con;
-    while (conToUse instanceof ConnectionProxy) {
-      conToUse = ((ConnectionProxy) conToUse).getTargetConnection();
+    while (conToUse instanceof ConnectionProxy connectionProxy) {
+      conToUse = connectionProxy.getTargetConnection();
     }
     return conToUse;
   }
@@ -460,9 +460,9 @@ public abstract class DataSourceUtils {
   private static int getConnectionSynchronizationOrder(DataSource dataSource) {
     int order = CONNECTION_SYNCHRONIZATION_ORDER;
     DataSource currDs = dataSource;
-    while (currDs instanceof DelegatingDataSource) {
+    while (currDs instanceof DelegatingDataSource delegatingDataSource) {
       order--;
-      currDs = ((DelegatingDataSource) currDs).getTargetDataSource();
+      currDs = delegatingDataSource.getTargetDataSource();
     }
     return order;
   }
