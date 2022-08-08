bec32211cbb8e1103bff555e65cdff5df00f8d73
Harry Yang <2228592280@qq.com>
2022-08-08 21:38:41 +0800

    :green_heart: DefaultCacheAwareContextLoaderDelegate#loadContextInternal

--- today-test/src/main/java/cn/taketoday/test/context/cache/DefaultCacheAwareContextLoaderDelegate.java
+++ today-test/src/main/java/cn/taketoday/test/context/cache/DefaultCacheAwareContextLoaderDelegate.java
@@ -97,19 +97,14 @@ public class DefaultCacheAwareContextLoaderDelegate implements CacheAwareContext
     Assert.notNull(contextLoader, "Cannot load an ApplicationContext with a NULL 'contextLoader'. " +
             "Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
 
-    ApplicationContext applicationContext;
-
     if (contextLoader instanceof SmartContextLoader smartContextLoader) {
-      applicationContext = smartContextLoader.loadContext(mergedContextConfiguration);
-    }
-    else {
-      String[] locations = mergedContextConfiguration.getLocations();
-      Assert.notNull(locations, "Cannot load an ApplicationContext with a NULL 'locations' array. " +
-              "Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
-      applicationContext = contextLoader.loadContext(locations);
+      return smartContextLoader.loadContext(mergedContextConfiguration);
     }
 
-    return applicationContext;
+    String[] locations = mergedContextConfiguration.getLocations();
+    Assert.notNull(locations, "Cannot load an ApplicationContext with a NULL 'locations' array. " +
+            "Consider annotating your test class with @ContextConfiguration or @ContextHierarchy.");
+    return contextLoader.loadContext(locations);
   }
 
   @Override
