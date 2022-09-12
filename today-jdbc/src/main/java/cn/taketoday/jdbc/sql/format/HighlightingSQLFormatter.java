9145786738dc3dfeded3b6c98d7e4a3691c620cb
Harry Yang <2228592280@qq.com>
2022-09-12 22:27:47 +0800

    :sparkles: 添加 SQLFormatter

---
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/sql/format/HighlightingSQLFormatter.java
@@ -0,0 +1,144 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+
+package cn.taketoday.jdbc.sql.format;
+
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * Performs basic syntax highlighting for SQL using ANSI escape codes.
+ *
+ * @author Gavin King
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/9/12 19:20
+ */
+public final class HighlightingSQLFormatter implements SQLFormatter {
+
+  private static final Set<String> KEYWORDS = Set.of(
+          "ADD", "ALL", "ALLOCATE", "ALTER", "AND", "ANY", "ARE", "ARRAY", "AS", "ASENSITIVE",
+          "ASYMMETRIC", "AT", "ATOMIC", "AUTHORIZATION", "BEGIN", "BETWEEN", "BIGINT",
+          "BLOB", "BINARY", "BOTH", "BY", "CALL", "CALLED", "CASCADED", "CASE", "CAST",
+          "CHAR", "CHARACTER", "CHECK", "CLOB", "CLOSE", "COLLATE", "COLUMN", "COMMIT",
+          "CONDITION", "CONNECT", "CONSTRAINT", "CONTINUE", "CORRESPONDING", "CREATE",
+          "CROSS", "CUBE", "CURRENT", "CURRENT_DATE", "CURRENT_PATH", "CURRENT_ROLE",
+          "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "CYCLE", "DATE",
+          "DAY", "DEALLOCATE", "DEC", "DECIMAL", "DECLARE", "DEFAULT", "DELETE", "DEREF",
+          "DESCRIBE", "DETERMINISTIC", "DISCONNECT", "DISTINCT", "DO", "DOUBLE", "DROP",
+          "DYNAMIC", "EACH", "ELEMENT", "ELSE", "ELSIF", "END", "ESCAPE", "EXCEPT", "EXEC",
+          "EXECUTE", "EXISTS", "EXIT", "EXTERNAL", "FALSE", "FETCH", "FILTER", "FLOAT", "FOR",
+          "FOREIGN", "FREE", "FROM", "FULL", "FUNCTION", "GET", "GLOBAL", "GRANT", "GROUP",
+          "GROUPING", "HANDLER", "HAVING", "HOLD", "HOUR", "IDENTITY", "IF", "IMMEDIATE", "IN",
+          "INDICATOR", "INNER", "INOUT", "INPUT", "INSENSITIVE", "INSERT", "INT", "INTEGER",
+          "INTERSECT", "INTERVAL", "INTO", "IS", "ITERATE", "JOIN", "LANGUAGE", "LARGE", "LATERAL",
+          "LEADING", "LEAVE", "LEFT", "LIKE", "LOCAL", "LOCALTIME", "LOCALTIMESTAMP", "LOOP",
+          "MATCH", "MEMBER", "MERGE", "METHOD", "MINUTE", "MODIFIES", "MODULE", "MONTH", "MULTISET",
+          "NATIONAL", "NATURAL", "NCHAR", "NCLOB", "NEW", "NO", "NONE", "NOT", "NULL", "NUMERIC",
+          "OF", "OLD", "ON", "ONLY", "OPEN", "OR", "ORDER", "OUT", "OUTER", "OUTPUT", "OVER", "OVERLAPS",
+          "PARAMETER", "PARTITION", "PRECISION", "PREPARE", "PRIMARY", "PROCEDURE", "RANGE", "READS",
+          "REAL", "RECURSIVE", "REF", "REFERENCES", "REFERENCING", "RELEASE", "REPEAT", "RESIGNAL",
+          "RESULT", "RETURN", "RETURNS", "REVOKE", "RIGHT", "ROLLBACK", "ROLLUP", "ROW", "ROWS",
+          "SAVEPOINT", "SCROLL", "SEARCH", "SECOND", "SELECT", "SENSITIVE", "SESSION_USE", "SET", "SIGNAL",
+          "SIMILAR", "SMALLINT", "SOME", "SPECIFIC", "SPECIFICTYPE", "SQL", "SQLEXCEPTION", "SQLSTATE",
+          "SQLWARNING", "START", "STATIC", "SUBMULTISET", "SYMMETRIC", "SYSTEM", "SYSTEM_USER", "TABLE",
+          "TABLESAMPLE", "THEN", "TIME", "TIMESTAMP", "TIMEZONE_HOUR", "TIMEZONE_MINUTE", "TO",
+          "TRAILING", "TRANSLATION", "TREAT", "TRIGGER", "TRUE", "UNDO", "UNION", "UNIQUE", "UNKNOWN", "UNNEST",
+          "UNTIL", "UPDATE", "USER", "USING", "VALUE", "VALUES", "VARCHAR", "VARYING", "WHEN", "WHENEVER",
+          "WHERE", "WHILE", "WINDOW", "WITH", "WITHIN", "WITHOUT", "YEAR", "KEY", "SEQUENCE", "CASCADE", "INCREMENT"
+  );
+
+  public static final SQLFormatter INSTANCE = new HighlightingSQLFormatter(
+          "34", // blue
+          "36", // cyan
+          "32"
+  );
+  private static final String SYMBOLS_AND_WS = "=><!+-*/()',.|&`\"?" + WHITESPACE;
+
+  private final String keywordEscape;
+  private final String stringEscape;
+  private final String quotedEscape;
+  private final String normalEscape;
+
+  /**
+   * @param keywordCode the ANSI escape code to use for highlighting SQL keywords
+   * @param stringCode the ANSI escape code to use for highlighting SQL strings
+   */
+  public HighlightingSQLFormatter(String keywordCode, String stringCode, String quotedCode) {
+    this.normalEscape = escape("0");
+    this.stringEscape = escape(stringCode);
+    this.quotedEscape = escape(quotedCode);
+    this.keywordEscape = escape(keywordCode);
+  }
+
+  private static String escape(String code) {
+    return "\u001b[" + code + "m";
+  }
+
+  @Override
+  public String format(String sql) {
+    StringBuilder result = new StringBuilder();
+    boolean inString = false;
+    boolean inQuoted = false;
+
+    StringTokenizer tokenizer = new StringTokenizer(sql, SYMBOLS_AND_WS, true);
+    while (tokenizer.hasMoreTokens()) {
+      String token = tokenizer.nextToken();
+      switch (token) {
+        case "\"":
+        case "`": // for MySQL
+          if (inString) {
+            result.append(token);
+          }
+          else if (inQuoted) {
+            inQuoted = false;
+            result.append(token).append(normalEscape);
+          }
+          else {
+            inQuoted = true;
+            result.append(quotedEscape).append(token);
+          }
+          break;
+        case "'":
+          if (inQuoted) {
+            result.append('\'');
+          }
+          else if (inString) {
+            inString = false;
+            result.append('\'').append(normalEscape);
+          }
+          else {
+            inString = true;
+            result.append(stringEscape).append('\'');
+          }
+          break;
+        default:
+          if (KEYWORDS.contains(token.toUpperCase())) {
+            result.append(keywordEscape).append(token).append(normalEscape);
+          }
+          else {
+            result.append(token);
+          }
+      }
+    }
+    return result.toString();
+  }
+
+}
---
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/sql/format/SqlStatementLogger.java
@@ -0,0 +1,178 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.jdbc.sql.format;
+
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+
+import cn.taketoday.logging.Logger;
+import cn.taketoday.logging.LoggerFactory;
+
+/**
+ * Centralize logging for SQL statements.
+ *
+ * @author Steve Ebersole
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2022/9/12 19:19
+ */
+public class SqlStatementLogger {
+  private static final Logger log = LoggerFactory.getLogger("today.infra.SQL");
+  private static final Logger slowLogger = LoggerFactory.getLogger("today.infra.SQL_SLOW");
+
+  private final boolean format;
+  private final boolean logToStdout;
+  private final boolean highlight;
+
+  /**
+   * Configuration value that indicates slow query. (In milliseconds) 0 - disabled.
+   */
+  private final long logSlowQuery;
+
+  /**
+   * Constructs a new SqlStatementLogger instance.
+   */
+  public SqlStatementLogger() {
+    this(false, false, false);
+  }
+
+  /**
+   * Constructs a new SqlStatementLogger instance.
+   *
+   * @param logToStdout Should we log to STDOUT in addition to our internal logger.
+   * @param format Should we format the statements in the console and log
+   */
+  public SqlStatementLogger(boolean logToStdout, boolean format) {
+    this(logToStdout, format, false);
+  }
+
+  /**
+   * Constructs a new SqlStatementLogger instance.
+   *
+   * @param logToStdout Should we log to STDOUT in addition to our internal logger.
+   * @param format Should we format the statements in the console and log
+   * @param highlight Should we highlight the statements in the console
+   */
+  public SqlStatementLogger(boolean logToStdout, boolean format, boolean highlight) {
+    this(logToStdout, format, highlight, 0);
+  }
+
+  /**
+   * Constructs a new SqlStatementLogger instance.
+   *
+   * @param logToStdout Should we log to STDOUT in addition to our internal logger.
+   * @param format Should we format the statements in the console and log
+   * @param highlight Should we highlight the statements in the console
+   * @param logSlowQuery Should we logs query which executed slower than specified milliseconds. 0 - disabled.
+   */
+  public SqlStatementLogger(boolean logToStdout, boolean format, boolean highlight, long logSlowQuery) {
+    this.logToStdout = logToStdout;
+    this.format = format;
+    this.highlight = highlight;
+    this.logSlowQuery = logSlowQuery;
+  }
+
+  /**
+   * Are we currently logging to stdout?
+   *
+   * @return True if we are currently logging to stdout; false otherwise.
+   */
+  public boolean isLogToStdout() {
+    return logToStdout;
+  }
+
+  public boolean isFormat() {
+    return format;
+  }
+
+  public long getLogSlowQuery() {
+    return logSlowQuery;
+  }
+
+  /**
+   * Log a SQL statement string.
+   *
+   * @param statement The SQL statement.
+   */
+  public void logStatement(String statement) {
+    // for now just assume a DML log for formatting
+    logStatement(statement, FormatStyle.BASIC.formatter);
+  }
+
+  /**
+   * Log a SQL statement string using the specified formatter
+   *
+   * @param statement The SQL statement.
+   * @param formatter The formatter to use.
+   */
+  public void logStatement(String statement, SQLFormatter formatter) {
+    if (logToStdout || log.isDebugEnabled()) {
+      if (format) {
+        statement = formatter.format(statement);
+      }
+      if (highlight) {
+        statement = FormatStyle.HIGHLIGHT.formatter.format(statement);
+      }
+    }
+    log.debug(statement);
+    if (logToStdout) {
+      String prefix = highlight ? "\u001b[35m[Infra]\u001b[0m " : "Infra: ";
+      System.out.println(prefix + statement);
+    }
+  }
+
+  /**
+   * Log a slow SQL query
+   *
+   * @param statement SQL statement.
+   * @param startTimeNanos Start time in nanoseconds.
+   */
+  public void logSlowQuery(Statement statement, long startTimeNanos) {
+    if (logSlowQuery < 1) {
+      return;
+    }
+    logSlowQuery(statement.toString(), startTimeNanos);
+  }
+
+  /**
+   * Log a slow SQL query
+   *
+   * @param sql The SQL query.
+   * @param startTimeNanos Start time in nanoseconds.
+   */
+  public void logSlowQuery(String sql, long startTimeNanos) {
+    if (logSlowQuery < 1) {
+      return;
+    }
+    if (startTimeNanos <= 0) {
+      throw new IllegalArgumentException("startTimeNanos [" + startTimeNanos + "] should be greater than 0!");
+    }
+
+    long queryExecutionMillis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTimeNanos);
+
+    if (queryExecutionMillis > logSlowQuery) {
+      String logData = "SlowQuery: " + queryExecutionMillis + " milliseconds. SQL: '" + sql + "'";
+      slowLogger.info(logData);
+      if (logToStdout) {
+        System.out.println(logData);
+      }
+    }
+  }
+}
