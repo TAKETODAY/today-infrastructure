0c087cd6d2e57ca2857e6c890d09a343bcee2a8a
Harry Yang <2228592280@qq.com>
2022-09-14 19:47:48 +0800

    :zap: Decode quoted pairs in ContentDisposition
    This commit makes sure that quoted pairs, as used in Content-Disposition
    header file names (i.e. \" and \\), are properly decoded, whereas before
    they were stored as is.

--- today-web/src/test/java/cn/taketoday/http/ContentDispositionTests.java
+++ today-web/src/test/java/cn/taketoday/http/ContentDispositionTests.java
@@ -40,7 +40,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
  */
 public class ContentDispositionTests {
 
-  private static DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+  private static final DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
 
   @Test
   @SuppressWarnings("deprecation")
@@ -96,9 +96,42 @@ public class ContentDispositionTests {
   }
 
   @Test
+  void parseBase64EncodedFilenameMultipleSegments() {
+    String input =
+            "attachment; filename=\"=?utf-8?B?U3ByaW5n5qGG5p625Li65Z+65LqOSmF2YeeahOeOsOS7o+S8geS4muW6lA==?= " +
+                    "=?utf-8?B?55So56iL5bqP5o+Q5L6b5LqG5YWo6Z2i55qE57yW56iL5ZKM6YWN572u5qih?= " +
+                    "=?utf-8?B?5Z6LLnR4dA==?=\"";
+    assertThat(parse(input).getFilename()).isEqualTo("Spring框架为基于Java的现代企业应用程序提供了全面的编程和配置模型.txt");
+  }
+
+  @Test
     // gh-26463
   void parseBase64EncodedShiftJISFilename() {
     String input = "attachment; filename=\"=?SHIFT_JIS?B?k/qWe4zqLmNzdg==?=\"";
+    assertThat(parse(input).getFilename()).isEqualTo("日本語.csv");
+  }
+
+  @Test
+  void parseQuotedPrintableFilename() {
+    String input = "attachment; filename=\"=?UTF-8?Q?=E6=97=A5=E6=9C=AC=E8=AA=9E.csv?=\"";
+    assertThat(parse(input).getFilename()).isEqualTo("日本語.csv");
+  }
+
+  @Test
+  void parseQuotedPrintableFilenameMultipleSegments() {
+    String input =
+            "attachment; filename=\"=?utf-8?Q?Spring=E6=A1=86=E6=9E=B6=E4=B8=BA=E5=9F=BA=E4=BA=8E?=" +
+                    "=?utf-8?Q?Java=E7=9A=84=E7=8E=B0=E4=BB=A3=E4=BC=81=E4=B8=9A=E5=BA=94?=" +
+                    "=?utf-8?Q?=E7=94=A8=E7=A8=8B=E5=BA=8F=E6=8F=90=E4=BE=9B=E4=BA=86=E5=85=A8?=" +
+                    "=?utf-8?Q?=E9=9D=A2=E7=9A=84=E7=BC=96=E7=A8=8B=E5=92=8C=E9=85=8D=E7=BD=AE?=" +
+                    "=?utf-8?Q?=E6=A8=A1=E5=9E=8B.txt?=\"";
+    assertThat(parse(input).getFilename()).isEqualTo("Spring框架为基于Java的现代企业应用程序提供了全面的编程和配置模型.txt");
+
+  }
+
+  @Test
+  void parseQuotedPrintableShiftJISFilename() {
+    String input = "attachment; filename=\"=?SHIFT_JIS?Q?=93=FA=96{=8C=EA.csv?=\"";
     assertThat(parse(input).getFilename()).isEqualTo("日本語.csv");
   }
 
@@ -126,25 +159,22 @@ public class ContentDispositionTests {
             .isThrownBy(() -> parse("form-data; name=\"name\"; filename*=UTF-8''%A.txt"));
   }
 
-  @Test // gh-23077
-  @SuppressWarnings("deprecation")
-  void parseWithEscapedQuote() {
-    BiConsumer<String, String> tester = (description, filename) ->
-            assertThat(parse("form-data; name=\"file\"; filename=\"" + filename + "\"; size=123"))
-                    .as(description)
-                    .isEqualTo(ContentDisposition.formData().name("file").filename(filename).size(123L).build());
+  @Test
+  void parseBackslash() {
+    String s = "form-data; name=\"foo\"; filename=\"foo\\\\bar \\\"baz\\\" qux \\\\\\\" quux.txt\"";
+    ContentDisposition cd = ContentDisposition.parse(
+            s);
+    assertThat(cd.getName()).isEqualTo("foo");
+    assertThat(cd.getFilename()).isEqualTo("foo\\bar \"baz\" qux \\\" quux.txt");
+    assertThat(cd.toString()).isEqualTo(s);
+  }
 
-    tester.accept("Escaped quotes should be ignored",
-            "\\\"The Twilight Zone\\\".txt");
-
-    tester.accept("Escaped quotes preceded by escaped backslashes should be ignored",
-            "\\\\\\\"The Twilight Zone\\\\\\\".txt");
-
-    tester.accept("Escaped backslashes should not suppress quote",
-            "The Twilight Zone \\\\");
-
-    tester.accept("Escaped backslashes should not suppress quote",
-            "The Twilight Zone \\\\\\\\");
+  @Test
+  void parseBackslashInLastPosition() {
+    ContentDisposition cd = ContentDisposition.parse("form-data; name=\"foo\"; filename=\"bar\\\"");
+    assertThat(cd.getName()).isEqualTo("foo");
+    assertThat(cd.getFilename()).isEqualTo("bar\\");
+    assertThat(cd.toString()).isEqualTo("form-data; name=\"foo\"; filename=\"bar\\\\\"");
   }
 
   @Test
@@ -258,26 +288,26 @@ public class ContentDispositionTests {
     };
 
     String filename = "\"foo.txt";
-    tester.accept(filename, "\\" + filename);
+    tester.accept(filename, "\\\"foo.txt");
 
     filename = "\\\"foo.txt";
-    tester.accept(filename, filename);
+    tester.accept(filename, "\\\\\\\"foo.txt");
 
     filename = "\\\\\"foo.txt";
-    tester.accept(filename, "\\" + filename);
+    tester.accept(filename, "\\\\\\\\\\\"foo.txt");
 
     filename = "\\\\\\\"foo.txt";
-    tester.accept(filename, filename);
+    tester.accept(filename, "\\\\\\\\\\\\\\\"foo.txt");
 
     filename = "\\\\\\\\\"foo.txt";
-    tester.accept(filename, "\\" + filename);
+    tester.accept(filename, "\\\\\\\\\\\\\\\\\\\"foo.txt");
 
     tester.accept("\"\"foo.txt", "\\\"\\\"foo.txt");
     tester.accept("\"\"\"foo.txt", "\\\"\\\"\\\"foo.txt");
 
-    tester.accept("foo.txt\\", "foo.txt");
-    tester.accept("foo.txt\\\\", "foo.txt\\\\");
-    tester.accept("foo.txt\\\\\\", "foo.txt\\\\");
+    tester.accept("foo.txt\\", "foo.txt\\\\");
+    tester.accept("foo.txt\\\\", "foo.txt\\\\\\\\");
+    tester.accept("foo.txt\\\\\\", "foo.txt\\\\\\\\\\\\");
   }
 
   @Test
@@ -288,6 +318,16 @@ public class ContentDispositionTests {
                     .filename("test.txt", StandardCharsets.UTF_16)
                     .build()
                     .toString());
+  }
+
+  @Test
+  void parseFormatted() {
+    ContentDisposition cd = ContentDisposition.builder("form-data")
+            .name("foo")
+            .filename("foo\\bar \"baz\" qux \\\" quux.txt").build();
+    ContentDisposition parsed = ContentDisposition.parse(cd.toString());
+    assertThat(parsed).isEqualTo(cd);
+    assertThat(parsed.toString()).isEqualTo(cd.toString());
   }
 
 }
