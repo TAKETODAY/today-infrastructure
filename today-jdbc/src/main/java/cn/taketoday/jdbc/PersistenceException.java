2fe2258c046a46671ff16f1d70dc052b7b86eb6d
Harry Yang <2228592280@qq.com>
2022-09-20 12:36:58 +0800

    :hammer: PersistenceException 继承 DataAccessException

--- today-jdbc/src/main/java/cn/taketoday/jdbc/PersistenceException.java
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/PersistenceException.java
@@ -2,14 +2,14 @@ package cn.taketoday.jdbc;
 
 import java.io.Serial;
 
-import cn.taketoday.core.NestedRuntimeException;
+import cn.taketoday.dao.DataAccessException;
 
 /**
  * Represents an exception thrown by today-jdbc.
  *
  * @author TODAY
  */
-public class PersistenceException extends NestedRuntimeException {
+public class PersistenceException extends DataAccessException {
   @Serial
   private static final long serialVersionUID = 1L;
 
