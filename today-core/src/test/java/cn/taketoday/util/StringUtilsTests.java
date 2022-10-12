bf4aec64d2508ac1316aeea78bc5950b61ed9645
Harry Yang <2228592280@qq.com>
2022-10-12 23:10:08 +0800

    :white_check_mark: StringUtilsTests

--- today-core/src/test/java/cn/taketoday/util/StringUtilsTests.java
+++ today-core/src/test/java/cn/taketoday/util/StringUtilsTests.java
@@ -41,6 +41,83 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StringUtilsTests {
 
   @Test
+  void trivial() {
+    assertThat(StringUtils.simpleMatch((String) null, "")).isFalse();
+    assertThat(StringUtils.simpleMatch("1", null)).isFalse();
+    doTest("*", "123", true);
+    doTest("123", "123", true);
+  }
+
+  @Test
+  void startsWith() {
+    doTest("get*", "getMe", true);
+    doTest("get*", "setMe", false);
+  }
+
+  @Test
+  void endsWith() {
+    doTest("*Test", "getMeTest", true);
+    doTest("*Test", "setMe", false);
+  }
+
+  @Test
+  void between() {
+    doTest("*stuff*", "getMeTest", false);
+    doTest("*stuff*", "getstuffTest", true);
+    doTest("*stuff*", "stuffTest", true);
+    doTest("*stuff*", "getstuff", true);
+    doTest("*stuff*", "stuff", true);
+  }
+
+  @Test
+  void startsEnds() {
+    doTest("on*Event", "onMyEvent", true);
+    doTest("on*Event", "onEvent", true);
+    doTest("3*3", "3", false);
+    doTest("3*3", "33", true);
+  }
+
+  @Test
+  void startsEndsBetween() {
+    doTest("12*45*78", "12345678", true);
+    doTest("12*45*78", "123456789", false);
+    doTest("12*45*78", "012345678", false);
+    doTest("12*45*78", "124578", true);
+    doTest("12*45*78", "1245457878", true);
+    doTest("3*3*3", "33", false);
+    doTest("3*3*3", "333", true);
+  }
+
+  @Test
+  void ridiculous() {
+    doTest("*1*2*3*", "0011002001010030020201030", true);
+    doTest("1*2*3*4", "10300204", false);
+    doTest("1*2*3*3", "10300203", false);
+    doTest("*1*2*3*", "123", true);
+    doTest("*1*2*3*", "132", false);
+  }
+
+  @Test
+  void patternVariants() {
+    doTest("*a", "*", false);
+    doTest("*a", "a", true);
+    doTest("*a", "b", false);
+    doTest("*a", "aa", true);
+    doTest("*a", "ba", true);
+    doTest("*a", "ab", false);
+    doTest("**a", "*", false);
+    doTest("**a", "a", true);
+    doTest("**a", "b", false);
+    doTest("**a", "aa", true);
+    doTest("**a", "ba", true);
+    doTest("**a", "ab", false);
+  }
+
+  private void doTest(String pattern, String str, boolean shouldMatch) {
+    assertThat(StringUtils.simpleMatch(pattern, str)).isEqualTo(shouldMatch);
+  }
+
+  @Test
   void test_IsEmpty() {
     assert !StringUtils.isEmpty("1234");
     assert !StringUtils.isEmpty(" ");
