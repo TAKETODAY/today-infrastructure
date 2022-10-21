fe37609a2d0f1de8bfb093b0d95f9a2362667b3d
Harry Yang <2228592280@qq.com>
2022-10-21 13:13:23 +0800

    :zap: demo/netty-app

--- today-infrastructure-samples/netty-app/src/main/java/cn/taketoday/netty/NettyDemoApplication.java
+++ today-infrastructure-samples/netty-app/src/main/java/cn/taketoday/netty/NettyDemoApplication.java
@@ -20,22 +20,102 @@
 
 package cn.taketoday.netty;
 
-import cn.taketoday.framework.Application;
+import cn.taketoday.beans.factory.annotation.Autowired;
+import cn.taketoday.context.ApplicationEventPublisher;
+import cn.taketoday.context.annotation.Configuration;
+import cn.taketoday.context.event.EventListener;
 import cn.taketoday.framework.InfraApplication;
+import cn.taketoday.framework.builder.ApplicationBuilder;
 import cn.taketoday.framework.web.netty.EnableNettyHandling;
+import cn.taketoday.web.RequestContext;
+import cn.taketoday.web.annotation.ExceptionHandler;
+import cn.taketoday.web.annotation.GET;
+import cn.taketoday.web.annotation.RestController;
+import cn.taketoday.web.annotation.RestControllerAdvice;
 import cn.taketoday.web.config.EnableWebMvc;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0 2022/10/20 13:10
  */
+@Slf4j
 @EnableWebMvc
 @InfraApplication
-@EnableNettyHandling
+@RestController
+@RestControllerAdvice
+@EnableNettyHandling(async = false)
 public class NettyDemoApplication {
 
   public static void main(String[] args) {
-    Application.run(NettyDemoApplication.class, args);
+    ApplicationBuilder.from(NettyDemoApplication.class)
+            .run(args);
+  }
+
+  @GET
+  public String home() {
+    return "Home";
+  }
+
+  @GET("/index")
+  public String index() {
+    return "Hello";
+  }
+
+  @GET("/body/{name}/{age}")
+  public Body body(String name, int age) {
+    return new Body(name, age);
+  }
+
+  @GET("/publish-event")
+  public void index(String name, @Autowired ApplicationEventPublisher publisher) {
+    publisher.publishEvent(new MyEvent(name));
+  }
+
+  @GET("/request-context")
+  public String context(RequestContext context) {
+    String requestURL = context.getRequestURL();
+    String queryString = context.getQueryString();
+    System.out.println(requestURL);
+    System.out.println(queryString);
+
+    return queryString;
+  }
+
+  @Getter
+  static class Body {
+    final String name;
+    final int age;
+
+    Body(String name, int age) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+
+  @Configuration
+  static class AppConfig {
+
+    @EventListener(MyEvent.class)
+    public void event(MyEvent event) {
+      log.info("event :{}", event);
+    }
+  }
+
+  @ToString
+  static class MyEvent {
+    final String name;
+
+    MyEvent(String name) {
+      this.name = name;
+    }
+  }
+
+  @ExceptionHandler(Throwable.class)
+  public void throwable(Throwable throwable) {
+    throwable.printStackTrace();
   }
 
 }
