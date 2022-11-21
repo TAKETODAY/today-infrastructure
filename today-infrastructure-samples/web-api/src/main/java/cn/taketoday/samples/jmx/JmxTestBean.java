ce59289a0301a1e46a43b27f5acbe4bb1eb6dd30
Harry Yang <2228592280@qq.com>
2022-11-21 21:34:23 +0800

    :sparkles: jmx samples

---
+++ today-infrastructure-samples/web-api/src/main/java/cn/taketoday/samples/jmx/JmxTestBean.java
@@ -0,0 +1,40 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.samples.jmx;
+
+public interface JmxTestBean {
+
+  int add(int x, int y);
+
+  long myOperation();
+
+  int getAge();
+
+  void setAge(int age);
+
+  void setName(String name) throws Exception;
+
+  String getName();
+
+  // used to test invalid methods that exist in the proxy interface
+  void dontExposeMe();
+
+}
