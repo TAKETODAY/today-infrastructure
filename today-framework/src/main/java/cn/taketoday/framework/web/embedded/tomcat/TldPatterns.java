4035c48c46b1c4a7fae8659a6ab5f374b7d3ff19
Harry Yang <i.take.today@gmail.com>
2023-01-06 17:47:59 +0800

    TldPatterns add websocket-client-api.jar

--- today-framework/src/main/java/cn/taketoday/framework/web/embedded/tomcat/TldPatterns.java
+++ today-framework/src/main/java/cn/taketoday/framework/web/embedded/tomcat/TldPatterns.java
@@ -1,6 +1,6 @@
 /*
  * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
@@ -116,6 +116,7 @@ final class TldPatterns {
     skipPatterns.add("tomcat-websocket.jar");
     skipPatterns.add("tools.jar");
     skipPatterns.add("websocket-api.jar");
+    skipPatterns.add("websocket-client-api.jar");
     skipPatterns.add("wsdl4j*.jar");
     skipPatterns.add("xercesImpl.jar");
     skipPatterns.add("xml-apis.jar");
