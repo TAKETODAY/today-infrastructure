a59e67c01507db10a0cb5abca447ad2a0037eab0
Harry Yang <i.take.today@gmail.com>
2023-03-09 21:59:14 +0800

    Avoid rollback after a commit failure in TransactionalOperator
    A failure to commit a reactive transaction will complete the
    transaction and clean up resources. Executing a rollback at
    that point is invalid, which causes an
    IllegalTransactionStateException that masks the cause of the
    commit failure.
    
    This change restructures TransactionalOperatorImpl and
    ReactiveTransactionSupport to avoid executing a rollback after
    a failed commit. While there, the Mono transaction handling in
    TransactionalOperator is simplified by moving it to a default
    method on the interface.

--- today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAspectSupport.java
+++ today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAspectSupport.java
@@ -1,6 +1,6 @@
 /*
  * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
+ * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
@@ -802,58 +802,44 @@ public abstract class TransactionAspectSupport implements BeanFactoryAware, Init
             InvocationCallback invocation, @Nullable TransactionAttribute txAttr, ReactiveTransactionManager rtm) {
 
       String joinpointIdentification = methodIdentification(method, targetClass, txAttr);
-      // For Mono and suspending functions not returning kotlinx.coroutines.flow.Flow
+
       if (Mono.class.isAssignableFrom(method.getReturnType())) {
+
         return TransactionContextManager.currentContext()
-                .flatMap(context -> createTransactionIfNecessary(rtm, txAttr, joinpointIdentification).flatMap(it -> {
-                  try {
-                    // Need re-wrapping until we get hold of the exception through usingWhen.
-                    return Mono.<Object, ReactiveTransactionInfo>usingWhen(Mono.just(it),
-                            txInfo -> {
-                              try {
-                                return (Mono<?>) invocation.proceedWithInvocation();
-                              }
-                              catch (Throwable ex) {
-                                return Mono.error(ex);
-                              }
-                            },
-                            this::commitTransactionAfterReturning,
-                            (txInfo, err) -> Mono.empty(),
-                            this::rollbackTransactionOnCancel
-                    ).onErrorResume(ex -> completeTransactionAfterThrowing(it, ex).then(Mono.error(ex)));
-                  }
-                  catch (Throwable ex) {
-                    // target invocation exception
-                    return completeTransactionAfterThrowing(it, ex).then(Mono.error(ex));
-                  }
-                }))
+                .flatMap(context -> Mono.<Object, ReactiveTransactionInfo>usingWhen(
+                                createTransactionIfNecessary(rtm, txAttr, joinpointIdentification),
+                                tx -> {
+                                  try {
+                                    return (Mono<?>) invocation.proceedWithInvocation();
+                                  }
+                                  catch (Throwable ex) {
+                                    return Mono.error(ex);
+                                  }
+                                },
+                                this::commitTransactionAfterReturning,
+                                this::completeTransactionAfterThrowing,
+                                this::rollbackTransactionOnCancel)
+                        .onErrorMap(this::unwrapIfResourceCleanupFailure))
                 .contextWrite(TransactionContextManager.getOrCreateContext())
                 .contextWrite(TransactionContextManager.getOrCreateContextHolder());
       }
 
       // Any other reactive type, typically a Flux
-      return this.adapter.fromPublisher(TransactionContextManager.currentContext().flatMapMany(context ->
-                      createTransactionIfNecessary(rtm, txAttr, joinpointIdentification).flatMapMany(it -> {
-                        try {
-                          // Need re-wrapping until we get hold of the exception through usingWhen.
-                          return Flux.usingWhen(Mono.just(it), txInfo -> {
-                                    try {
-                                      return this.adapter.toPublisher(invocation.proceedWithInvocation());
-                                    }
-                                    catch (Throwable ex) {
-                                      return Mono.error(ex);
-                                    }
-                                  },
-                                  this::commitTransactionAfterReturning,
-                                  (txInfo, ex) -> Mono.empty(),
-                                  this::rollbackTransactionOnCancel
-                          ).onErrorResume(ex -> completeTransactionAfterThrowing(it, ex).then(Mono.error(ex)));
-                        }
-                        catch (Throwable ex) {
-                          // target invocation exception
-                          return completeTransactionAfterThrowing(it, ex).then(Mono.error(ex));
-                        }
-                      })).contextWrite(TransactionContextManager.getOrCreateContext())
+      return this.adapter.fromPublisher(TransactionContextManager.currentContext()
+              .flatMapMany(context -> Flux.usingWhen(createTransactionIfNecessary(rtm, txAttr, joinpointIdentification),
+                              tx -> {
+                                try {
+                                  return this.adapter.toPublisher(invocation.proceedWithInvocation());
+                                }
+                                catch (Throwable ex) {
+                                  return Mono.error(ex);
+                                }
+                              },
+                              this::commitTransactionAfterReturning,
+                              this::completeTransactionAfterThrowing,
+                              this::rollbackTransactionOnCancel)
+                      .onErrorMap(this::unwrapIfResourceCleanupFailure))
+              .contextWrite(TransactionContextManager.getOrCreateContext())
               .contextWrite(TransactionContextManager.getOrCreateContextHolder()));
     }
 
@@ -926,6 +912,9 @@ public abstract class TransactionAspectSupport implements BeanFactoryAware, Init
                     if (ex2 instanceof TransactionSystemException) {
                       ((TransactionSystemException) ex2).initApplicationException(ex);
                     }
+                    else {
+                      ex2.addSuppressed(ex);
+                    }
                     return ex2;
                   }
           );
@@ -938,12 +927,30 @@ public abstract class TransactionAspectSupport implements BeanFactoryAware, Init
                     if (ex2 instanceof TransactionSystemException) {
                       ((TransactionSystemException) ex2).initApplicationException(ex);
                     }
+                    else {
+                      ex2.addSuppressed(ex);
+                    }
                     return ex2;
                   }
           );
         }
       }
       return Mono.empty();
+    }
+
+    /**
+     * Unwrap the cause of a throwable, if produced by a failure
+     * during the async resource cleanup in {@link Flux#usingWhen}.
+     *
+     * @param ex the throwable to try to unwrap
+     */
+    private Throwable unwrapIfResourceCleanupFailure(Throwable ex) {
+      if (ex instanceof RuntimeException
+              && ex.getCause() != null
+              && ex.getMessage().startsWith("Async resource cleanup failed")) {
+        return ex.getCause();
+      }
+      return ex;
     }
   }
 
