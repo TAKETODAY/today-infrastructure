34d856003033ba718b71f7a0edd7b7435ae09921
Harry Yang <i.take.today@gmail.com>
2023-03-27 22:53:25 +0800

    RouterFunction samples

--- today-infrastructure-samples/web-api/src/main/java/cn/taketoday/samples/ApplicationControllerAdvice.java
+++ today-infrastructure-samples/web-api/src/main/java/cn/taketoday/samples/ApplicationControllerAdvice.java
@@ -1,6 +1,6 @@
 /*
  * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
@@ -36,7 +36,6 @@ public class ApplicationControllerAdvice {
   @ExceptionHandler(Throwable.class)
   public void throwable(Throwable throwable, PrintWriter writer) {
     throwable.printStackTrace(writer);
-    writer.flush();
   }
 
   @ExceptionHandler(AsyncRequestTimeoutException.class)
