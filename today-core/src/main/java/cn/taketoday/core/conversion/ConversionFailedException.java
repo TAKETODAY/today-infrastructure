254a0ac71031666e1a67662b022b86feae12b07a
Harry Yang <i.take.today@gmail.com>
2023-04-16 22:25:13 +0800

    Polish ConversionFailedException

--- today-core/src/main/java/cn/taketoday/core/conversion/ConversionFailedException.java
+++ today-core/src/main/java/cn/taketoday/core/conversion/ConversionFailedException.java
@@ -50,11 +50,11 @@ public class ConversionFailedException extends ConversionException {
    * @param value the value we tried to convert
    * @param cause the cause of the conversion failure
    */
-  public ConversionFailedException(@Nullable TypeDescriptor sourceType, TypeDescriptor targetType,
-                                   @Nullable Object value, Throwable cause) {
+  public ConversionFailedException(@Nullable TypeDescriptor sourceType,
+          TypeDescriptor targetType, @Nullable Object value, Throwable cause) {
 
     super("Failed to convert from type [" + sourceType + "] to type [" + targetType +
-            "] for value '" + ObjectUtils.nullSafeConciseToString(value) + "'", cause);
+            "] for value [" + ObjectUtils.nullSafeConciseToString(value) + "]", cause);
     this.sourceType = sourceType;
     this.targetType = targetType;
     this.value = value;
