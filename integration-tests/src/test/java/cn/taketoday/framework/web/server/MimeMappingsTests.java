38c8bf96e5fab5a0f8606ce0b8c0906659fefd2e
Harry Yang <i.take.today@gmail.com>
2023-04-27 21:41:37 +0800

    MimeMappings

--- integration-tests/src/test/java/cn/taketoday/framework/web/server/MimeMappingsTests.java
+++ integration-tests/src/test/java/cn/taketoday/framework/web/server/MimeMappingsTests.java
@@ -1,6 +1,6 @@
 /*
  * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
  *
@@ -20,13 +20,22 @@
 
 package cn.taketoday.framework.web.server;
 
+import org.apache.catalina.startup.Tomcat;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.regex.Pattern;
+
+import cn.taketoday.core.io.ClassPathResource;
+import cn.taketoday.core.io.PropertiesUtils;
+import cn.taketoday.framework.web.server.MimeMappings.DefaultMimeMappings;
+import cn.taketoday.framework.web.server.MimeMappings.Mapping;
+import cn.taketoday.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -69,8 +78,8 @@ class MimeMappingsTests {
     MimeMappings mappings = new MimeMappings();
     mappings.add("foo", "bar");
     mappings.add("baz", "boo");
-    List<MimeMappings.Mapping> mappingList = new ArrayList<>();
-    for (MimeMappings.Mapping mapping : mappings) {
+    List<Mapping> mappingList = new ArrayList<>();
+    for (Mapping mapping : mappings) {
       mappingList.add(mapping);
     }
     assertThat(mappingList.get(0).getExtension()).isEqualTo("foo");
@@ -84,7 +93,7 @@ class MimeMappingsTests {
     MimeMappings mappings = new MimeMappings();
     mappings.add("foo", "bar");
     mappings.add("baz", "boo");
-    List<MimeMappings.Mapping> mappingList = new ArrayList<>(mappings.getAll());
+    List<Mapping> mappingList = new ArrayList<>(mappings.getAll());
     assertThat(mappingList.get(0).getExtension()).isEqualTo("foo");
     assertThat(mappingList.get(0).getMimeType()).isEqualTo("bar");
     assertThat(mappingList.get(1).getExtension()).isEqualTo("baz");
@@ -145,6 +154,74 @@ class MimeMappingsTests {
     String regName = "[A-Za-z0-9!#$&.+\\-^_]{1,127}";
     Pattern pattern = Pattern.compile("^" + regName + "\\/" + regName + "$");
     assertThat(MimeMappings.DEFAULT).allSatisfy((mapping) -> assertThat(mapping.getMimeType()).matches(pattern));
+  }
+
+  @Test
+  void getCommonTypeOnDefaultMimeMappingsDoesNotLoadMappings() {
+    DefaultMimeMappings mappings = new DefaultMimeMappings();
+    assertThat(mappings.get("json")).isEqualTo("application/json");
+    assertThat((Object) mappings).extracting("loaded").isNull();
+  }
+
+  @Test
+  void getExoticTypeOnDefaultMimeMappingsLoadsMappings() {
+    DefaultMimeMappings mappings = new DefaultMimeMappings();
+    assertThat(mappings.get("123")).isEqualTo("application/vnd.lotus-1-2-3");
+    assertThat((Object) mappings).extracting("loaded").isNotNull();
+  }
+
+  @Test
+  void iterateOnDefaultMimeMappingsLoadsMappings() {
+    DefaultMimeMappings mappings = new DefaultMimeMappings();
+    assertThat(mappings).isNotEmpty();
+    assertThat((Object) mappings).extracting("loaded").isNotNull();
+  }
+
+  @Test
+  void commonMappingsAreSubsetOfAllMappings() {
+    MimeMappings defaultMappings = new DefaultMimeMappings();
+    MimeMappings commonMappings = (MimeMappings) ReflectionTestUtils.getField(DefaultMimeMappings.class, "COMMON");
+    for (Mapping commonMapping : commonMappings) {
+      assertThat(defaultMappings.get(commonMapping.getExtension())).isEqualTo(commonMapping.getMimeType());
+    }
+  }
+
+  @Test
+  void lazyCopyWhenNotMutatedDelegates() {
+    DefaultMimeMappings mappings = new DefaultMimeMappings();
+    MimeMappings lazyCopy = MimeMappings.lazyCopy(mappings);
+    assertThat(lazyCopy.get("json")).isEqualTo("application/json");
+    assertThat((Object) mappings).extracting("loaded").isNull();
+  }
+
+  @Test
+  void lazyCopyWhenMutatedCreatesCopy() {
+    DefaultMimeMappings mappings = new DefaultMimeMappings();
+    MimeMappings lazyCopy = MimeMappings.lazyCopy(mappings);
+    lazyCopy.add("json", "other/json");
+    assertThat(lazyCopy.get("json")).isEqualTo("other/json");
+    assertThat((Object) mappings).extracting("loaded").isNotNull();
+  }
+
+  @Test
+  void lazyCopyWhenMutatedCreatesCopyOnlyOnce() {
+    MimeMappings mappings = new MimeMappings();
+    mappings.add("json", "one/json");
+    MimeMappings lazyCopy = MimeMappings.lazyCopy(mappings);
+    lazyCopy.add("first", "copy/yes");
+    assertThat(lazyCopy.get("json")).isEqualTo("one/json");
+    mappings.add("json", "two/json");
+    lazyCopy.add("second", "copy/no");
+    assertThat(lazyCopy.get("json")).isEqualTo("one/json");
+  }
+
+  @Test
+  void mimeMappingsMatchesTomcatDefaults() throws IOException {
+    Properties ourDefaultMimeMappings = PropertiesUtils
+            .loadProperties(new ClassPathResource("cn/taketoday/framework/web/server/mime-mappings.properties", getClass()));
+    Properties tomcatDefaultMimeMappings = PropertiesUtils
+            .loadProperties(new ClassPathResource("MimeTypeMappings.properties", Tomcat.class));
+    assertThat(ourDefaultMimeMappings).containsExactlyInAnyOrderEntriesOf(tomcatDefaultMimeMappings);
   }
 
 }
