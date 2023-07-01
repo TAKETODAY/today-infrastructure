d0b81e56d63130e62a90f7d8bf06a52fc38d9b22
Harry Yang <i.take.today@gmail.com>
2023-07-02 01:04:44 +0800

    :art: LoggerConfiguration

--- today-framework/src/main/java/cn/taketoday/framework/logging/LoggerConfiguration.java
+++ today-framework/src/main/java/cn/taketoday/framework/logging/LoggerConfiguration.java
@@ -23,20 +23,25 @@ package cn.taketoday.framework.logging;
 import java.util.Objects;
 
 import cn.taketoday.lang.Assert;
+import cn.taketoday.lang.Nullable;
+import cn.taketoday.util.ObjectUtils;
 
 /**
  * Immutable class that represents the configuration of a {@link LoggingSystem}'s logger.
  *
  * @author Ben Hale
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0
  */
 public final class LoggerConfiguration {
 
   private final String name;
 
-  private final LogLevel configuredLevel;
+  @Nullable
+  private final LevelConfiguration levelConfiguration;
 
-  private final LogLevel effectiveLevel;
+  @Nullable
+  private final LevelConfiguration inheritedLevelConfiguration;
 
   /**
    * Create a new {@link LoggerConfiguration instance}.
@@ -45,30 +50,29 @@ public final class LoggerConfiguration {
    * @param configuredLevel the configured level of the logger
    * @param effectiveLevel the effective level of the logger
    */
-  public LoggerConfiguration(String name, LogLevel configuredLevel, LogLevel effectiveLevel) {
+  public LoggerConfiguration(String name, @Nullable LogLevel configuredLevel, LogLevel effectiveLevel) {
     Assert.notNull(name, "Name must not be null");
     Assert.notNull(effectiveLevel, "EffectiveLevel must not be null");
     this.name = name;
-    this.configuredLevel = configuredLevel;
-    this.effectiveLevel = effectiveLevel;
+    this.levelConfiguration = (configuredLevel != null) ? LevelConfiguration.of(configuredLevel) : null;
+    this.inheritedLevelConfiguration = LevelConfiguration.of(effectiveLevel);
   }
 
   /**
-   * Returns the configured level of the logger.
+   * Create a new {@link LoggerConfiguration instance}.
    *
-   * @return the configured level of the logger
+   * @param name the name of the logger
+   * @param levelConfiguration the level configuration
+   * @param inheritedLevelConfiguration the inherited level configuration
    */
-  public LogLevel getConfiguredLevel() {
-    return this.configuredLevel;
-  }
-
-  /**
-   * Returns the effective level of the logger.
-   *
-   * @return the effective level of the logger
-   */
-  public LogLevel getEffectiveLevel() {
-    return this.effectiveLevel;
+  public LoggerConfiguration(String name,
+          @Nullable LevelConfiguration levelConfiguration,
+          LevelConfiguration inheritedLevelConfiguration) {
+    Assert.notNull(name, "Name must not be null");
+    Assert.notNull(inheritedLevelConfiguration, "EffectiveLevelConfiguration must not be null");
+    this.name = name;
+    this.levelConfiguration = levelConfiguration;
+    this.inheritedLevelConfiguration = inheritedLevelConfiguration;
   }
 
   /**
@@ -80,31 +84,183 @@ public final class LoggerConfiguration {
     return this.name;
   }
 
+  /**
+   * Returns the configured level of the logger.
+   *
+   * @return the configured level of the logger
+   * @see #getLevelConfiguration(ConfigurationScope)
+   */
+  @Nullable
+  public LogLevel getConfiguredLevel() {
+    LevelConfiguration configuration = getLevelConfiguration(ConfigurationScope.DIRECT);
+    return (configuration != null) ? configuration.getLevel() : null;
+  }
+
+  /**
+   * Returns the effective level of the logger.
+   *
+   * @return the effective level of the logger
+   * @see #getLevelConfiguration(ConfigurationScope)
+   */
+  public LogLevel getEffectiveLevel() {
+    return getLevelConfiguration().getLevel();
+  }
+
+  /**
+   * Return the level configuration, considering inherited loggers.
+   *
+   * @return the level configuration
+   */
+  @Nullable
+  public LevelConfiguration getLevelConfiguration() {
+    return getLevelConfiguration(ConfigurationScope.INHERITED);
+  }
+
+  /**
+   * Return the level configuration for the given scope.
+   *
+   * @param scope the configuration scope
+   * @return the level configuration or {@code null} for
+   * {@link ConfigurationScope#DIRECT direct scope} results without applied
+   * configuration
+   */
+  @Nullable
+  public LevelConfiguration getLevelConfiguration(ConfigurationScope scope) {
+    return (scope != ConfigurationScope.DIRECT) ? this.inheritedLevelConfiguration : this.levelConfiguration;
+  }
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
       return true;
     }
-    if (obj == null) {
+    if (obj == null || getClass() != obj.getClass()) {
       return false;
     }
-    if (obj instanceof LoggerConfiguration other) {
-      return Objects.equals(this.name, other.name)
-              && Objects.equals(this.configuredLevel, other.configuredLevel)
-              && Objects.equals(this.effectiveLevel, other.effectiveLevel);
-    }
-    return super.equals(obj);
+    LoggerConfiguration other = (LoggerConfiguration) obj;
+    return ObjectUtils.nullSafeEquals(this.name, other.name)
+            && ObjectUtils.nullSafeEquals(this.levelConfiguration, other.levelConfiguration)
+            && ObjectUtils.nullSafeEquals(this.inheritedLevelConfiguration, other.inheritedLevelConfiguration);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name, configuredLevel, effectiveLevel);
+    return Objects.hash(this.name, this.levelConfiguration, this.inheritedLevelConfiguration);
   }
 
   @Override
   public String toString() {
-    return "LoggerConfiguration [name=" + this.name + ", configuredLevel=" + this.configuredLevel
-            + ", effectiveLevel=" + this.effectiveLevel + "]";
+    return "LoggerConfiguration [name=" + this.name + ", levelConfiguration=" + this.levelConfiguration
+            + ", inheritedLevelConfiguration=" + this.inheritedLevelConfiguration + "]";
+  }
+
+  /**
+   * Supported logger configurations scopes.
+   */
+  public enum ConfigurationScope {
+
+    /**
+     * Only return configuration that has been applied directly applied. Often
+     * referred to as 'configured' or 'assigned' configuration.
+     */
+    DIRECT,
+
+    /**
+     * May return configuration that has been applied to a parent logger. Often
+     * referred to as 'effective' configuration.
+     */
+    INHERITED
+
+  }
+
+  /**
+   * Logger level configuration.
+   */
+  public static final class LevelConfiguration {
+
+    private final String name;
+
+    @Nullable
+    private final LogLevel logLevel;
+
+    private LevelConfiguration(String name, @Nullable LogLevel logLevel) {
+      this.name = name;
+      this.logLevel = logLevel;
+    }
+
+    /**
+     * Return the name of the level.
+     *
+     * @return the level name
+     */
+    public String getName() {
+      return this.name;
+    }
+
+    /**
+     * Return the actual level value if possible.
+     *
+     * @return the level value
+     * @throws IllegalStateException if this is a {@link #isCustom() custom} level
+     */
+    public LogLevel getLevel() {
+      Assert.state(this.logLevel != null, "Unable to provide LogLevel for '" + this.name + "'");
+      return this.logLevel;
+    }
+
+    /**
+     * Return if this is a custom level and cannot be represented by {@link LogLevel}.
+     *
+     * @return if this is a custom level
+     */
+    public boolean isCustom() {
+      return this.logLevel == null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null || getClass() != obj.getClass()) {
+        return false;
+      }
+      LevelConfiguration other = (LevelConfiguration) obj;
+      return this.logLevel == other.logLevel && ObjectUtils.nullSafeEquals(this.name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.logLevel, this.name);
+    }
+
+    @Override
+    public String toString() {
+      return "LevelConfiguration [name=" + this.name + ", logLevel=" + this.logLevel + "]";
+    }
+
+    /**
+     * Create a new {@link LevelConfiguration} instance of the given {@link LogLevel}.
+     *
+     * @param logLevel the log level
+     * @return a new {@link LevelConfiguration} instance
+     */
+    public static LevelConfiguration of(LogLevel logLevel) {
+      Assert.notNull(logLevel, "LogLevel must not be null");
+      return new LevelConfiguration(logLevel.name(), logLevel);
+    }
+
+    /**
+     * Create a new {@link LevelConfiguration} instance for a custom level name.
+     *
+     * @param name the log level name
+     * @return a new {@link LevelConfiguration} instance
+     */
+    public static LevelConfiguration ofCustom(String name) {
+      Assert.hasText(name, "Name must not be empty");
+      return new LevelConfiguration(name, null);
+    }
+
   }
 
 }
