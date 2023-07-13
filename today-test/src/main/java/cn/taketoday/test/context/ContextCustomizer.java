53377bb937302e879e57a977e2869064a4b3ead0
Harry Yang <i.take.today@gmail.com>
2023-07-13 23:11:38 +0800

    :memo: Emphasize that a ContextCustomizer must implement equals() and hashCode()

--- today-test/src/main/java/cn/taketoday/test/context/ContextCustomizer.java
+++ today-test/src/main/java/cn/taketoday/test/context/ContextCustomizer.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,12 +27,14 @@ import cn.taketoday.test.context.support.AbstractContextLoader;
  *
  * <p>Customizers are created by {@link ContextCustomizerFactory} implementations.
  *
- * <p>Implementations must implement correct {@code equals} and {@code hashCode}
+ * <p><strong>WARNING</strong>: implementations must implement correct
+ * {@link Object#equals(Object) equals} and {@link Object#hashCode() hashCode}
  * methods since customizers form part of the {@link MergedContextConfiguration}
  * which is used as a cache key.
  *
  * @author Phillip Webb
  * @author Sam Brannen
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @see ContextCustomizerFactory
  * @see AbstractContextLoader#customizeContext
  * @since 4.0
