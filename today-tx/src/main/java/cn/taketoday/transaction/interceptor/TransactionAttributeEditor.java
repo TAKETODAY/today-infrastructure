902cd49a0070765f20efc08beb39394b7b817792
Harry Yang <i.take.today@gmail.com>
2023-07-19 21:00:57 +0800

    :art: Remove cross reference to Constants utility

--- today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAttributeEditor.java
+++ today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAttributeEditor.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,7 +38,6 @@ import cn.taketoday.util.StringUtils;
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @see cn.taketoday.transaction.TransactionDefinition
- * @see cn.taketoday.core.Constants
  * @since 4.0
  */
 public class TransactionAttributeEditor extends PropertyEditorSupport {
