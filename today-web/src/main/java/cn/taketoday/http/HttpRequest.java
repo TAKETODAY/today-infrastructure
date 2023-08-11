a3728c3c416a76134f86184477dce5e73e41d5b1
Harry Yang <i.take.today@gmail.com>
2023-08-12 00:20:46 +0800

    :art: HttpMethod

--- today-web/src/main/java/cn/taketoday/http/HttpRequest.java
+++ today-web/src/main/java/cn/taketoday/http/HttpRequest.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,7 +33,7 @@ public interface HttpRequest extends HttpMessage {
    * Return the HTTP method of the request.
    *
    * @return the HTTP method as an HttpMethod value
-   * @see HttpMethod#from(String)
+   * @see HttpMethod#resolve(String)
    */
   HttpMethod getMethod();
 
