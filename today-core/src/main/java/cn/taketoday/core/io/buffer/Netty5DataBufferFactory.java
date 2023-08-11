5cac70df8cd44f6d217b5e453efaf9d86d3727b1
Harry Yang <i.take.today@gmail.com>
2023-08-12 00:54:26 +0800

    :art: 处理 Deprecated

--- today-core/src/main/java/cn/taketoday/core/io/buffer/Netty5DataBufferFactory.java
+++ today-core/src/main/java/cn/taketoday/core/io/buffer/Netty5DataBufferFactory.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -59,7 +56,6 @@ public class Netty5DataBufferFactory implements DataBufferFactory {
   }
 
   @Override
-  @Deprecated
   public Netty5DataBuffer allocateBuffer() {
     Buffer buffer = this.bufferAllocator.allocate(256);
     return new Netty5DataBuffer(buffer, this);
--- today-core/src/main/java/cn/taketoday/core/io/buffer/NettyDataBufferFactory.java
+++ today-core/src/main/java/cn/taketoday/core/io/buffer/NettyDataBufferFactory.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -63,7 +60,6 @@ public class NettyDataBufferFactory implements DataBufferFactory {
   }
 
   @Override
-  @Deprecated
   public NettyDataBuffer allocateBuffer() {
     ByteBuf byteBuf = this.byteBufAllocator.buffer();
     return new NettyDataBuffer(byteBuf, this);
