adc03960b6af5f167db3c272f8a592d185703083
Harry Yang <i.take.today@gmail.com>
2023-08-13 11:37:26 +0800

    :art: Use DockerImageNames in Docker compose integration tests

--- today-test-support/src/main/java/cn/taketoday/test/testcontainers/DockerImageNames.java
+++ today-test-support/src/main/java/cn/taketoday/test/testcontainers/DockerImageNames.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,26 +23,55 @@ import org.testcontainers.utility.DockerImageName;
  * Create {@link DockerImageName} instances for services used in integration tests.
  *
  * @author Stephane Nicoll
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0
  */
 public final class DockerImageNames {
 
+  private static final String ACTIVE_MQ_VERSION = "5.18.0";
+
   private static final String CASSANDRA_VERSION = "3.11.10";
 
-  private static final String COUCHBASE_VERSION = "6.5.1";
+  private static final String COUCHBASE_VERSION = "7.1.4";
 
-  private static final String MONGO_VERSION = "4.0.23";
+  private static final String ELASTICSEARCH_VERSION = "7.17.5";
 
-  private static final String NEO4J_VERSION = "4.0";
+  private static final String ELASTICSEARCH_8_VERSION = "8.6.1";
+
+  private static final String KAFKA_VERSION = "7.4.0";
+
+  private static final String MARIADB_VERSION = "10.10";
+
+  private static final String MONGO_VERSION = "5.0.17";
+
+  private static final String MYSQL_VERSION = "8.0";
+
+  private static final String NEO4J_VERSION = "4.4.11";
+
+  private static final String ORACLE_XE_VERSION = "18.4.0-slim";
 
   private static final String POSTGRESQL_VERSION = "14.0";
 
-  private static final String REDIS_VERSION = "4.0.14";
+  private static final String RABBIT_VERSION = "3.11-alpine";
+
+  private static final String REDIS_VERSION = "7.0.11";
+
+  private static final String REDPANDA_VERSION = "v23.1.2";
 
   private static final String REGISTRY_VERSION = "2.7.1";
 
-  private DockerImageNames() {
+  private static final String ZIPKIN_VERSION = "2.24.1";
 
+  private DockerImageNames() {
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running ActiveMQ.
+   *
+   * @return a docker image name for running activeMq
+   */
+  public static DockerImageName activeMq() {
+    return DockerImageName.parse("symptoma/activemq").withTag(ACTIVE_MQ_VERSION);
   }
 
   /**
@@ -67,14 +93,39 @@ public final class DockerImageNames {
   }
 
   /**
-   * Return a {@link DockerImageName} suitable for running Elasticsearch according to
-   * the version available on the classpath.
+   * Return a {@link DockerImageName} suitable for running Elasticsearch 7.
    *
    * @return a docker image name for running elasticsearch
    */
   public static DockerImageName elasticsearch() {
-    String version = org.elasticsearch.Version.CURRENT.toString();
-    return DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag(version);
+    return DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag(ELASTICSEARCH_VERSION);
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running Elasticsearch 8.
+   *
+   * @return a docker image name for running elasticsearch
+   */
+  public static DockerImageName elasticsearch8() {
+    return DockerImageName.parse("elasticsearch").withTag(ELASTICSEARCH_8_VERSION);
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running Kafka.
+   *
+   * @return a docker image name for running Kafka
+   */
+  public static DockerImageName kafka() {
+    return DockerImageName.parse("confluentinc/cp-kafka").withTag(KAFKA_VERSION);
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running MariaDB.
+   *
+   * @return a docker image name for running Mariadb
+   */
+  public static DockerImageName mariadb() {
+    return DockerImageName.parse("mariadb").withTag(MARIADB_VERSION);
   }
 
   /**
@@ -87,12 +138,30 @@ public final class DockerImageNames {
   }
 
   /**
+   * Return a {@link DockerImageName} suitable for running MySQL.
+   *
+   * @return a docker image name for running MySQL
+   */
+  public static DockerImageName mysql() {
+    return DockerImageName.parse("mysql").withTag(MYSQL_VERSION);
+  }
+
+  /**
    * Return a {@link DockerImageName} suitable for running Neo4j.
    *
    * @return a docker image name for running neo4j
    */
   public static DockerImageName neo4j() {
     return DockerImageName.parse("neo4j").withTag(NEO4J_VERSION);
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running the Oracle database.
+   *
+   * @return a docker image name for running the Oracle database
+   */
+  public static DockerImageName oracleXe() {
+    return DockerImageName.parse("gvenzl/oracle-xe").withTag(ORACLE_XE_VERSION);
   }
 
   /**
@@ -105,6 +174,15 @@ public final class DockerImageNames {
   }
 
   /**
+   * Return a {@link DockerImageName} suitable for running RabbitMQ.
+   *
+   * @return a docker image name for running redis
+   */
+  public static DockerImageName rabbit() {
+    return DockerImageName.parse("rabbitmq").withTag(RABBIT_VERSION);
+  }
+
+  /**
    * Return a {@link DockerImageName} suitable for running Redis.
    *
    * @return a docker image name for running redis
@@ -114,13 +192,41 @@ public final class DockerImageNames {
   }
 
   /**
+   * Return a {@link DockerImageName} suitable for running Redpanda.
+   *
+   * @return a docker image name for running redpanda
+   */
+  public static DockerImageName redpanda() {
+    return DockerImageName.parse("redpandadata/redpanda")
+            .withTag(REDPANDA_VERSION)
+            .asCompatibleSubstituteFor("docker.redpanda.com/redpandadata/redpanda");
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running Microsoft SQLServer.
+   *
+   * @return a docker image name for running SQLServer
+   */
+  public static DockerImageName sqlserver() {
+    return DockerImageName.parse("mcr.microsoft.com/mssql/server");
+  }
+
+  /**
    * Return a {@link DockerImageName} suitable for running a Docker registry.
    *
    * @return a docker image name for running a registry
-   * @since 4.0
    */
   public static DockerImageName registry() {
     return DockerImageName.parse("registry").withTag(REGISTRY_VERSION);
+  }
+
+  /**
+   * Return a {@link DockerImageName} suitable for running Zipkin.
+   *
+   * @return a docker image name for running Zipkin
+   */
+  public static DockerImageName zipkin() {
+    return DockerImageName.parse("openzipkin/zipkin").withTag(ZIPKIN_VERSION);
   }
 
 }
