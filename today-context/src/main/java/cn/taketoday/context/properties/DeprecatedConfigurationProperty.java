3ec0cfe3dda612927687a9fd0d3e27ed5734cb06
Harry Yang <i.take.today@gmail.com>
2023-08-13 14:05:21 +0800

    Add since attribute to @DeprecatedConfigurationProperty annotation

--- today-context/src/main/java/cn/taketoday/context/properties/DeprecatedConfigurationProperty.java
+++ today-context/src/main/java/cn/taketoday/context/properties/DeprecatedConfigurationProperty.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.context.properties;
@@ -56,5 +53,12 @@ public @interface DeprecatedConfigurationProperty {
    * @return the replacement field
    */
   String replacement() default "";
+
+  /**
+   * The version in which the property became deprecated.
+   *
+   * @return the version
+   */
+  String since() default "";
 
 }
