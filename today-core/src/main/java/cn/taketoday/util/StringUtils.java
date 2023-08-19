38eefaaebf7de1f5d0452de6392f1cbde585d3a4
Harry Yang <i.take.today@gmail.com>
2023-08-19 23:28:58 +0800

    :art: Optimize whitespace checks in StringUtils

--- today-core/src/main/java/cn/taketoday/util/StringUtils.java
+++ today-core/src/main/java/cn/taketoday/util/StringUtils.java
@@ -1033,7 +1033,21 @@ else */
    * @see Character#isWhitespace
    */
   public static boolean hasText(@Nullable CharSequence str) {
-    return isNotEmpty(str) && containsText(str);
+    if (str == null) {
+      return false;
+    }
+
+    int strLen = str.length();
+    if (strLen == 0) {
+      return false;
+    }
+
+    for (int i = 0; i < strLen; i++) {
+      if (!Character.isWhitespace(str.charAt(i))) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
@@ -1050,17 +1064,7 @@ else */
    * @see Character#isWhitespace
    */
   public static boolean hasText(@Nullable String str) {
-    return (str != null && !str.isEmpty() && containsText(str));
-  }
-
-  private static boolean containsText(CharSequence str) {
-    int strLen = str.length();
-    for (int i = 0; i < strLen; i++) {
-      if (!Character.isWhitespace(str.charAt(i))) {
-        return true;
-      }
-    }
-    return false;
+    return str != null && !str.isBlank();
   }
 
   /**
