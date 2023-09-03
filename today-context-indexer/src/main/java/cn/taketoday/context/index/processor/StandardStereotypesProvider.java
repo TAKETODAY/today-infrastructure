bf6c7b7ee382be78c9e5e09bc95b48625f353cf0
Harry Yang <i.take.today@gmail.com>
2023-09-03 12:51:54 +0800

    :art: Reinstate support for javax.* annotations in component indexer
    - for consistency with the changes made in e1826d2322

--- today-context-indexer/src/main/java/cn/taketoday/context/index/processor/StandardStereotypesProvider.java
+++ today-context-indexer/src/main/java/cn/taketoday/context/index/processor/StandardStereotypesProvider.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -51,7 +48,7 @@ class StandardStereotypesProvider implements StereotypesProvider {
     }
     for (AnnotationMirror annotation : this.typeHelper.getAllAnnotationMirrors(element)) {
       String type = this.typeHelper.getType(annotation);
-      if (type.startsWith("jakarta.")) {
+      if (type.startsWith("jakarta.") || type.startsWith("javax.")) {
         stereotypes.add(type);
       }
     }
