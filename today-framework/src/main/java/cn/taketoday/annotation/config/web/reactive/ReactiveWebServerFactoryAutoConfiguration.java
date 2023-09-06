34410f0dec39cd5df962df5ff6fac7d6b30b6cac
Harry Yang <i.take.today@gmail.com>
2023-09-06 16:03:47 +0800

    :bug:

--- today-framework/src/main/java/cn/taketoday/annotation/config/web/reactive/ReactiveWebServerFactoryAutoConfiguration.java
+++ today-framework/src/main/java/cn/taketoday/annotation/config/web/reactive/ReactiveWebServerFactoryAutoConfiguration.java
@@ -17,14 +17,10 @@
 
 package cn.taketoday.annotation.config.web.reactive;
 
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import cn.taketoday.beans.factory.ObjectProvider;
 import cn.taketoday.beans.factory.annotation.DisableDependencyInjection;
 import cn.taketoday.beans.factory.config.ConfigurableBeanFactory;
 import cn.taketoday.beans.factory.support.RootBeanDefinition;
 import cn.taketoday.context.BootstrapContext;
-import cn.taketoday.context.annotation.Configuration;
 import cn.taketoday.context.annotation.Import;
 import cn.taketoday.context.annotation.ImportBeanDefinitionRegistrar;
 import cn.taketoday.context.annotation.Lazy;
@@ -40,29 +36,13 @@ import cn.taketoday.core.Ordered;
 import cn.taketoday.core.ssl.SslBundles;
 import cn.taketoday.core.type.AnnotationMetadata;
 import cn.taketoday.framework.annotation.ConditionalOnWebApplication;
-import cn.taketoday.framework.web.embedded.jetty.JettyReactiveWebServerFactory;
-import cn.taketoday.framework.web.embedded.jetty.JettyServerCustomizer;
-import cn.taketoday.framework.web.embedded.netty.NettyRouteProvider;
-import cn.taketoday.framework.web.embedded.netty.ReactorNettyReactiveWebServerFactory;
-import cn.taketoday.framework.web.embedded.netty.ReactorNettyServerCustomizer;
-import cn.taketoday.framework.web.embedded.tomcat.TomcatConnectorCustomizer;
-import cn.taketoday.framework.web.embedded.tomcat.TomcatContextCustomizer;
-import cn.taketoday.framework.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import cn.taketoday.framework.web.embedded.tomcat.TomcatReactiveWebServerFactory;
-import cn.taketoday.framework.web.embedded.undertow.UndertowBuilderCustomizer;
-import cn.taketoday.framework.web.embedded.undertow.UndertowReactiveWebServerFactory;
-import cn.taketoday.framework.web.reactive.server.ReactiveWebServerFactory;
 import cn.taketoday.framework.web.server.ServerProperties;
 import cn.taketoday.framework.web.server.WebServerFactoryCustomizerBeanPostProcessor;
 import cn.taketoday.http.ReactiveHttpInputMessage;
-import cn.taketoday.http.client.reactive.JettyResourceFactory;
-import cn.taketoday.http.client.reactive.ReactorResourceFactory;
 import cn.taketoday.http.server.reactive.ForwardedHeaderTransformer;
 import cn.taketoday.lang.Nullable;
 import cn.taketoday.stereotype.Component;
 import cn.taketoday.util.CollectionUtils;
-import io.undertow.Undertow;
-import reactor.netty.http.server.HttpServer;
 
 import static cn.taketoday.annotation.config.web.reactive.ReactiveWebServerFactoryAutoConfiguration.BeanPostProcessorsRegistrar;
 import static cn.taketoday.framework.annotation.ConditionalOnWebApplication.Type;
@@ -80,7 +60,12 @@ import static cn.taketoday.framework.annotation.ConditionalOnWebApplication.Type
 @ConditionalOnClass(ReactiveHttpInputMessage.class)
 @ConditionalOnWebApplication(type = Type.REACTIVE)
 @EnableConfigurationProperties(ServerProperties.class)
-@Import(BeanPostProcessorsRegistrar.class)
+@Import({ BeanPostProcessorsRegistrar.class,
+        ReactiveWebServerFactoryConfiguration.EmbeddedTomcat.class,
+        ReactiveWebServerFactoryConfiguration.EmbeddedJetty.class,
+        ReactiveWebServerFactoryConfiguration.EmbeddedUndertow.class,
+        ReactiveWebServerFactoryConfiguration.EmbeddedNetty.class
+})
 public class ReactiveWebServerFactoryAutoConfiguration {
 
   @Component
@@ -101,88 +86,6 @@ public class ReactiveWebServerFactoryAutoConfiguration {
   @ConditionalOnProperty(value = "server.forward-headers-strategy", havingValue = "framework")
   static ForwardedHeaderTransformer forwardedHeaderTransformer() {
     return new ForwardedHeaderTransformer();
-  }
-
-  @Configuration(proxyBeanMethods = false)
-  @ConditionalOnClass({ HttpServer.class })
-  @ConditionalOnMissingBean(ReactiveWebServerFactory.class)
-  static class EmbeddedNetty {
-
-    @Component
-    @ConditionalOnMissingBean
-    static ReactorResourceFactory reactorServerResourceFactory() {
-      return new ReactorResourceFactory();
-    }
-
-    @Component
-    static ReactorNettyReactiveWebServerFactory reactorNettyReactiveWebServerFactory(ReactorResourceFactory resourceFactory,
-            ObjectProvider<NettyRouteProvider> routes, ObjectProvider<ReactorNettyServerCustomizer> serverCustomizers) {
-
-      ReactorNettyReactiveWebServerFactory serverFactory = new ReactorNettyReactiveWebServerFactory();
-      serverFactory.setResourceFactory(resourceFactory);
-      for (NettyRouteProvider route : routes) {
-        serverFactory.addRouteProviders(route);
-      }
-      serverCustomizers.addOrderedTo(serverFactory.getServerCustomizers());
-      return serverFactory;
-    }
-
-  }
-
-  @Configuration(proxyBeanMethods = false)
-  @ConditionalOnClass({ org.apache.catalina.startup.Tomcat.class })
-  @ConditionalOnMissingBean(ReactiveWebServerFactory.class)
-  static class EmbeddedTomcat {
-
-    @Component
-    static TomcatReactiveWebServerFactory tomcatReactiveWebServerFactory(
-            ObjectProvider<TomcatConnectorCustomizer> connectorCustomizers,
-            ObjectProvider<TomcatContextCustomizer> contextCustomizers,
-            ObjectProvider<TomcatProtocolHandlerCustomizer<?>> protocolHandlerCustomizers) {
-      TomcatReactiveWebServerFactory factory = new TomcatReactiveWebServerFactory();
-      contextCustomizers.addOrderedTo(factory.getTomcatContextCustomizers());
-      connectorCustomizers.addOrderedTo(factory.getTomcatConnectorCustomizers());
-      protocolHandlerCustomizers.addOrderedTo(factory.getTomcatProtocolHandlerCustomizers());
-      return factory;
-    }
-
-  }
-
-  @Configuration(proxyBeanMethods = false)
-  @ConditionalOnClass({ org.eclipse.jetty.server.Server.class, ServletHolder.class })
-  @ConditionalOnMissingBean(ReactiveWebServerFactory.class)
-  static class EmbeddedJetty {
-
-    @Component
-    @ConditionalOnMissingBean
-    static JettyResourceFactory jettyServerResourceFactory() {
-      return new JettyResourceFactory();
-    }
-
-    @Component
-    static JettyReactiveWebServerFactory jettyReactiveWebServerFactory(
-            JettyResourceFactory resourceFactory, ObjectProvider<JettyServerCustomizer> serverCustomizers) {
-      JettyReactiveWebServerFactory serverFactory = new JettyReactiveWebServerFactory();
-      serverCustomizers.addOrderedTo(serverFactory.getServerCustomizers());
-      serverFactory.setResourceFactory(resourceFactory);
-      return serverFactory;
-    }
-
-  }
-
-  @Configuration(proxyBeanMethods = false)
-  @ConditionalOnClass({ Undertow.class })
-  @ConditionalOnMissingBean(ReactiveWebServerFactory.class)
-  static class EmbeddedUndertow {
-
-    @Component
-    static UndertowReactiveWebServerFactory undertowReactiveWebServerFactory(
-            ObjectProvider<UndertowBuilderCustomizer> builderCustomizers) {
-      UndertowReactiveWebServerFactory factory = new UndertowReactiveWebServerFactory();
-      builderCustomizers.addOrderedTo(factory.getBuilderCustomizers());
-      return factory;
-    }
-
   }
 
   /**
