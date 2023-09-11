59e5a3cab1363eadc4819e901e4f42a8b8fcf1a6
Harry Yang <i.take.today@gmail.com>
2023-09-11 22:46:59 +0800

    :sparkles: Revise FilePatternResourceHintsRegistrar API and improve documentation
    This commit revises the FilePatternResourceHintsRegistrar API and
    introduces List<String> overrides of various var-args methods used with
    the new builder API.

--- today-core/src/main/java/cn/taketoday/aot/hint/support/FilePatternResourceHintsRegistrar.java
+++ today-core/src/main/java/cn/taketoday/aot/hint/support/FilePatternResourceHintsRegistrar.java
@@ -25,60 +25,52 @@ import cn.taketoday.aot.hint.ResourceHints;
 import cn.taketoday.lang.Assert;
 import cn.taketoday.lang.Nullable;
 import cn.taketoday.util.ResourceUtils;
-import cn.taketoday.util.StringUtils;
 
 /**
- * Register the necessary resource hints for loading files from the classpath.
+ * Register the necessary resource hints for loading files from the classpath,
+ * based on file name prefixes and file extensions with convenience to support
+ * multiple classpath locations.
  *
- * <p>Candidates are identified by a file name, a location, and an extension.
- * The location can be the empty string to refer to the root of the classpath.
+ * <p>Only registers hints for matching classpath locations, which allows for
+ * several locations to be provided without contributing unnecessary hints.
  *
  * @author Stephane Nicoll
+ * @author Sam Brannen
  * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0
  */
 public class FilePatternResourceHintsRegistrar {
 
-  private final List<String> names;
+  private final List<String> classpathLocations;
 
-  private final List<String> locations;
+  private final List<String> filePrefixes;
 
-  private final List<String> extensions;
-
-  /**
-   * Create a new instance for the specified file names, locations, and file
-   * extensions.
-   *
-   * @param names the file names
-   * @param locations the classpath locations
-   * @param extensions the file extensions (starts with a dot)
-   */
-  public FilePatternResourceHintsRegistrar(List<String> names, List<String> locations, List<String> extensions) {
-    this.names = Builder.validateFilePrefixes(StringUtils.toStringArray(names));
-    this.extensions = Builder.validateFileExtensions(StringUtils.toStringArray(extensions));
-    this.locations = Builder.validateClasspathLocations(StringUtils.toStringArray(locations));
-  }
+  private final List<String> fileExtensions;
 
   /**
-   * Configure the registrar with the specified
-   * {@linkplain Builder#withClasspathLocations(String...) classpath locations}.
+   * Create a new instance for the specified file prefixes, classpath locations,
+   * and file extensions.
    *
-   * @param locations the classpath locations
-   * @return a {@link Builder} to further configure the registrar
+   * @param filePrefixes the file prefixes
+   * @param classpathLocations the classpath locations
+   * @param fileExtensions the file extensions (starting with a dot)
    */
-  public static Builder forClassPathLocations(String... locations) {
-    Assert.notEmpty(locations, "At least one classpath location should be specified");
-    return new Builder().withClasspathLocations(locations);
+  public FilePatternResourceHintsRegistrar(List<String> filePrefixes,
+          List<String> classpathLocations, List<String> fileExtensions) {
+
+    this.classpathLocations = validateClasspathLocations(classpathLocations);
+    this.filePrefixes = validateFilePrefixes(filePrefixes);
+    this.fileExtensions = validateFileExtensions(fileExtensions);
   }
 
   public void registerHints(ResourceHints hints, @Nullable ClassLoader classLoader) {
-    ClassLoader classLoaderToUse = (classLoader != null) ? classLoader : getClass().getClassLoader();
+    ClassLoader classLoaderToUse = (classLoader != null ? classLoader : getClass().getClassLoader());
     List<String> includes = new ArrayList<>();
-    for (String location : this.locations) {
+    for (String location : this.classpathLocations) {
       if (classLoaderToUse.getResource(location) != null) {
-        for (String extension : this.extensions) {
-          for (String name : this.names) {
-            includes.add(location + name + "*" + extension);
+        for (String filePrefix : this.filePrefixes) {
+          for (String fileExtension : this.fileExtensions) {
+            includes.add(location + filePrefix + "*" + fileExtension);
           }
         }
       }
@@ -86,6 +78,66 @@ public class FilePatternResourceHintsRegistrar {
     if (!includes.isEmpty()) {
       hints.registerPattern(hint -> hint.includes(includes.toArray(String[]::new)));
     }
+  }
+
+  /**
+   * Configure the registrar with the specified
+   * {@linkplain Builder#withClasspathLocations(String...) classpath locations}.
+   *
+   * @param classpathLocations the classpath locations
+   * @return a {@link Builder} to further configure the registrar
+   * @see #forClassPathLocations(List)
+   */
+  public static Builder forClassPathLocations(String... classpathLocations) {
+    return forClassPathLocations(Arrays.asList(classpathLocations));
+  }
+
+  /**
+   * Configure the registrar with the specified
+   * {@linkplain Builder#withClasspathLocations(List) classpath locations}.
+   *
+   * @param classpathLocations the classpath locations
+   * @return a {@link Builder} to further configure the registrar
+   * @see #forClassPathLocations(String...)
+   */
+  public static Builder forClassPathLocations(List<String> classpathLocations) {
+    return new Builder().withClasspathLocations(classpathLocations);
+  }
+
+  private static List<String> validateClasspathLocations(List<String> classpathLocations) {
+    Assert.notEmpty(classpathLocations, "At least one classpath location must be specified");
+    List<String> parsedLocations = new ArrayList<>();
+    for (String location : classpathLocations) {
+      if (location.startsWith(ResourceUtils.CLASSPATH_URL_PREFIX)) {
+        location = location.substring(ResourceUtils.CLASSPATH_URL_PREFIX.length());
+      }
+      if (location.startsWith("/")) {
+        location = location.substring(1);
+      }
+      if (!location.isEmpty() && !location.endsWith("/")) {
+        location = location + "/";
+      }
+      parsedLocations.add(location);
+    }
+    return parsedLocations;
+  }
+
+  private static List<String> validateFilePrefixes(List<String> filePrefixes) {
+    for (String filePrefix : filePrefixes) {
+      if (filePrefix.contains("*")) {
+        throw new IllegalArgumentException("File prefix '" + filePrefix + "' cannot contain '*'");
+      }
+    }
+    return filePrefixes;
+  }
+
+  private static List<String> validateFileExtensions(List<String> fileExtensions) {
+    for (String fileExtension : fileExtensions) {
+      if (!fileExtension.startsWith(".")) {
+        throw new IllegalArgumentException("Extension '" + fileExtension + "' must start with '.'");
+      }
+    }
+    return fileExtensions;
   }
 
   /**
@@ -99,97 +151,109 @@ public class FilePatternResourceHintsRegistrar {
 
     private final List<String> fileExtensions = new ArrayList<>();
 
+    private Builder() {
+      // no-op
+    }
+
     /**
-     * Consider the specified classpath locations. A location can either be
-     * a special "classpath" pseudo location or a standard location, such as
-     * {@code com/example/resources}. An empty String represents the root of
-     * the classpath.
+     * Consider the specified classpath locations.
+     * <p>A location can either be a special {@value ResourceUtils#CLASSPATH_URL_PREFIX}
+     * pseudo location or a standard location, such as {@code com/example/resources}.
+     * An empty String represents the root of the classpath.
      *
      * @param classpathLocations the classpath locations to consider
      * @return this builder
+     * @see #withClasspathLocations(List)
      */
     public Builder withClasspathLocations(String... classpathLocations) {
+      return withClasspathLocations(Arrays.asList(classpathLocations));
+    }
+
+    /**
+     * Consider the specified classpath locations.
+     * <p>A location can either be a special {@value ResourceUtils#CLASSPATH_URL_PREFIX}
+     * pseudo location or a standard location, such as {@code com/example/resources}.
+     * An empty String represents the root of the classpath.
+     *
+     * @param classpathLocations the classpath locations to consider
+     * @return this builder
+     * @see #withClasspathLocations(String...)
+     */
+    public Builder withClasspathLocations(List<String> classpathLocations) {
       this.classpathLocations.addAll(validateClasspathLocations(classpathLocations));
       return this;
     }
 
     /**
      * Consider the specified file prefixes. Any file whose name starts with one
-     * of the specified prefix is considered. A prefix cannot contain the {@code *}
+     * of the specified prefixes is considered. A prefix cannot contain the {@code *}
      * character.
      *
      * @param filePrefixes the file prefixes to consider
      * @return this builder
+     * @see #withFilePrefixes(List)
      */
     public Builder withFilePrefixes(String... filePrefixes) {
+      return withFilePrefixes(Arrays.asList(filePrefixes));
+    }
+
+    /**
+     * Consider the specified file prefixes. Any file whose name starts with one
+     * of the specified prefixes is considered. A prefix cannot contain the {@code *}
+     * character.
+     *
+     * @param filePrefixes the file prefixes to consider
+     * @return this builder
+     * @see #withFilePrefixes(String...)
+     */
+    public Builder withFilePrefixes(List<String> filePrefixes) {
       this.filePrefixes.addAll(validateFilePrefixes(filePrefixes));
       return this;
     }
 
     /**
-     * Consider the specified file extensions. A file extension must starts with a
-     * {@code .} character..
+     * Consider the specified file extensions. A file extension must start with a
+     * {@code .} character.
      *
      * @param fileExtensions the file extensions to consider
      * @return this builder
+     * @see #withFileExtensions(List)
      */
     public Builder withFileExtensions(String... fileExtensions) {
+      return withFileExtensions(Arrays.asList(fileExtensions));
+    }
+
+    /**
+     * Consider the specified file extensions. A file extension must start with a
+     * {@code .} character.
+     *
+     * @param fileExtensions the file extensions to consider
+     * @return this builder
+     * @see #withFileExtensions(String...)
+     */
+    public Builder withFileExtensions(List<String> fileExtensions) {
       this.fileExtensions.addAll(validateFileExtensions(fileExtensions));
       return this;
     }
 
-    FilePatternResourceHintsRegistrar build() {
-      Assert.notEmpty(this.classpathLocations, "At least one location should be specified");
+    private FilePatternResourceHintsRegistrar build() {
       return new FilePatternResourceHintsRegistrar(this.filePrefixes,
               this.classpathLocations, this.fileExtensions);
     }
 
     /**
      * Register resource hints for the current state of this builder. For each
-     * classpath location that resolves against the {@code ClassLoader}, file
-     * starting with the configured file prefixes and extensions are registered.
+     * classpath location that resolves against the {@code ClassLoader}, files
+     * with the configured file prefixes and extensions are registered.
      *
      * @param hints the hints contributed so far for the deployment unit
-     * @param classLoader the classloader, or {@code null} if even the system ClassLoader isn't accessible
+     * @param classLoader the classloader, or {@code null} if even the system
+     * ClassLoader isn't accessible
      */
     public void registerHints(ResourceHints hints, @Nullable ClassLoader classLoader) {
       build().registerHints(hints, classLoader);
     }
 
-    private static List<String> validateClasspathLocations(String... locations) {
-      List<String> parsedLocations = new ArrayList<>();
-      for (String location : locations) {
-        if (location.startsWith(ResourceUtils.CLASSPATH_URL_PREFIX)) {
-          location = location.substring(ResourceUtils.CLASSPATH_URL_PREFIX.length());
-        }
-        if (location.startsWith("/")) {
-          location = location.substring(1);
-        }
-        if (!location.isEmpty() && !location.endsWith("/")) {
-          location = location + "/";
-        }
-        parsedLocations.add(location);
-      }
-      return parsedLocations;
-    }
-
-    private static List<String> validateFilePrefixes(String... fileNames) {
-      for (String name : fileNames) {
-        if (name.contains("*")) {
-          throw new IllegalArgumentException("File prefix '" + name + "' cannot contain '*'");
-        }
-      }
-      return Arrays.asList(fileNames);
-    }
-
-    private static List<String> validateFileExtensions(String... fileExtensions) {
-      for (String fileExtension : fileExtensions) {
-        if (!fileExtension.startsWith(".")) {
-          throw new IllegalArgumentException("Extension '" + fileExtension + "' should start with '.'");
-        }
-      }
-      return Arrays.asList(fileExtensions);
-    }
-
   }
+
 }
