5189ef87fcbe3ef091907075251cd287b0493859
Harry Yang <i.take.today@gmail.com>
2023-09-14 17:29:44 +0800

    :white_check_mark: ResponseEntityTests

--- today-web/src/test/java/cn/taketoday/http/ResponseEntityTests.java
+++ today-web/src/test/java/cn/taketoday/http/ResponseEntityTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -306,4 +303,11 @@ public class ResponseEntityTests {
     assertThat((int) responseEntity.getBody()).isEqualTo((int) entity);
   }
 
+  @Test
+  void problemDetail() {
+    ProblemDetail body = ProblemDetail.forRawStatusCode(400);
+    ResponseEntity<ProblemDetail> response = ResponseEntity.of(body)
+            .build();
+    assertThat(response.getBody()).isEqualTo(body);
+  }
 }
