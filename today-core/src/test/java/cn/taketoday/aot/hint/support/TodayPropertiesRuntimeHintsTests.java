6773098fa09df283d6da84276cf3d7f65eacf139
Harry Yang <i.take.today@gmail.com>
2023-09-22 20:00:10 +0800

    :art: Add resource hint for today.properties

---
+++ today-core/src/test/java/cn/taketoday/aot/hint/support/TodayPropertiesRuntimeHintsTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 - 2023 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.aot.hint.support;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cn.taketoday.aot.hint.RuntimeHints;
+import cn.taketoday.aot.hint.RuntimeHintsRegistrar;
+import cn.taketoday.aot.hint.predicate.RuntimeHintsPredicates;
+import cn.taketoday.lang.TodayStrategies;
+import cn.taketoday.util.ClassUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2023/9/22 19:58
+ */
+class TodayPropertiesRuntimeHintsTests {
+
+  private RuntimeHints hints;
+
+  @BeforeEach
+  void setup() {
+    this.hints = new RuntimeHints();
+    TodayStrategies.forLocation("META-INF/config/aot.factories")
+            .load(RuntimeHintsRegistrar.class).forEach(registrar -> registrar
+                    .registerHints(this.hints, ClassUtils.getDefaultClassLoader()));
+  }
+
+  @Test
+  void springPropertiesResourceHasHints() {
+    assertThat(RuntimeHintsPredicates.resource().forResource("today.properties")).accepts(this.hints);
+  }
+
+}
