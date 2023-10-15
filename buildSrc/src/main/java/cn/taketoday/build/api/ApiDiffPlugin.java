d8ce5327abce4b7bc0a1e1c3552fc28aca726d43
Harry Yang <i.take.today@gmail.com>
2023-10-15 20:11:32 +0800

    :art: Replace deprecated method getBuildDir()

--- buildSrc/src/main/java/cn/taketoday/build/api/ApiDiffPlugin.java
+++ buildSrc/src/main/java/cn/taketoday/build/api/ApiDiffPlugin.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -139,8 +136,9 @@ public class ApiDiffPlugin implements Plugin<Project> {
   }
 
   private File getOutputFile(String baseLineVersion, Project project) {
-    Path outDir = Paths.get(project.getRootProject().getBuildDir().getAbsolutePath(),
-            "reports", "api-diff",
+    String buildDirectoryPath = project.getRootProject()
+            .getLayout().getBuildDirectory().getAsFile().get().getAbsolutePath();
+    Path outDir = Paths.get(buildDirectoryPath, "reports", "api-diff",
             baseLineVersion + "_to_" + project.getRootProject().getVersion());
     return project.file(outDir.resolve(project.getName() + ".html").toString());
   }
