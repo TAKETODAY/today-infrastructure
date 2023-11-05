0cc018959a15db6ce486698b53de4854abb6a4c2
Harry Yang <i.take.today@gmail.com>
2023-11-05 20:42:37 +0800

    :sparkles: Support pattern matching for method names in ControlFlowPointcut
    Prior to this commit, ControlFlowPointcut supported a single method
    name which was matched exactly. Although it was possible to extend
    ControlFlowPointcut to add support for pattern matching, it was a bit
    cumbersome.
    
    To address that, this commit introduces built-in pattern matching
    support for method names in ControlFlowPointcut, analogous to the
    pattern matching support in NameMatchMethodPointcut.
    
    Specifically, a user can provide one or more method name patterns, and
    the patterns will be matched against candidate method names using OR
    semantics.
    
    By default, the matching algorithm delegates to
    StringUtils.simpleMatch(), but this can be overridden in
    subclasses by overriding the new protected isMatch() method.

--- today-aop/src/main/java/cn/taketoday/aop/support/ControlFlowPointcut.java
+++ today-aop/src/main/java/cn/taketoday/aop/support/ControlFlowPointcut.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,6 +21,9 @@ import org.aopalliance.intercept.MethodInvocation;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import cn.taketoday.aop.ClassFilter;
@@ -31,7 +31,7 @@ import cn.taketoday.aop.MethodMatcher;
 import cn.taketoday.aop.Pointcut;
 import cn.taketoday.lang.Assert;
 import cn.taketoday.lang.Nullable;
-import cn.taketoday.util.ObjectUtils;
+import cn.taketoday.util.StringUtils;
 
 /**
  * Pointcut and method matcher for use in simple <b>cflow</b>-style pointcut.
@@ -48,38 +48,76 @@ import cn.taketoday.util.ObjectUtils;
 @SuppressWarnings("serial")
 public class ControlFlowPointcut implements Pointcut, ClassFilter, MethodMatcher, Serializable {
 
-  private final Class<?> clazz;
-
-  @Nullable
-  private final String methodName;
-
-  private final AtomicInteger evaluations = new AtomicInteger();
+  /**
+   * The class against which to match.
+   */
+  protected final Class<?> clazz;
 
   /**
-   * Construct a new pointcut that matches all control flows below that class.
+   * An immutable list of distinct method name patterns against which to match.
+   */
+  protected final List<String> methodNamePatterns;
+
+  private final AtomicInteger evaluationCount = new AtomicInteger();
+
+  /**
+   * Construct a new pointcut that matches all control flows below the given class.
    *
-   * @param clazz the clazz
+   * @param clazz the class
    */
   public ControlFlowPointcut(Class<?> clazz) {
-    this(clazz, null);
+    this(clazz, (String) null);
   }
 
   /**
-   * Construct a new pointcut that matches all calls below the given method
-   * in the given class. If no method name is given, matches all control flows
+   * Construct a new pointcut that matches all calls below a method matching
+   * the given method name pattern in the given class.
+   * <p>If no method name pattern is given, the pointcut matches all control flows
    * below the given class.
    *
-   * @param clazz the clazz
-   * @param methodName the name of the method (may be {@code null})
+   * @param clazz the class
+   * @param methodNamePattern the method name pattern (may be {@code null})
    */
-  public ControlFlowPointcut(Class<?> clazz, @Nullable String methodName) {
-    Assert.notNull(clazz, "Class must not be null");
+  public ControlFlowPointcut(Class<?> clazz, @Nullable String methodNamePattern) {
+    Assert.notNull(clazz, "Class is required");
     this.clazz = clazz;
-    this.methodName = methodName;
+    this.methodNamePatterns = (methodNamePattern != null ?
+                               Collections.singletonList(methodNamePattern) : Collections.emptyList());
+  }
+
+  /**
+   * Construct a new pointcut that matches all calls below a method matching
+   * one of the given method name patterns in the given class.
+   * <p>If no method name pattern is given, the pointcut matches all control flows
+   * below the given class.
+   *
+   * @param clazz the class
+   * @param methodNamePatterns the method name patterns (potentially empty)
+   */
+  public ControlFlowPointcut(Class<?> clazz, String... methodNamePatterns) {
+    this(clazz, Arrays.asList(methodNamePatterns));
+  }
+
+  /**
+   * Construct a new pointcut that matches all calls below a method matching
+   * one of the given method name patterns in the given class.
+   * <p>If no method name pattern is given, the pointcut matches all control flows
+   * below the given class.
+   *
+   * @param clazz the class
+   * @param methodNamePatterns the method name patterns (potentially empty)
+   */
+  public ControlFlowPointcut(Class<?> clazz, List<String> methodNamePatterns) {
+    Assert.notNull(clazz, "Class is required");
+    Assert.notNull(methodNamePatterns, "List of method name patterns is required");
+    Assert.noNullElements(methodNamePatterns, "List of method name patterns must not contain null elements");
+    this.clazz = clazz;
+    this.methodNamePatterns = methodNamePatterns.stream().distinct().toList();
   }
 
   /**
    * Subclasses can override this for greater filtering (and performance).
+   * <p>The default implementation always returns {@code true}.
    */
   @Override
   public boolean matches(Class<?> clazz) {
@@ -88,6 +126,7 @@ public class ControlFlowPointcut implements Pointcut, ClassFilter, MethodMatcher
 
   /**
    * Subclasses can override this if it's possible to filter out some candidate classes.
+   * <p>The default implementation always returns {@code true}.
    */
   @Override
   public boolean matches(Method method, Class<?> targetClass) {
@@ -101,22 +140,81 @@ public class ControlFlowPointcut implements Pointcut, ClassFilter, MethodMatcher
 
   @Override
   public boolean matches(MethodInvocation invocation) {
-    this.evaluations.incrementAndGet();
+    incrementEvaluationCount();
 
     for (StackTraceElement element : new Throwable().getStackTrace()) {
-      if (element.getClassName().equals(this.clazz.getName()) &&
-              (this.methodName == null || element.getMethodName().equals(this.methodName))) {
-        return true;
+      if (element.getClassName().equals(this.clazz.getName())) {
+        if (this.methodNamePatterns.isEmpty()) {
+          return true;
+        }
+        String methodName = element.getMethodName();
+        for (int i = 0; i < this.methodNamePatterns.size(); i++) {
+          if (isMatch(methodName, i)) {
+            return true;
+          }
+        }
       }
     }
     return false;
   }
 
   /**
-   * It's useful to know how many times we've fired, for optimization.
+   * Get the number of times {@link #matches(MethodInvocation)} has been
+   * evaluated.
+   * <p>Useful for optimization and testing purposes.
    */
   public int getEvaluations() {
-    return this.evaluations.get();
+    return this.evaluationCount.get();
+  }
+
+  /**
+   * Increment the {@link #getEvaluations() evaluation count}.
+   *
+   * @see #matches(MethodInvocation)
+   */
+  protected final void incrementEvaluationCount() {
+    this.evaluationCount.incrementAndGet();
+  }
+
+  /**
+   * Determine if the given method name matches the method name pattern at the
+   * specified index.
+   * <p>This method is invoked by {@link #matches(MethodInvocation)}.
+   * <p>The default implementation retrieves the method name pattern from
+   * {@link #methodNamePatterns} and delegates to {@link #isMatch(String, String)}.
+   * <p>Can be overridden in subclasses &mdash; for example, to support
+   * regular expressions.
+   *
+   * @param methodName the method name to check
+   * @param patternIndex the index of the method name pattern
+   * @return {@code true} if the method name matches the pattern at the specified
+   * index
+   * @see #methodNamePatterns
+   * @see #isMatch(String, String)
+   * @see #matches(MethodInvocation)
+   */
+  protected boolean isMatch(String methodName, int patternIndex) {
+    String methodNamePattern = this.methodNamePatterns.get(patternIndex);
+    return isMatch(methodName, methodNamePattern);
+  }
+
+  /**
+   * Determine if the given method name matches the method name pattern.
+   * <p>This method is invoked by {@link #isMatch(String, int)}.
+   * <p>The default implementation checks for direct equality as well as
+   * {@code xxx*}, {@code *xxx}, {@code *xxx*}, and {@code xxx*yyy} matches.
+   * <p>Can be overridden in subclasses &mdash; for example, to support a
+   * different style of simple pattern matching.
+   *
+   * @param methodName the method name to check
+   * @param methodNamePattern the method name pattern
+   * @return {@code true} if the method name matches the pattern
+   * @see #isMatch(String, int)
+   * @see cn.taketoday.util.StringUtils#simpleMatch(String, String)
+   */
+  protected boolean isMatch(String methodName, String methodNamePattern) {
+    return methodName.equals(methodNamePattern)
+            || StringUtils.simpleMatch(methodNamePattern, methodName);
   }
 
   @Override
@@ -131,27 +229,20 @@ public class ControlFlowPointcut implements Pointcut, ClassFilter, MethodMatcher
 
   @Override
   public boolean equals(@Nullable Object other) {
-    if (this == other) {
-      return true;
-    }
-    if (!(other instanceof ControlFlowPointcut that)) {
-      return false;
-    }
-    return (this.clazz.equals(that.clazz)) && ObjectUtils.nullSafeEquals(this.methodName, that.methodName);
+    return (this == other || (other instanceof ControlFlowPointcut that &&
+            this.clazz.equals(that.clazz)) && this.methodNamePatterns.equals(that.methodNamePatterns));
   }
 
   @Override
   public int hashCode() {
     int code = this.clazz.hashCode();
-    if (this.methodName != null) {
-      code = 37 * code + this.methodName.hashCode();
-    }
+    code = 37 * code + this.methodNamePatterns.hashCode();
     return code;
   }
 
   @Override
   public String toString() {
-    return getClass().getName() + ": class = " + this.clazz.getName() + "; methodName = " + this.methodName;
+    return getClass().getName() + ": class = " + this.clazz.getName() + "; methodNamePatterns = " + this.methodNamePatterns;
   }
 
 }
