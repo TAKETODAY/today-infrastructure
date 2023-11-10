48055ce519ae7bdc8340b0a24babdb4fdbb4d127
Harry Yang <i.take.today@gmail.com>
2023-11-10 12:56:49 +0800

    :art: Add SAP HANA duplicate key exception error code

--- today-jdbc/src/main/java/cn/taketoday/jdbc/support/SQLStateSQLExceptionTranslator.java
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/support/SQLStateSQLExceptionTranslator.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +18,6 @@
 package cn.taketoday.jdbc.support;
 
 import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.Set;
 
 import cn.taketoday.dao.CannotAcquireLockException;
@@ -47,46 +43,57 @@ import cn.taketoday.lang.Nullable;
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @author Thomas Risberg
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @see java.sql.SQLException#getSQLState()
  * @see SQLErrorCodeSQLExceptionTranslator
+ * @since 4.0
  */
 public class SQLStateSQLExceptionTranslator extends AbstractFallbackSQLExceptionTranslator {
 
-  private static final Set<String> BAD_SQL_GRAMMAR_CODES = new HashSet<>(8);
-  private static final Set<String> PESSIMISTIC_LOCKING_FAILURE_CODES = Set.of(
-          "40", // Transaction rollback
-          "61" // Oracle: deadlock
+  private static final Set<String> BAD_SQL_GRAMMAR_CODES = Set.of(
+          "07",  // Dynamic SQL error
+          "21",  // Cardinality violation
+          "2A",  // Syntax error direct SQL
+          "37",  // Syntax error dynamic SQL
+          "42",  // General SQL syntax error
+          "65"   // Oracle: unknown identifier
   );
 
-  private static final Set<String> DATA_INTEGRITY_VIOLATION_CODES = new HashSet<>(8);
-  private static final Set<String> DATA_ACCESS_RESOURCE_FAILURE_CODES = new HashSet<>(8);
+  private static final Set<String> DATA_INTEGRITY_VIOLATION_CODES = Set.of(
+          "01",  // Data truncation
+          "02",  // No data found
+          "22",  // Value out of range
+          "23",  // Integrity constraint violation
+          "27",  // Triggered data change violation
+          "44"   // With check violation
+  );
+
+  private static final Set<String> DATA_ACCESS_RESOURCE_FAILURE_CODES = Set.of(
+          "08",  // Connection exception
+          "53",  // PostgreSQL: insufficient resources (e.g. disk full)
+          "54",  // PostgreSQL: program limit exceeded (e.g. statement too complex)
+          "57",  // DB2: out-of-memory exception / database not started
+          "58"   // DB2: unexpected system error
+  );
+
   private static final Set<String> TRANSIENT_DATA_ACCESS_RESOURCE_CODES = Set.of(
           "JW",  // Sybase: internal I/O error
           "JZ",  // Sybase: unexpected I/O error
-          "S1"  // DB2: communication failure
+          "S1"   // DB2: communication failure
   );
 
-  static {
-    BAD_SQL_GRAMMAR_CODES.add("07");  // Dynamic SQL error
-    BAD_SQL_GRAMMAR_CODES.add("21");  // Cardinality violation
-    BAD_SQL_GRAMMAR_CODES.add("2A");  // Syntax error direct SQL
-    BAD_SQL_GRAMMAR_CODES.add("37");  // Syntax error dynamic SQL
-    BAD_SQL_GRAMMAR_CODES.add("42");  // General SQL syntax error
-    BAD_SQL_GRAMMAR_CODES.add("65");  // Oracle: unknown identifier
+  private static final Set<String> PESSIMISTIC_LOCKING_FAILURE_CODES = Set.of(
+          "40",  // Transaction rollback
+          "61"   // Oracle: deadlock
+  );
 
-    DATA_INTEGRITY_VIOLATION_CODES.add("01");  // Data truncation
-    DATA_INTEGRITY_VIOLATION_CODES.add("02");  // No data found
-    DATA_INTEGRITY_VIOLATION_CODES.add("22");  // Value out of range
-    DATA_INTEGRITY_VIOLATION_CODES.add("23");  // Integrity constraint violation
-    DATA_INTEGRITY_VIOLATION_CODES.add("27");  // Triggered data change violation
-    DATA_INTEGRITY_VIOLATION_CODES.add("44");  // With check violation
-
-    DATA_ACCESS_RESOURCE_FAILURE_CODES.add("08");  // Connection exception
-    DATA_ACCESS_RESOURCE_FAILURE_CODES.add("53");  // PostgreSQL: insufficient resources (e.g. disk full)
-    DATA_ACCESS_RESOURCE_FAILURE_CODES.add("54");  // PostgreSQL: program limit exceeded (e.g. statement too complex)
-    DATA_ACCESS_RESOURCE_FAILURE_CODES.add("57");  // DB2: out-of-memory exception / database not started
-    DATA_ACCESS_RESOURCE_FAILURE_CODES.add("58");  // DB2: unexpected system error
-  }
+  private static final Set<Integer> DUPLICATE_KEY_ERROR_CODES = Set.of(
+          1,     // Oracle
+          301,   // SAP HANA
+          1062,  // MySQL/MariaDB
+          2601,  // MS SQL Server
+          2627   // MS SQL Server
+  );
 
   @Override
   @Nullable
@@ -114,7 +121,7 @@ public class SQLStateSQLExceptionTranslator extends AbstractFallbackSQLException
         return new TransientDataAccessResourceException(buildMessage(task, sql, ex), ex);
       }
       else if (PESSIMISTIC_LOCKING_FAILURE_CODES.contains(classCode)) {
-        if ("40001".equals(sqlState)) {
+        if (indicatesCannotAcquireLock(sqlState)) {
           return new CannotAcquireLockException(buildMessage(task, sql, ex), ex);
         }
         return new PessimisticLockingFailureException(buildMessage(task, sql, ex), ex);
@@ -153,19 +160,27 @@ public class SQLStateSQLExceptionTranslator extends AbstractFallbackSQLException
   }
 
   /**
-   * Check whether the given SQL state (and the associated error code in case
-   * of a generic SQL state value) indicate a duplicate key exception:
+   * Check whether the given SQL state and the associated error code (in case
+   * of a generic SQL state value) indicate a {@link DuplicateKeyException}:
    * either SQL state 23505 as a specific indication, or the generic SQL state
-   * 23000 with well-known vendor codes (1 for Oracle, 1062 for MySQL/MariaDB,
-   * 2601/2627 for MS SQL Server).
+   * 23000 with a well-known vendor code.
    *
    * @param sqlState the SQL state value
-   * @param errorCode the error code value
+   * @param errorCode the error code
    */
   static boolean indicatesDuplicateKey(@Nullable String sqlState, int errorCode) {
     return ("23505".equals(sqlState) ||
-            ("23000".equals(sqlState) &&
-                    (errorCode == 1 || errorCode == 1062 || errorCode == 2601 || errorCode == 2627)));
+            ("23000".equals(sqlState) && DUPLICATE_KEY_ERROR_CODES.contains(errorCode)));
+  }
+
+  /**
+   * Check whether the given SQL state indicates a {@link CannotAcquireLockException},
+   * with SQL state 40001 as a specific indication.
+   *
+   * @param sqlState the SQL state value
+   */
+  static boolean indicatesCannotAcquireLock(@Nullable String sqlState) {
+    return "40001".equals(sqlState);
   }
 
 }
