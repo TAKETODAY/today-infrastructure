051c440b46949595464a9b616109614c1bd61149
Harry Yang <i.take.today@gmail.com>
2023-11-22 20:50:25 +0800

    :art:

--- today-aop/src/test/java/cn/taketoday/aop/target/HotSwappableTargetSourceTests.java
+++ today-aop/src/test/java/cn/taketoday/aop/target/HotSwappableTargetSourceTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -113,7 +110,7 @@ public class HotSwappableTargetSourceTests {
     HotSwappableTargetSource swapper = (HotSwappableTargetSource) beanFactory.getBean("swapper");
     assertThatIllegalArgumentException().as("Shouldn't be able to swap to invalid value").isThrownBy(() ->
                     swapper.swap(null))
-            .withMessageContaining("null");
+            .withMessageContaining("is required");
     // It shouldn't be corrupted, it should still work
     testBasicFunctionality();
   }
