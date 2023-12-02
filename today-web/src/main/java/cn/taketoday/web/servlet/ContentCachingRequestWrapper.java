e5e02ddc4c8635588b04036bc7917d85d16724b2
Harry Yang <i.take.today@gmail.com>
2023-12-02 21:25:21 +0800

    :art: Avoid byte array copy in getContentAsString
    The getContentAsString method was originally added in d9b8826 to avoid
    the extra copying inherent to calling ByteArrayOutputStream.toByteArray;
    however, in f83c609 the class was updated to instead use
    FastByteArrayOutputStream, and in the process the extra copy was brought
    back when getContentAsString was changed to call toByteArray.
    
    Switch to calling toByteArrayUnsafe, a method provided by
    FastByteArrayOutputStream, which avoids the extra copy; since we
    immediately pass the byte array to the String constructor, and it isn't
    accessed anywhere else, the usage is safe.

--- today-web/src/main/java/cn/taketoday/web/servlet/ContentCachingRequestWrapper.java
+++ today-web/src/main/java/cn/taketoday/web/servlet/ContentCachingRequestWrapper.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.web.servlet;
@@ -200,7 +200,7 @@ public class ContentCachingRequestWrapper extends HttpServletRequestWrapper {
    * @see #getContentAsByteArray()
    */
   public String getContentAsString() {
-    return new String(cachedContent.toByteArray(), Charset.forName(getCharacterEncoding()));
+    return new String(cachedContent.toByteArrayUnsafe(), Charset.forName(getCharacterEncoding()));
   }
 
   /**
