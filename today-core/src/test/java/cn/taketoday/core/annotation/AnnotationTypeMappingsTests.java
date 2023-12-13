9d3a9363c84644adf44b929119d87c8578a788b9
Harry Yang <i.take.today@gmail.com>
2023-12-13 19:37:53 +0800

    :art:

--- today-core/src/test/java/cn/taketoday/core/annotation/AnnotationTypeMappingsTests.java
+++ today-core/src/test/java/cn/taketoday/core/annotation/AnnotationTypeMappingsTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.core.annotation;
@@ -40,7 +37,6 @@ import javax.annotation.Nullable;
 
 import cn.taketoday.core.annotation.AnnotationTypeMapping.MirrorSets;
 import cn.taketoday.core.annotation.AnnotationTypeMapping.MirrorSets.MirrorSet;
-import cn.taketoday.lang.UsesSunMisc;
 import cn.taketoday.util.ReflectionUtils;
 
 import static java.util.stream.Collectors.toList;
