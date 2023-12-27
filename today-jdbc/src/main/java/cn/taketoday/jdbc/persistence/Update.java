22c52b5910227932eb932e184ed1eeabaa303322
Harry Yang <i.take.today@gmail.com>
2023-12-27 16:03:58 +0800

    :zap: Update

--- today-jdbc/src/main/java/cn/taketoday/jdbc/persistence/Update.java
+++ today-jdbc/src/main/java/cn/taketoday/jdbc/persistence/Update.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2023 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.jdbc.persistence;
@@ -191,7 +188,8 @@ public class Update {
     var iter = columns.entrySet().iterator();
     while (iter.hasNext()) {
       Map.Entry<String, String> e = iter.next();
-      buf.append(e.getKey()).append('=').append(e.getValue());
+      buf.append('`').append(e.getKey()).append('`')
+              .append('=').append(e.getValue());
       if (iter.hasNext()) {
         buf.append(", ");
       }
