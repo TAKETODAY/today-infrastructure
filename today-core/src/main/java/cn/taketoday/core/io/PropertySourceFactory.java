28e82b86c007479448353c7110ee7737559a688f
Harry Yang <i.take.today@gmail.com>
2024-01-04 20:00:20 +0800

    :art: Recommend ResourcePropertySource for distinct PropertySource names

--- today-core/src/main/java/cn/taketoday/core/io/PropertySourceFactory.java
+++ today-core/src/main/java/cn/taketoday/core/io/PropertySourceFactory.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 package cn.taketoday.core.io;
 
@@ -28,14 +25,22 @@ import cn.taketoday.lang.Nullable;
  * Strategy interface for creating resource-based {@link PropertySource} wrappers.
  *
  * @author Juergen Hoeller
- * @author TODAY 2021/10/28 17:34
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @see DefaultPropertySourceFactory
- * @since 4.0
+ * @since 4.0 2021/10/28 17:34
  */
 public interface PropertySourceFactory {
 
   /**
    * Create a {@link PropertySource} that wraps the given resource.
+   * <p>Implementations will typically create {@link ResourcePropertySource}
+   * instances, with {@link PropertySourceProcessor} automatically adapting
+   * property source names via {@link ResourcePropertySource#withResourceName()}
+   * if necessary, e.g. when combining multiple sources for the same name
+   * into a {@link cn.taketoday.core.env.CompositePropertySource}.
+   * Custom implementations with custom {@link PropertySource} types need
+   * to make sure to expose distinct enough names, possibly deriving from
+   * {@link ResourcePropertySource} where possible.
    *
    * @param name the name of the property source
    * (can be {@code null} in which case the factory implementation
