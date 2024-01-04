923ae2cc99a521455b684fc5fc28bea5bd6da718
Harry Yang <i.take.today@gmail.com>
2024-01-04 20:02:30 +0800

    :art: Document target method expectations for ReactiveTransactionManager

--- today-tx/src/main/java/cn/taketoday/transaction/annotation/Transactional.java
+++ today-tx/src/main/java/cn/taketoday/transaction/annotation/Transactional.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.transaction.annotation;
@@ -109,10 +106,16 @@ import cn.taketoday.transaction.TransactionDefinition;
  * all participating data access operations need to execute within the same
  * Reactor context in the same reactive pipeline.
  *
+ * <p><b>Note: When configured with a {@code ReactiveTransactionManager}, all
+ * transaction-demarcated methods are expected to return a reactive pipeline.</b>
+ * Void methods or regular return types need to be associated with a regular
+ * {@code PlatformTransactionManager}, e.g. through {@link #transactionManager()}.
+ *
  * @author Colin Sampaleanu
  * @author Juergen Hoeller
  * @author Sam Brannen
  * @author Mark Paluch
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @see cn.taketoday.transaction.interceptor.TransactionAttribute
  * @see cn.taketoday.transaction.interceptor.DefaultTransactionAttribute
  * @see cn.taketoday.transaction.interceptor.RuleBasedTransactionAttribute
--- today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAspectSupport.java
+++ today-tx/src/main/java/cn/taketoday/transaction/interceptor/TransactionAspectSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.transaction.interceptor;
@@ -325,8 +325,8 @@ public abstract class TransactionAspectSupport implements BeanFactoryAware, Init
         Class<?> reactiveType = key.getReturnType();
         ReactiveAdapter adapter = reactiveAdapterRegistry.getAdapter(reactiveType);
         if (adapter == null) {
-          throw new IllegalStateException(
-                  "Cannot apply reactive transaction to non-reactive return type: " + key.getReturnType());
+          throw new IllegalStateException("Cannot apply reactive transaction to non-reactive return type [" +
+                  method.getReturnType() + "] with specified transaction manager: " + tm);
         }
         return new ReactiveTransactionSupport(adapter);
       });
