78d989487d8e682e2bd182248870de7a69ba5969
Harry Yang <i.take.today@gmail.com>
2024-01-20 20:35:12 +0800

    :art:

--- today-web/src/main/java/cn/taketoday/web/config/InterceptorRegistration.java
+++ today-web/src/main/java/cn/taketoday/web/config/InterceptorRegistration.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.web.config;
@@ -24,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import cn.taketoday.core.Ordered;
 import cn.taketoday.lang.Assert;
 import cn.taketoday.lang.Nullable;
 import cn.taketoday.util.StringUtils;
@@ -36,10 +34,10 @@ import cn.taketoday.web.util.pattern.PathPattern;
  *
  * @author Rossen Stoyanchev
  * @author Keith Donald
- * @author TODAY 2021/8/30 21:38
- * @since 4.0
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 4.0 2021/8/30 21:38
  */
-public class InterceptorRegistration {
+public class InterceptorRegistration implements Ordered {
 
   private final HandlerInterceptor interceptor;
 
@@ -108,7 +106,8 @@ public class InterceptorRegistration {
   /**
    * Return the order position to be used.
    */
-  protected int getOrder() {
+  @Override
+  public int getOrder() {
     return this.order;
   }
 
