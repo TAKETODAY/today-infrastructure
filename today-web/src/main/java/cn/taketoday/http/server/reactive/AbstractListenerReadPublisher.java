3255b686f84b1c3c190ed62521f31b057d6cb508
Harry Yang <i.take.today@gmail.com>
2024-01-21 20:26:02 +0800

    :art: Improve cancel handling in AbstractListenerReadPublisher

--- today-web/src/main/java/cn/taketoday/http/server/reactive/AbstractListenerReadPublisher.java
+++ today-web/src/main/java/cn/taketoday/http/server/reactive/AbstractListenerReadPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.http.server.reactive;
@@ -46,6 +46,7 @@ import reactor.core.publisher.Operators;
  * @author Arjen Poutsma
  * @author Violeta Georgieva
  * @author Rossen Stoyanchev
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0
  */
 public abstract class AbstractListenerReadPublisher<T> implements Publisher<T> {
@@ -352,6 +353,12 @@ public abstract class AbstractListenerReadPublisher<T> implements Publisher<T> {
         publisher.errorPending = ex;
         publisher.handlePendingCompletionOrError();
       }
+
+      @Override
+      <T> void cancel(AbstractListenerReadPublisher<T> publisher) {
+        publisher.completionPending = true;
+        publisher.handlePendingCompletionOrError();
+      }
     },
 
     NO_DEMAND {
@@ -422,6 +429,14 @@ public abstract class AbstractListenerReadPublisher<T> implements Publisher<T> {
         publisher.errorPending = ex;
         publisher.handlePendingCompletionOrError();
       }
+
+      @Override
+      <T> void cancel(AbstractListenerReadPublisher<T> publisher) {
+        publisher.discardData();
+        publisher.completionPending = true;
+        publisher.handlePendingCompletionOrError();
+      }
+      
     },
 
     COMPLETED {
