7346da2eb290330cf48305903bd7139eca4798ed
Harry Yang <i.take.today@gmail.com>
2024-02-01 21:14:06 +0800

    :art: 优化代码

--- today-context/src/test/java/cn/taketoday/context/annotation/configuration/BeanAnnotationAttributePropagationTests.java
+++ today-context/src/test/java/cn/taketoday/context/annotation/configuration/BeanAnnotationAttributePropagationTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2021 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,15 +12,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.context.annotation.configuration;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import cn.taketoday.context.annotation.Primary;
 import cn.taketoday.beans.factory.config.BeanDefinition;
 import cn.taketoday.beans.factory.support.AbstractBeanDefinition;
 import cn.taketoday.beans.factory.support.RootBeanDefinition;
@@ -33,6 +28,7 @@ import cn.taketoday.context.annotation.Configuration;
 import cn.taketoday.context.annotation.ConfigurationClassPostProcessor;
 import cn.taketoday.context.annotation.DependsOn;
 import cn.taketoday.context.annotation.Lazy;
+import cn.taketoday.context.annotation.Primary;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
