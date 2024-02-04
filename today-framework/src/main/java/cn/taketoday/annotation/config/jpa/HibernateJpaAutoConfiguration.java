e8921289cd232e31f036944c113dc97525e0064f
Harry Yang <i.take.today@gmail.com>
2024-02-04 20:35:22 +0800

    :art: Auto-configure Hibernate JPA before DataSource TransactionManager

--- today-framework/src/main/java/cn/taketoday/annotation/config/jpa/HibernateJpaAutoConfiguration.java
+++ today-framework/src/main/java/cn/taketoday/annotation/config/jpa/HibernateJpaAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.annotation.config.jpa;
@@ -20,6 +20,7 @@ package cn.taketoday.annotation.config.jpa;
 import org.hibernate.engine.spi.SessionImplementor;
 
 import cn.taketoday.annotation.config.jdbc.DataSourceAutoConfiguration;
+import cn.taketoday.annotation.config.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import cn.taketoday.annotation.config.transaction.TransactionAutoConfiguration;
 import cn.taketoday.context.annotation.Import;
 import cn.taketoday.context.annotation.config.DisableDIAutoConfiguration;
@@ -36,9 +37,12 @@ import jakarta.persistence.EntityManager;
  * @author Josh Long
  * @author Manuel Doninger
  * @author Andy Wilkinson
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 4.0
  */
-@DisableDIAutoConfiguration(after = DataSourceAutoConfiguration.class, before = TransactionAutoConfiguration.class)
+@DisableDIAutoConfiguration(
+        after = { DataSourceAutoConfiguration.class },
+        before = { TransactionAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @ConditionalOnClass({ LocalContainerEntityManagerFactoryBean.class, EntityManager.class, SessionImplementor.class })
 @EnableConfigurationProperties(JpaProperties.class)
 @Import(HibernateJpaConfiguration.class)
