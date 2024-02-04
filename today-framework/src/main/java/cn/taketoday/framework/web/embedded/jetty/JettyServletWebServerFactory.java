dd8795ee79df045d6935d02a3ce339784969340f
Harry Yang <i.take.today@gmail.com>
2024-02-04 22:27:41 +0800

    :bug: Fix Jetty ConnectionLimit configuration

--- today-framework/src/main/java/cn/taketoday/framework/web/embedded/jetty/JettyServletWebServerFactory.java
+++ today-framework/src/main/java/cn/taketoday/framework/web/embedded/jetty/JettyServletWebServerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.framework.web.embedded.jetty;
@@ -180,7 +180,7 @@ public class JettyServletWebServerFactory extends AbstractServletWebServerFactor
     this.logger.info("Server initialized with port: {}", port);
 
     if (this.maxConnections > -1) {
-      server.addBean(new ConnectionLimit(this.maxConnections, server));
+      server.addBean(new ConnectionLimit(this.maxConnections, server.getConnectors()));
     }
 
     if (Ssl.isEnabled(getSsl())) {
