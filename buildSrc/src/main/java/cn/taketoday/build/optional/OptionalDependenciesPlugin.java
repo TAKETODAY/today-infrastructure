c9d74f829f5efaac5e054d2bf1197dcba4b3c70b
Harry Yang <i.take.today@gmail.com>
2024-02-13 23:09:24 +0800

    :art:

--- buildSrc/src/main/java/cn/taketoday/build/optional/OptionalDependenciesPlugin.java
+++ buildSrc/src/main/java/cn/taketoday/build/optional/OptionalDependenciesPlugin.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.build.optional;
@@ -23,6 +20,7 @@ package cn.taketoday.build.optional;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.SourceSetContainer;
@@ -46,10 +44,10 @@ public class OptionalDependenciesPlugin implements Plugin<Project> {
 
   @Override
   public void apply(Project project) {
-    Configuration optional = project.getConfigurations().create("optional");
+    Configuration optional = project.getConfigurations().create(OPTIONAL_CONFIGURATION_NAME);
     optional.setCanBeConsumed(false);
     optional.setCanBeResolved(false);
-    project.getPlugins().withType(JavaPlugin.class, (javaPlugin) -> {
+    project.getPlugins().withType(JavaBasePlugin.class, (javaBasePlugin) -> {
       SourceSetContainer sourceSets = project.getExtensions().getByType(JavaPluginExtension.class)
               .getSourceSets();
       sourceSets.all((sourceSet) -> {
