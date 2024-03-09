358e0bf84db42644bec8dab57ce25b9270581b4b
Harry Yang <i.take.today@gmail.com>
2024-03-09 19:36:05 +0800

    :art:

--- today-web/src/test/java/cn/taketoday/web/handler/method/ModelHandlerTests.java
+++ today-web/src/test/java/cn/taketoday/web/handler/method/ModelHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.web.handler.method;
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import cn.taketoday.beans.testfixture.beans.TestBean;
 import cn.taketoday.context.annotation.AnnotationConfigApplicationContext;
 import cn.taketoday.lang.Nullable;
 import cn.taketoday.session.WebSessionRequiredException;
@@ -302,7 +303,7 @@ class ModelHandlerTests {
     return new InvocableHandlerMethod(this.controller, method, parameterFactory);
   }
 
-  @SessionAttributes({ "sessionAttr", "foo" })
+  @SessionAttributes(names = { "sessionAttr", "foo" }, types = TestBean.class)
   static class TestController {
 
     @ModelAttribute
