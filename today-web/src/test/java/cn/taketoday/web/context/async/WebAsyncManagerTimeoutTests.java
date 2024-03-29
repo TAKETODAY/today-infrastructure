dc99c7add652b185b25acd7f99d94280a0bde40a
Harry Yang <i.take.today@gmail.com>
2024-03-29 23:20:35 +0800

    :white_check_mark:

--- today-web/src/test/java/cn/taketoday/web/context/async/WebAsyncManagerTimeoutTests.java
+++ today-web/src/test/java/cn/taketoday/web/context/async/WebAsyncManagerTimeoutTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.web.context.async;
@@ -24,9 +21,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 import cn.taketoday.core.task.AsyncTaskExecutor;
+import cn.taketoday.util.concurrent.Future;
 import cn.taketoday.web.RequestContext;
 import cn.taketoday.web.servlet.ServletRequestContext;
 import cn.taketoday.web.testfixture.servlet.MockAsyncContext;
