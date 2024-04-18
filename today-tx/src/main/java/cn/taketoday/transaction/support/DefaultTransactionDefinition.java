a1fcd4e120ede5455a39651fc12207cf20ea5e6e
Harry Yang <i.take.today@gmail.com>
2024-04-18 10:23:15 +0800

    :art: Fix transaction definition setTimeout error message since zero is valid
    This commit rephrases the `DefaultTransactionDefinition#setTimeout`
    exception message to better reflect the fact that 0 is a valid input
    value.
    
    Even though this often leads to a transaction immediately timing out
    after opening, there is one notable case where this has another
    meaningful effect: in Jakarta transactions (`UserTransaction`), when
    passing 0 "the transaction service restores the default value".

--- today-tx/src/main/java/cn/taketoday/transaction/support/DefaultTransactionDefinition.java
+++ today-tx/src/main/java/cn/taketoday/transaction/support/DefaultTransactionDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.transaction.support;
@@ -236,7 +236,7 @@ public class DefaultTransactionDefinition implements TransactionDefinition, Seri
    */
   public final void setTimeout(int timeout) {
     if (timeout < TIMEOUT_DEFAULT) {
-      throw new IllegalArgumentException("Timeout must be a positive integer or TIMEOUT_DEFAULT");
+      throw new IllegalArgumentException("Timeout must be a non-negative integer or TIMEOUT_DEFAULT");
     }
     this.timeout = timeout;
   }
