48815f3f6720165a2f9607f72a59495282c6be77
Harry Yang <i.take.today@gmail.com>
2024-05-03 09:48:13 +0800

    :art:

---
+++ today-websocket/src/main/java/cn/taketoday/web/socket/client/support/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 - 2024 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+/**
+ * Client-side supports for WebSocket applications.
+ *
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 5.0 2024/5/3 09:29
+ */
+@NonNullApi
+@NonNullFields
+package cn.taketoday.web.socket.client.support;
+
+import cn.taketoday.lang.NonNullApi;
+import cn.taketoday.lang.NonNullFields;
