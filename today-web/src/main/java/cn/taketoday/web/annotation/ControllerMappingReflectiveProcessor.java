ed3097a88deff49bc2669cff7d97b592b04a7deb
Harry Yang <i.take.today@gmail.com>
2024-05-03 22:39:33 +0800

    :art: Review usage of BindingReflectionHintsRegistrar#registerReflectionHints

--- today-web/src/main/java/cn/taketoday/web/annotation/ControllerMappingReflectiveProcessor.java
+++ today-web/src/main/java/cn/taketoday/web/annotation/ControllerMappingReflectiveProcessor.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © Harry Yang & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.web.annotation;
@@ -89,7 +86,10 @@ class ControllerMappingReflectiveProcessor implements ReflectiveProcessor {
       this.bindingRegistrar.registerReflectionHints(hints, methodParameter.getGenericParameterType());
     }
     else if (HttpEntity.class.isAssignableFrom(methodParameter.getParameterType())) {
-      this.bindingRegistrar.registerReflectionHints(hints, getHttpEntityType(methodParameter));
+      Type httpEntityType = getHttpEntityType(methodParameter);
+      if (httpEntityType != null) {
+        this.bindingRegistrar.registerReflectionHints(hints, httpEntityType);
+      }
     }
   }
 
@@ -99,7 +99,10 @@ class ControllerMappingReflectiveProcessor implements ReflectiveProcessor {
       this.bindingRegistrar.registerReflectionHints(hints, returnTypeParameter.getGenericParameterType());
     }
     else if (HttpEntity.class.isAssignableFrom(returnTypeParameter.getParameterType())) {
-      this.bindingRegistrar.registerReflectionHints(hints, getHttpEntityType(returnTypeParameter));
+      Type httpEntityType = getHttpEntityType(returnTypeParameter);
+      if (httpEntityType != null) {
+        this.bindingRegistrar.registerReflectionHints(hints, httpEntityType);
+      }
     }
   }
 
