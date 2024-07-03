7cdd2bf4cdd8195c9eecdcffc29400ece2dee47c
Harry Yang <i.take.today@gmail.com>
2024-07-03 17:13:44 +0800

    :art: Remove superfluous addToClassHierarchy call for Enum types

--- today-core/src/main/java/cn/taketoday/core/conversion/support/GenericConversionService.java
+++ today-core/src/main/java/cn/taketoday/core/conversion/support/GenericConversionService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.core.conversion.support;
@@ -568,8 +568,8 @@ public class GenericConversionService implements ConfigurableConversionService {
      * @return an ordered list of all classes that the given type extends or implements
      */
     private List<Class<?>> getClassHierarchy(Class<?> type) {
-      HashSet<Class<?>> visited = new HashSet<>(20);
-      ArrayList<Class<?>> hierarchy = new ArrayList<>(20);
+      List<Class<?>> hierarchy = new ArrayList<>(20);
+      Set<Class<?>> visited = new HashSet<>(20);
       addToClassHierarchy(0, ClassUtils.resolvePrimitiveIfNecessary(type), false, hierarchy, visited);
       boolean array = type.isArray();
 
@@ -586,9 +586,8 @@ public class GenericConversionService implements ConfigurableConversionService {
       }
 
       if (Enum.class.isAssignableFrom(type)) {
-        addToClassHierarchy(hierarchy.size(), Enum.class, array, hierarchy, visited);
         addToClassHierarchy(hierarchy.size(), Enum.class, false, hierarchy, visited);
-        addInterfacesToClassHierarchy(Enum.class, array, hierarchy, visited);
+        addInterfacesToClassHierarchy(Enum.class, false, hierarchy, visited);
       }
 
       addToClassHierarchy(hierarchy.size(), Object.class, array, hierarchy, visited);
