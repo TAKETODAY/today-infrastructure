5f969db50efc13c240d12aa99452cc23ceafe8ca
Harry Yang <i.take.today@gmail.com>
2024-07-07 11:25:56 +0800

    :art: Use error handler for reactive cache aspect
    This change ensures that the cache error handler is used in case of
    future-based or publisher-based asynchronous caching completing with an
    exception.

--- today-context/src/main/java/cn/taketoday/cache/interceptor/CacheAspectSupport.java
+++ today-context/src/main/java/cn/taketoday/cache/interceptor/CacheAspectSupport.java
@@ -495,7 +495,10 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
       if (CompletableFuture.class.isAssignableFrom(context.getMethod().getReturnType())) {
         CompletableFuture<?> result = cache.retrieve(key);
         if (result != null) {
-          return result.thenCompose(value -> (CompletableFuture<?>) evaluate(
+          return result.exceptionally(ex -> {
+            getErrorHandler().handleCacheGetError((RuntimeException) ex, cache, key);
+            return null;
+          }).thenCompose(value -> (CompletableFuture<?>) evaluate(
                   (value != null ? CompletableFuture.completedFuture(unwrapCacheValue(value)) : null),
                   invoker, method, contexts));
         }
@@ -1098,12 +1101,30 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
         if (adapter.isMultiValue()) {
           return adapter.fromPublisher(Flux.from(Mono.fromFuture(cachedFuture))
                   .switchIfEmpty(Flux.defer(() -> (Flux) evaluate(null, invoker, method, contexts)))
-                  .flatMap(v -> evaluate(valueToFlux(v, contexts), invoker, method, contexts)));
+                  .flatMap(v -> evaluate(valueToFlux(v, contexts), invoker, method, contexts))
+                  .onErrorResume(RuntimeException.class, ex -> {
+                    try {
+                      getErrorHandler().handleCacheGetError((RuntimeException) ex, cache, key);
+                      return evaluate(null, invoker, method, contexts);
+                    }
+                    catch (RuntimeException exception) {
+                      return Flux.error(exception);
+                    }
+                  }));
         }
         else {
           return adapter.fromPublisher(Mono.fromFuture(cachedFuture)
                   .switchIfEmpty(Mono.defer(() -> (Mono) evaluate(null, invoker, method, contexts)))
-                  .flatMap(v -> evaluate(Mono.justOrEmpty(unwrapCacheValue(v)), invoker, method, contexts)));
+                  .flatMap(v -> evaluate(Mono.justOrEmpty(unwrapCacheValue(v)), invoker, method, contexts))
+                  .onErrorResume(RuntimeException.class, ex -> {
+                    try {
+                      getErrorHandler().handleCacheGetError((RuntimeException) ex, cache, key);
+                      return evaluate(null, invoker, method, contexts);
+                    }
+                    catch (RuntimeException exception) {
+                      return Mono.error(exception);
+                    }
+                  }));
         }
       }
       return NOT_HANDLED;
@@ -1119,7 +1140,8 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
       ReactiveAdapter adapter = (result != null ? registry.getAdapter(result.getClass()) : null);
       if (adapter != null) {
         if (adapter.isMultiValue()) {
-          Flux<?> source = Flux.from(adapter.toPublisher(result));
+          Flux<?> source = Flux.from(adapter.toPublisher(result))
+                  .publish().refCount(2);
           source.subscribe(new CachePutListSubscriber(request));
           return adapter.fromPublisher(source);
         }
