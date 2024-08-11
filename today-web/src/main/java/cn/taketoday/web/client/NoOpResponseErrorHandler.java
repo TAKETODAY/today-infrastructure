a1f96af427715303fc46f355ac1cf482a35cbe4f
海子 Yang <i.take.today@gmail.com>
2024-08-11 21:49:32 +0800

    :memo:

--- today-web/src/main/java/cn/taketoday/web/client/NoOpResponseErrorHandler.java
+++ today-web/src/main/java/cn/taketoday/web/client/NoOpResponseErrorHandler.java
@@ -18,12 +18,20 @@
 package cn.taketoday.web.client;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 import cn.taketoday.http.client.ClientHttpResponse;
+import cn.taketoday.web.client.RestClient.ResponseSpec.ErrorHandler;
 
 /**
  * A basic, no operation {@link ResponseErrorHandler} implementation suitable
- * for ignoring any error.
+ * for ignoring any error using the {@link RestTemplate}.
+ * <p>This implementation is not suitable with the {@link RestClient} as it uses
+ * a list of candidates where the first matching is invoked. If you want to
+ * disable default status handlers with the {@code RestClient}, consider
+ * registering a noop {@link ErrorHandler ErrorHandler} with a
+ * predicate that matches all status code, see
+ * {@link RestClient.Builder#defaultStatusHandler(Predicate, ErrorHandler)}.
  *
  * @author Stephane Nicoll
  * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
