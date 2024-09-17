34c7668711a01bb5bf9b3c34af7bc9f39f6f3a16
海子 Yang <i.take.today@gmail.com>
2024-09-17 14:16:02 +0800

    :art: Remove commented entries from mime.types

--- today-web/src/test/java/cn/taketoday/http/MediaTypeFactoryTests.java
+++ today-web/src/test/java/cn/taketoday/http/MediaTypeFactoryTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.http;
@@ -37,6 +34,7 @@ class MediaTypeFactoryTests {
     assertThat(MediaTypeFactory.getMediaType("file.xml").get()).isEqualTo(MediaType.APPLICATION_XML);
     assertThat(MediaTypeFactory.getMediaType("file.js").get()).isEqualTo(MediaType.parseMediaType("application/javascript"));
     assertThat(MediaTypeFactory.getMediaType("file.css").get()).isEqualTo(MediaType.parseMediaType("text/css"));
+    assertThat(MediaTypeFactory.getMediaType("file.wasm")).contains(MediaType.parseMediaType("application/wasm"));
     assertThat(MediaTypeFactory.getMediaType("file.foobar").isPresent()).isFalse();
   }
 
