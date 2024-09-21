069b623f8e69da8b9134840f2c5760d22ab6e76f
海子 Yang <i.take.today@gmail.com>
2024-09-21 21:15:25 +0800

    :art: Explain that enabling virtual threads disables traditional thread pools

--- today-framework/src/main/java/cn/taketoday/annotation/config/task/TaskSchedulingProperties.java
+++ today-framework/src/main/java/cn/taketoday/annotation/config/task/TaskSchedulingProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.annotation.config.task;
@@ -65,7 +65,8 @@ public class TaskSchedulingProperties {
   public static class Pool {
 
     /**
-     * Maximum allowed number of threads.
+     * Maximum allowed number of threads. Doesn't have an effect if virtual threads
+     * are enabled.
      */
     private int size = 1;
 
