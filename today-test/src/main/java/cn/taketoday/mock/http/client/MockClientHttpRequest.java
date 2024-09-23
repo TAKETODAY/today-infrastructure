283c5ed1d47126fc1b2b49690f1b42e74d16c7c9
海子 Yang <i.take.today@gmail.com>
2024-09-23 09:53:17 +0800

    :sparkles: ClientHttpRequest 异步 API

--- today-test/src/main/java/cn/taketoday/mock/http/client/MockClientHttpRequest.java
+++ today-test/src/main/java/cn/taketoday/mock/http/client/MockClientHttpRequest.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.mock.http.client;
@@ -30,6 +27,7 @@ import cn.taketoday.http.client.ClientHttpResponse;
 import cn.taketoday.lang.Assert;
 import cn.taketoday.lang.Nullable;
 import cn.taketoday.mock.http.MockHttpOutputMessage;
+import cn.taketoday.util.concurrent.Future;
 
 /**
  * Mock implementation of {@link ClientHttpRequest}.
@@ -112,6 +110,11 @@ public class MockClientHttpRequest extends MockHttpOutputMessage implements Clie
   public final ClientHttpResponse execute() throws IOException {
     this.executed = true;
     return executeInternal();
+  }
+
+  @Override
+  public Future<ClientHttpResponse> async() {
+    return Future.run(this::execute);
   }
 
   /**
