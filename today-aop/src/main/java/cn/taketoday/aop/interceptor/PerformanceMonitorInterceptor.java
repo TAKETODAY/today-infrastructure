e1f22a96251c14d4a3499f9c538e95b40726c2ae
海子 Yang <i.take.today@gmail.com>
2024-09-27 15:09:56 +0800

    :fire:

--- today-aop/src/main/java/cn/taketoday/aop/interceptor/PerformanceMonitorInterceptor.java
+++ today-aop/src/main/java/cn/taketoday/aop/interceptor/PerformanceMonitorInterceptor.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,8 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
+
 package cn.taketoday.aop.interceptor;
 
 import org.aopalliance.intercept.MethodInvocation;
@@ -33,9 +31,8 @@ import cn.taketoday.util.StopWatch;
  * @author Rod Johnson
  * @author Dmitriy Kopylenko
  * @author Rob Harrop
- * @author TODAY
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
  * @see StopWatch
- * @see JamonPerformanceMonitorInterceptor
  * @since 3.0
  */
 @SuppressWarnings("serial")
