0527b289c2cb37ca30ce9cd1dfbee49f8228549c
海子 Yang <i.take.today@gmail.com>
2024-10-25 14:31:04 +0800

    :art:

--- infra-build/infra-app-loader-tools/src/main/java/cn/taketoday/app/loader/tools/Layer.java
+++ infra-build/infra-app-loader-tools/src/main/java/cn/taketoday/app/loader/tools/Layer.java
@@ -44,7 +44,8 @@ public class Layer {
   public Layer(String name) {
     Assert.hasText(name, "Name must not be empty");
     Assert.isTrue(PATTERN.matcher(name).matches(), () -> "Malformed layer name '" + name + "'");
-    Assert.isTrue(!name.equalsIgnoreCase("ext"), () -> "Layer name '" + name + "' is reserved");
+    Assert.isTrue(!name.equalsIgnoreCase("ext")
+            && !name.toLowerCase().startsWith("infra-app"), () -> "Layer name '" + name + "' is reserved");
     this.name = name;
   }
 
--- infra-build/infra-app-loader-tools/src/test/java/cn/taketoday/app/loader/tools/LayerTests.java
+++ infra-build/infra-app-loader-tools/src/test/java/cn/taketoday/app/loader/tools/LayerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2023 the original author or authors.
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.app.loader.tools;
@@ -66,8 +66,8 @@ class LayerTests {
 			.withMessage("Layer name 'ext' is reserved");
 		assertThatIllegalArgumentException().isThrownBy(() -> new Layer("ExT"))
 			.withMessage("Layer name 'ExT' is reserved");
-		assertThatIllegalArgumentException().isThrownBy(() -> new Layer("springbootloader"))
-			.withMessage("Layer name 'springbootloader' is reserved");
+		assertThatIllegalArgumentException().isThrownBy(() -> new Layer("infra-app-loader"))
+			.withMessage("Layer name 'infra-app-loader' is reserved");
 	}
 
 }
