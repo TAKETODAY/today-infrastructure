3890074b82499bcaf1663aa7adedb7a8f43d3ae6
海子 Yang <i.take.today@gmail.com>
2024-11-23 11:43:36 +0800

    :art: Exclusively mention CompletableFuture instead of ListenableFuture

--- today-context/src/main/java/cn/taketoday/scheduling/annotation/Async.java
+++ today-context/src/main/java/cn/taketoday/scheduling/annotation/Async.java
@@ -25,7 +25,6 @@ import java.lang.annotation.Target;
 
 import cn.taketoday.aot.hint.annotation.Reflective;
 import cn.taketoday.context.annotation.Configuration;
-import cn.taketoday.util.concurrent.Future;
 
 /**
  * Annotation that marks a method as a candidate for <i>asynchronous</i> execution.
@@ -37,17 +36,18 @@ import cn.taketoday.util.concurrent.Future;
  * <p>In terms of target method signatures, any parameter types are supported.
  * However, the return type is constrained to either {@code void} or
  * {@link java.util.concurrent.Future}. In the latter case, you may declare the
- * more specific {@link Future} or
- * {@link java.util.concurrent.CompletableFuture} types which allow for richer
- * interaction with the asynchronous task and for immediate composition with
- * further processing steps.
+ * more specific {@link java.util.concurrent.CompletableFuture} type which allows
+ * for richer interaction with the asynchronous task and for immediate composition
+ * with further processing steps.
  *
  * <p>A {@code Future} handle returned from the proxy will be an actual asynchronous
- * {@code Future} that can be used to track the result of the asynchronous method
- * execution. However, since the target method needs to implement the same signature,
- * it will have to return a temporary {@code Future} handle that just passes a value
- * through: for example {@link Future#ok(Object)}, EJB 3.1's {@link jakarta.ejb.AsyncResult},
- * or {@link java.util.concurrent.CompletableFuture#completedFuture(Object)}.
+ * {@code (Completable)Future} that can be used to track the result of the
+ * asynchronous method execution. However, since the target method needs to implement
+ * the same signature, it will have to return a temporary {@code Future} handle that
+ * just passes a value after computation in the execution thread: typically through
+ * {@link java.util.concurrent.CompletableFuture#completedFuture(Object)}. The
+ * provided value will be exposed to the caller through the actual asynchronous
+ * {@code Future} handle at runtime.
  *
  * @author Juergen Hoeller
  * @author Chris Beams
