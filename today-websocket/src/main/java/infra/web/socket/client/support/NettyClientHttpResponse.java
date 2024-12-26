431565aaab712e15b1fa8dfc69ef43a71e1751a3
海子 Yang <i.take.today@gmail.com>
2024-12-26 22:27:29 +0800

    :sparkles: websocket 客户端 SSL 支持

---
+++ today-websocket/src/main/java/infra/web/socket/client/support/NettyClientHttpResponse.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 - 2024 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+package infra.web.socket.client.support;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import infra.http.HttpHeaders;
+import infra.http.HttpStatusCode;
+import infra.http.MediaType;
+import infra.http.client.ClientHttpResponse;
+import infra.http.support.Netty4HttpHeaders;
+import infra.lang.Nullable;
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0 2024/12/26 20:14
+ */
+final class NettyClientHttpResponse implements ClientHttpResponse {
+
+  private final byte[] byteArray;
+
+  private final HttpHeaders httpHeaders;
+
+  private final FullHttpResponse response;
+
+  NettyClientHttpResponse(FullHttpResponse response) {
+    this.response = response;
+    ByteBuf content = response.content();
+    this.byteArray = new byte[content.readableBytes()];
+    content.readBytes(byteArray);
+    this.httpHeaders = new Netty4HttpHeaders(response.headers()).asReadOnly();
+  }
+
+  @Override
+  public HttpStatusCode getStatusCode() {
+    return HttpStatusCode.valueOf(response.status().code());
+  }
+
+  @Override
+  public String getStatusText() {
+    return response.status().reasonPhrase();
+  }
+
+  @Override
+  public void close() {
+    ReferenceCountUtil.safeRelease(response);
+  }
+
+  @Override
+  public InputStream getBody() {
+    return new ByteArrayInputStream(byteArray);
+  }
+
+  public byte[] bodyByteArray() {
+    return byteArray;
+  }
+
+  @Nullable
+  public Charset getCharset() {
+    MediaType contentType = httpHeaders.getContentType();
+    return contentType != null ? contentType.getCharset() : null;
+  }
+
+  @Override
+  public HttpHeaders getHeaders() {
+    return httpHeaders;
+  }
+
+}
--- today-websocket/src/main/java/infra/web/socket/client/support/NettyWebSocketClient.java
+++ today-websocket/src/main/java/infra/web/socket/client/support/NettyWebSocketClient.java
@@ -22,7 +22,12 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLException;
+
 import infra.core.io.buffer.NettyDataBufferFactory;
+import infra.core.ssl.SslBundle;
+import infra.core.ssl.SslManagerBundle;
+import infra.core.ssl.SslOptions;
 import infra.http.HttpHeaders;
 import infra.lang.Assert;
 import infra.lang.Nullable;
@@ -31,6 +36,10 @@ import infra.util.ExceptionUtils;
 import infra.util.StringUtils;
 import infra.util.concurrent.Future;
 import infra.util.concurrent.Promise;
+import infra.web.client.DefaultResponseErrorHandler;
+import infra.web.client.HttpClientErrorException;
+import infra.web.client.HttpServerErrorException;
+import infra.web.client.ResponseErrorHandler;
 import infra.web.socket.CloseStatus;
 import infra.web.socket.WebSocketExtension;
 import infra.web.socket.WebSocketHandler;
@@ -58,11 +67,14 @@ import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpDecoderConfig;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpObjectDecoder;
+import io.netty.handler.codec.http.HttpStatusClass;
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.util.concurrent.DefaultThreadFactory;
 
 import static infra.util.concurrent.Future.ok;
@@ -128,7 +140,7 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
    * @see io.netty.util.concurrent.MultithreadEventExecutorGroup
    * @since 5.0
    */
-  private int ioThreadCount = 4;
+  private int ioThreadCount = 1;
 
   /**
    * @since 5.0
@@ -140,6 +152,23 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
    * @since 5.0
    */
   private Duration connectTimeout = Duration.ofSeconds(10);
+
+  /**
+   * @since 5.0
+   */
+  private ResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
+
+  /**
+   * @since 5.0
+   */
+  @Nullable
+  private SslBundle sslBundle;
+
+  /**
+   * @since 5.0
+   */
+  @Nullable
+  private SslContext sslContext;
 
   public void setFailOnMissingResponse(boolean failOnMissingResponse) {
     this.failOnMissingResponse = failOnMissingResponse;
@@ -225,21 +254,50 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
     this.ioThreadPoolName = workerPoolName;
   }
 
+  /**
+   * Set the error handler.
+   * <p>By default, NettyWebSocketClient uses a {@link DefaultResponseErrorHandler}.
+   *
+   * @since 5.0
+   */
+  public void setErrorHandler(ResponseErrorHandler errorHandler) {
+    Assert.notNull(errorHandler, "ResponseErrorHandler is required");
+    this.errorHandler = errorHandler;
+  }
+
+  /**
+   * Set the SslBundle for WSS
+   *
+   * @since 5.0
+   */
+  public void setSslBundle(@Nullable SslBundle sslBundle) {
+    this.sslBundle = sslBundle;
+  }
+
+  /**
+   * Set the SslContext for WSS
+   *
+   * @since 5.0
+   */
+  public void setSslContext(@Nullable SslContext sslContext) {
+    this.sslContext = sslContext;
+  }
+
   @Override
   protected Future<WebSocketSession> doHandshakeInternal(WebSocketHandler webSocketHandler,
           HttpHeaders headers, URI uri, List<String> subProtocols, List<WebSocketExtension> extensions) {
 
+    boolean secure = "wss".equals(uri.getScheme());
     // Connect with V13 (RFC 6455 aka HyBi-17). You can change it to V08 or V00.
     // If you change it to V00, ping is not supported and remember to change
     // HttpResponseDecoder to WebSocketHttpResponseDecoder in the pipeline.
     WebSocketClientHandshaker handshaker = createHandshaker(uri, subProtocols, extensions, createHeaders(headers));
-    MessageHandler handler = new MessageHandler(uri, webSocketHandler, handshaker);
+    MessageHandler handler = new MessageHandler(secure, webSocketHandler, handshaker, uri, headers);
 
-    Bootstrap bootstrap = getBootstrap(handler);
+    Bootstrap bootstrap = createBootstrap(handler, secure);
     ChannelFuture connect = bootstrap.connect(uri.getHost(), uri.getPort())
             .addListener(handler);
-    return handler.promise
-            .onCancelled(() -> connect.cancel(true));
+    return handler.promise.onCancelled(() -> connect.cancel(true));
   }
 
   /**
@@ -256,19 +314,23 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
   /**
    * @since 5.0
    */
-  protected Bootstrap getBootstrap(ChannelHandler handler) {
+  protected Bootstrap createBootstrap(ChannelHandler handler, boolean secure) {
     NioEventLoopGroup eventLoopGroup = eventLoopGroup();
     ChannelFactory<?> channelFactory = channelFactory();
     Bootstrap bootstrap = new Bootstrap();
     processBootstrap(bootstrap);
-    return bootstrap
-            .group(eventLoopGroup)
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, Math.toIntExact(connectTimeout.toMillis()))
+    return bootstrap.group(eventLoopGroup)
             .channelFactory(channelFactory)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, Math.toIntExact(connectTimeout.toMillis()))
             .handler(new ChannelInitializer<SocketChannel>() {
 
               @Override
               protected void initChannel(SocketChannel ch) {
+                if (secure) {
+                  SslContext sslContext = getSslContext();
+                  ch.pipeline().addLast(sslContext.newHandler(ch.alloc()));
+                }
+
                 ch.pipeline()
                         .addLast("httpClientCodec", new HttpClientCodec(httpDecoderConfig, parseHttpAfterConnectRequest, failOnMissingResponse))
                         .addLast("httpObjectAggregator", new HttpObjectAggregator(maxContentLength, closeOnExpectationFailed))
@@ -287,6 +349,30 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
   protected NettyWebSocketSession createSession(Channel channel, boolean secure,
           WebSocketClientHandshaker handshaker, NettyDataBufferFactory allocator) {
     return new NettyClientWebSocketSession(secure, channel, handshaker, allocator);
+  }
+
+  private SslContext getSslContext() {
+    if (sslContext == null) {
+      try {
+        if (sslBundle != null) {
+          SslOptions options = sslBundle.getOptions();
+          SslManagerBundle managers = sslBundle.getManagers();
+          sslContext = SslContextBuilder.forClient()
+                  .keyManager(managers.getKeyManagerFactory())
+                  .trustManager(managers.getTrustManagerFactory())
+                  .ciphers(SslOptions.asSet(options.getCiphers()))
+                  .protocols(options.getEnabledProtocols())
+                  .build();
+        }
+        else {
+          sslContext = SslContextBuilder.forClient().build();
+        }
+      }
+      catch (SSLException e) {
+        throw ExceptionUtils.sneakyThrow(e);
+      }
+    }
+    return sslContext;
   }
 
   /**
@@ -342,33 +428,42 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
 
   final class MessageHandler extends ChannelInboundHandlerAdapter implements ChannelFutureListener {
 
-    private final URI uri;
+    private final boolean secure;
 
     private final WebSocketHandler handler;
-
-    private final WebSocketClientHandshaker handshaker;
 
     public final Promise<WebSocketSession> promise = Future.forPromise();
 
     @Nullable
     private NettyWebSocketSession session;
 
-    MessageHandler(URI uri, WebSocketHandler handler, WebSocketClientHandshaker handshaker) {
-      this.uri = uri;
+    private URI uri;
+
+    private HttpHeaders headers;
+
+    private WebSocketClientHandshaker handshaker;
+
+    MessageHandler(boolean secure, WebSocketHandler handler, WebSocketClientHandshaker handshaker, URI uri, HttpHeaders headers) {
+      this.secure = secure;
       this.handler = handler;
       this.handshaker = handshaker;
+      this.uri = uri;
+      this.headers = headers;
     }
 
     @Override
     public void operationComplete(ChannelFuture future) {
-      if (!future.isSuccess()) {
+      if (future.isCancelled()) {
+        promise.cancel(true);
+      }
+      else if (!future.isSuccess()) {
         promise.tryFailure(future.cause());
       }
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-      handshaker.handshake(ctx.channel());
+      handshaker.handshake(ctx.channel()).addListener(this);
     }
 
     @Override
@@ -406,14 +501,41 @@ public class NettyWebSocketClient extends AbstractWebSocketClient {
           }
         }
       }
-      else if (msg instanceof FullHttpResponse response) {
-        if (!handshaker.isHandshakeComplete()) {
+      else if (msg instanceof FullHttpResponse res) {
+        HttpStatusClass codeClass = res.status().codeClass();
+        if (codeClass == HttpStatusClass.CLIENT_ERROR
+                || codeClass == HttpStatusClass.SERVER_ERROR) {
+          try {
+            var response = new NettyClientHttpResponse(res);
+            if (errorHandler.hasError(response)) {
+              errorHandler.handleError(new NettyHttpRequest(uri, headers), response);
+            }
+
+            // default
+            if (codeClass == HttpStatusClass.CLIENT_ERROR) {
+              promise.tryFailure(HttpClientErrorException.create("Websocket handshake failed", response.getStatusCode(),
+                      response.getStatusText(), response.getHeaders(), response.bodyByteArray(), response.getCharset()));
+            }
+            else {
+              promise.tryFailure(HttpServerErrorException.create("Websocket handshake failed", response.getStatusCode(),
+                      response.getStatusText(), response.getHeaders(), response.bodyByteArray(), response.getCharset()));
+            }
+          }
+          catch (Throwable e) {
+            promise.tryFailure(e);
+          }
+        }
+        else if (!handshaker.isHandshakeComplete()) {
           Channel channel = ctx.channel();
           try {
-            handshaker.finishHandshake(channel, response);
-            session = createSession(channel, "wss".equals(uri.getScheme()), handshaker, new NettyDataBufferFactory(channel.alloc()));
+            handshaker.finishHandshake(channel, res);
+            session = createSession(channel, secure, handshaker, new NettyDataBufferFactory(channel.alloc()));
             handler.onOpen(session);
             promise.setSuccess(session);
+            headers.clear();
+            uri = null;
+            headers = null;
+            handshaker = null;
           }
           catch (Throwable e) {
             promise.setFailure(e);
