bb2fd11e8a7f80fbc0e91ab54988179d45f95495
海子 Yang <i.take.today@gmail.com>
2025-01-04 19:49:27 +0800

    :art:

--- today-web/src/main/java/infra/web/resource/ResourceHttpRequestHandler.java
+++ today-web/src/main/java/infra/web/resource/ResourceHttpRequestHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -719,8 +719,7 @@ public class ResourceHttpRequestHandler extends WebContentGenerator
 
   private String locationToString(List<Resource> locations) {
     return locations.toString()
-            .replaceAll("class path resource", "classpath")
-            .replaceAll("MockContext resource", "MockContext");
+            .replaceAll("class path resource", "classpath");
   }
 
 }
