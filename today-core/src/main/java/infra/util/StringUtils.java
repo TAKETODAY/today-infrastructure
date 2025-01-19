c5c56255777ab64a09fd84ad7af78e5d70c6e109
海子 Yang <i.take.today@gmail.com>
2025-01-19 22:27:12 +0800

    :art: StringUtils#commaDelimitedListToSet

--- today-core/src/main/java/infra/util/StringUtils.java
+++ today-core/src/main/java/infra/util/StringUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -579,7 +579,7 @@ public abstract class StringUtils {
    */
   public static Set<String> commaDelimitedListToSet(@Nullable String str) {
     String[] tokens = commaDelimitedListToStringArray(str);
-    return new LinkedHashSet<>(Arrays.asList(tokens));
+    return CollectionUtils.newLinkedHashSet(tokens);
   }
 
   /**
