00c2b56bfc0ce57126edba808b225e40fa7beaad
海子 Yang <i.take.today@gmail.com>
2025-01-19 20:28:38 +0800

    :sparkles: MergedAnnotation API
    - 减少对象创建
    - 增加 API 的易用性

--- today-context/src/main/java/infra/scheduling/annotation/ProxyAsyncConfiguration.java
+++ today-context/src/main/java/infra/scheduling/annotation/ProxyAsyncConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -51,7 +51,7 @@ public class ProxyAsyncConfiguration extends AbstractAsyncConfiguration {
     AsyncAnnotationBeanPostProcessor bpp = new AsyncAnnotationBeanPostProcessor();
     bpp.configure(this.executor, this.exceptionHandler);
     Class<? extends Annotation> customAsyncAnnotation = this.enableAsync.getClass("annotation");
-    if (customAsyncAnnotation != enableAsync.getDefaultValue("annotation").orElse(null)) {
+    if (customAsyncAnnotation != enableAsync.getDefaultValue("annotation")) {
       bpp.setAsyncAnnotationType(customAsyncAnnotation);
     }
     bpp.setProxyTargetClass(this.enableAsync.getBoolean("proxyTargetClass"));
