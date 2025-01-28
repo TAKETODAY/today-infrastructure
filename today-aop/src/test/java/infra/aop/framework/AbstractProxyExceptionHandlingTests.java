c851c7266c758052ecf28046ca0908c3960d1084
海子 Yang <i.take.today@gmail.com>
2025-01-28 13:40:57 +0800

    :art:

--- today-aop/src/test/java/infra/aop/framework/AbstractProxyExceptionHandlingTests.java
+++ today-aop/src/test/java/infra/aop/framework/AbstractProxyExceptionHandlingTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -95,7 +95,7 @@ abstract class AbstractProxyExceptionHandlingTests {
       assertThat(throwableSeenByInterceptor).isSameAs(undeclaredCheckedException);
       assertThat(throwableSeenByCaller)
               .isInstanceOf(UndeclaredThrowableException.class)
-              .hasCauseReference(undeclaredCheckedException);
+              .cause().isSameAs(undeclaredCheckedException);
     }
 
     @Test
@@ -142,7 +142,7 @@ abstract class AbstractProxyExceptionHandlingTests {
       invokeProxy();
       assertThat(throwableSeenByCaller)
               .isInstanceOf(UndeclaredThrowableException.class)
-              .hasCauseReference(undeclaredCheckedException);
+              .cause().isSameAs(undeclaredCheckedException);
     }
 
     @Test
