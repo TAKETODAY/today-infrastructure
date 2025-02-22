dc540eb7224d9afb215f1d3bded1c2a6619b310d
海子 Yang <i.take.today@gmail.com>
2025-02-22 23:41:23 +0800

    :art: Fix `SslOptions.isSpecified()` logic
    Prior to this commit `SslOptions.isSpecified()` only returned `true` if
    both ciphers and enabled protocols were set. If should have returned
    `true` if either were set.

--- today-core/src/test/java/infra/core/ssl/SslOptionsTests.java
+++ today-core/src/test/java/infra/core/ssl/SslOptionsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -71,6 +71,24 @@ class SslOptionsTests {
     SslOptions options = SslOptions.of(ciphers, enabledProtocols);
     assertThat(options.getCiphers()).isNull();
     assertThat(options.getEnabledProtocols()).isNull();
+  }
+
+  @Test
+  void isSpecifiedWhenHasCiphers() {
+    SslOptions options = SslOptions.of(Set.of("a", "b", "c"), null);
+    assertThat(options.isSpecified()).isTrue();
+  }
+
+  @Test
+  void isSpecifiedWhenHasEnabledProtocols() {
+    SslOptions options = SslOptions.of(null, Set.of("d", "e", "f"));
+    assertThat(options.isSpecified()).isTrue();
+  }
+
+  @Test
+  void isSpecifiedWhenHasNoCiphersOrEnabledProtocols() {
+    SslOptions options = SslOptions.NONE;
+    assertThat(options.isSpecified()).isFalse();
   }
 
 }
