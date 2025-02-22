2d11dfe890ae6d758559dd41f63cf9ed20a4946e
海子 Yang <i.take.today@gmail.com>
2025-02-22 16:15:13 +0800

    :art: Mark XML-configured executor/scheduler as infrastructure bean

--- today-context/src/main/java/infra/scheduling/config/ExecutorBeanDefinitionParser.java
+++ today-context/src/main/java/infra/scheduling/config/ExecutorBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ package infra.scheduling.config;
 
 import org.w3c.dom.Element;
 
+import infra.beans.factory.config.BeanDefinition;
 import infra.beans.factory.support.BeanDefinitionBuilder;
 import infra.beans.factory.support.RootBeanDefinition;
 import infra.beans.factory.xml.AbstractSingleBeanDefinitionParser;
@@ -55,6 +56,8 @@ public class ExecutorBeanDefinitionParser extends AbstractSingleBeanDefinitionPa
     if (StringUtils.hasText(poolSize)) {
       builder.addPropertyValue("poolSize", poolSize);
     }
+
+    builder.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
   }
 
   private void configureRejectionPolicy(Element element, BeanDefinitionBuilder builder) {
--- today-context/src/main/java/infra/scheduling/config/SchedulerBeanDefinitionParser.java
+++ today-context/src/main/java/infra/scheduling/config/SchedulerBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ package infra.scheduling.config;
 
 import org.w3c.dom.Element;
 
+import infra.beans.factory.config.BeanDefinition;
 import infra.beans.factory.support.BeanDefinitionBuilder;
 import infra.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import infra.util.StringUtils;
@@ -45,6 +46,7 @@ public class SchedulerBeanDefinitionParser extends AbstractSingleBeanDefinitionP
     }
 
     builder.setEnableDependencyInjection(false);
+    builder.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
   }
 
 }
