5ae88fa8f17e3a41d677c740b79f0f5f0741921f
海子 Yang <i.take.today@gmail.com>
2025-03-01 14:15:29 +0800

    :art: Defensively call isShutdown method for executor description

--- today-core/src/main/java/infra/core/task/TaskRejectedException.java
+++ today-core/src/main/java/infra/core/task/TaskRejectedException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,7 +73,13 @@ public class TaskRejectedException extends RejectedExecutionException {
 
   private static String executorDescription(Executor executor) {
     if (executor instanceof ExecutorService executorService) {
-      return "ExecutorService in " + (executorService.isShutdown() ? "shutdown" : "active") + " state";
+      try {
+        return "ExecutorService in " + (executorService.isShutdown() ? "shutdown" : "active") + " state";
+      }
+      catch (Exception ex) {
+        // UnsupportedOperationException/IllegalStateException from ManagedExecutorService.isShutdown()
+        // Falling back to toString() below.
+      }
     }
     return executor.toString();
   }
