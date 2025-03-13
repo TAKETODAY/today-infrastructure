704a40ac0995558bf7067a5c992a029c9ef99064
海子 Yang <i.take.today@gmail.com>
2025-03-13 20:00:38 +0800

    :white_check_mark:

--- today-web/src/test/java/infra/web/accept/SemanticApiVersionParserTests.java
+++ today-web/src/test/java/infra/web/accept/SemanticApiVersionParserTests.java
@@ -21,6 +21,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import infra.web.accept.SemanticApiVersionParser.Version;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -33,6 +36,59 @@ class SemanticApiVersionParserTests {
   private final SemanticApiVersionParser parser = new SemanticApiVersionParser();
 
   @Test
+  void compareVersionsWithSameMajor() {
+    Version v1 = parser.parseVersion("1.2.3");
+    Version v2 = parser.parseVersion("1.3.0");
+    assertThat(v1.compareTo(v2)).isLessThan(0);
+    assertThat(v1.equals(v2)).isFalse();
+  }
+
+  @Test
+  void compareVersionsWithDifferentMajor() {
+    Version v1 = parser.parseVersion("2.0.0");
+    Version v2 = parser.parseVersion("1.9.9");
+    assertThat(v1.compareTo(v2)).isGreaterThan(0);
+    assertThat(v1.equals(v2)).isFalse();
+  }
+
+  @Test
+  void compareVersionsWithSamePatchDifferentMinor() {
+    Version v1 = parser.parseVersion("1.2.3");
+    Version v2 = parser.parseVersion("1.1.3");
+    assertThat(v1.compareTo(v2)).isGreaterThan(0);
+    assertThat(v1.equals(v2)).isFalse();
+  }
+
+  @Test
+  void equalVersionsHaveSameHashCode() {
+    Version v1 = parser.parseVersion("1.2.3");
+    Version v2 = parser.parseVersion("1.2.3");
+    assertThat(v1.hashCode()).isEqualTo(v2.hashCode());
+    assertThat(v1.equals(v2)).isTrue();
+  }
+
+  @Test
+  void versionToString() {
+    Version version = parser.parseVersion("1.2.3");
+    assertThat(version.toString()).isEqualTo("1.2.3");
+  }
+
+  @Test
+  void parseNullVersion() {
+    assertThatIllegalArgumentException()
+            .isThrownBy(() -> parser.parseVersion(null))
+            .withMessage("'version' is required");
+  }
+
+  @Test
+  void skipMultipleNonDigitCharacters() {
+    Version version = parser.parseVersion("version-v1.2.3");
+    assertThat(version.getMajor()).isEqualTo(1);
+    assertThat(version.getMinor()).isEqualTo(2);
+    assertThat(version.getPatch()).isEqualTo(3);
+  }
+
+  @Test
   void parse() {
     testParse("0", 0, 0, 0);
     testParse("0.3", 0, 3, 0);
@@ -43,7 +99,7 @@ class SemanticApiVersionParserTests {
   }
 
   private void testParse(String input, int major, int minor, int patch) {
-    SemanticApiVersionParser.Version actual = this.parser.parseVersion(input);
+    Version actual = this.parser.parseVersion(input);
     assertThat(actual.getMajor()).isEqualTo(major);
     assertThat(actual.getMinor()).isEqualTo(minor);
     assertThat(actual.getPatch()).isEqualTo(patch);
