6bd9996b1f7dde18c077b7a78db124975ca4a8c9
海子 Yang <i.take.today@gmail.com>
2025-03-20 10:53:43 +0800

    :art: Defer triggerAfterCompletion invocation in doRollbackOnCommitException

--- today-tx/src/main/java/infra/transaction/reactive/AbstractReactiveTransactionManager.java
+++ today-tx/src/main/java/infra/transaction/reactive/AbstractReactiveTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -610,7 +610,7 @@ public abstract class AbstractReactiveTransactionManager extends AbstractTransac
                       }))
                       .then(Mono.error(rbex));
             })
-            .then(triggerAfterCompletion(synchronizationManager, status, TransactionSynchronization.STATUS_ROLLED_BACK))
+            .then(Mono.defer(() -> triggerAfterCompletion(synchronizationManager, status, TransactionSynchronization.STATUS_ROLLED_BACK)))
             .then(Mono.defer(() -> {
               executionListeners.afterRollback(status, null);
               return Mono.empty();
