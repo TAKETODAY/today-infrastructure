510a9d561e0fff6cd4f30ea4b1a4ec676ce81028
海子 Yang <i.take.today@gmail.com>
2025-03-28 20:33:08 +0800

    :white_check_mark:

--- today-context/src/test/java/infra/scheduling/concurrent/ThreadPoolExecutorFactoryBeanTests.java
+++ today-context/src/test/java/infra/scheduling/concurrent/ThreadPoolExecutorFactoryBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,9 +22,13 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import infra.context.ConfigurableApplicationContext;
 import infra.context.annotation.AnnotationConfigApplicationContext;
@@ -33,6 +37,7 @@ import infra.context.annotation.Configuration;
 import infra.context.support.GenericApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -75,6 +80,135 @@ class ThreadPoolExecutorFactoryBeanTests {
     context.refresh();
     ThreadPoolExecutor threadPoolExecutor = context.getBean(ThreadPoolExecutor.class);
     verify(threadPoolExecutor).prestartAllCoreThreads();
+  }
+
+  @Test
+  void customKeepAliveTimeIsApplied() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setKeepAliveSeconds(10);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.getKeepAliveTime(TimeUnit.SECONDS)).isEqualTo(10);
+  }
+
+  @Test
+  void zeroQueueCapacityCreatesSynchronousQueue() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setQueueCapacity(0);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.getQueue()).isInstanceOf(SynchronousQueue.class);
+  }
+
+  @Test
+  void allowCoreThreadTimeoutIsRespected() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setAllowCoreThreadTimeOut(true);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.allowsCoreThreadTimeOut()).isTrue();
+  }
+
+  @Test
+  void strictEarlyShutdownRejectsNewTasks() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setStrictEarlyShutdown(true);
+    factory.afterPropertiesSet();
+
+    ExecutorService executor = factory.getObject();
+    factory.destroy();
+
+    assertThatExceptionOfType(RejectedExecutionException.class)
+            .isThrownBy(() -> executor.submit(() -> { }));
+  }
+
+  @Test
+  void multipleCoresAreStartedWhenPrestarted() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setCorePoolSize(3);
+    factory.setPrestartAllCoreThreads(true);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.getPoolSize()).isEqualTo(3);
+  }
+
+  @Test
+  void negativeQueueCapacityCreatesSynchronousQueue() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setQueueCapacity(-1);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.getQueue()).isInstanceOf(SynchronousQueue.class);
+  }
+
+  @Test
+  void minMaxPoolSizesAreRespected() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setCorePoolSize(2);
+    factory.setMaxPoolSize(4);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    assertThat(executor.getCorePoolSize()).isEqualTo(2);
+    assertThat(executor.getMaximumPoolSize()).isEqualTo(4);
+  }
+
+  @Test
+  void customRejectedExecutionHandlerIsUsed() throws Exception {
+    RejectedExecutionHandler handler = mock(RejectedExecutionHandler.class);
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.setRejectedExecutionHandler(handler);
+    factory.afterPropertiesSet();
+
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    Runnable task = () -> { };
+    executor.getRejectedExecutionHandler().rejectedExecution(task, executor);
+
+    verify(handler).rejectedExecution(task, executor);
+  }
+
+  @Test
+  void afterExecuteHookIsInvoked() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    AtomicBoolean afterExecuteCalled = new AtomicBoolean();
+
+    factory = new ThreadPoolExecutorFactoryBean() {
+      @Override
+      protected void afterExecute(Runnable r, Throwable t) {
+        afterExecuteCalled.set(true);
+      }
+    };
+
+    factory.afterPropertiesSet();
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) factory.getObject();
+    executor.execute(() -> { });
+
+    Thread.sleep(100); // Wait for task completion
+    assertThat(afterExecuteCalled.get()).isTrue();
+  }
+
+  @Test
+  void isSingletonReturnsTrue() {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    assertThat(factory.isSingleton()).isTrue();
+  }
+
+  @Test
+  void objectTypeReturnsExecutorServiceWhenNotInitialized() {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    assertThat(factory.getObjectType()).isEqualTo(ExecutorService.class);
+  }
+
+  @Test
+  void objectTypeReturnsActualTypeWhenInitialized() throws Exception {
+    ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean();
+    factory.afterPropertiesSet();
+    assertThat(factory.getObjectType()).isEqualTo(factory.getObject().getClass());
   }
 
   @Configuration
