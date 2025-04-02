f4f3d1f6e4f02289e25f47d77c04a72dbc27293f
海子 Yang <i.take.today@gmail.com>
2025-04-02 21:07:22 +0800

    :white_check_mark:

--- today-context/src/test/java/infra/scripting/support/StandardScriptFactoryTests.java
+++ today-context/src/test/java/infra/scripting/support/StandardScriptFactoryTests.java
@@ -20,13 +20,15 @@ package infra.scripting.support;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import infra.scripting.ScriptEvaluator;
+import infra.scripting.ScriptCompilationException;
 import infra.scripting.ScriptSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
@@ -34,8 +36,117 @@ import static org.mockito.Mockito.mock;
  */
 class StandardScriptFactoryTests {
 
+  @Test
+  void scriptFactoryWithoutEngineNameCreatesInstance() {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js");
+    assertThat(factory.getScriptSourceLocator()).isEqualTo("test.js");
+    assertThat(factory.getScriptInterfaces()).isNull();
+  }
 
+  @Test
+  void scriptFactoryWithEngineNameCreatesInstance() {
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    assertThat(factory.getScriptSourceLocator()).isEqualTo("test.js");
+  }
 
+  @Test
+  void emptyScriptSourceLocatorThrowsException() {
+    assertThatIllegalArgumentException()
+            .isThrownBy(() -> new StandardScriptFactory(""))
+            .withMessage("'scriptSourceLocator' must not be empty");
+  }
 
+  @Test
+  void requiresConfigInterfaceReturnsFalse() {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js");
+    assertThat(factory.requiresConfigInterface()).isFalse();
+  }
+
+  @Test
+  void getScriptedObjectTypeReturnsNull() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    assertThat(factory.getScriptedObjectType(source)).isNull();
+  }
+
+  @Test
+  void requiresScriptedObjectRefreshDelegatesToSource() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    assertThat(factory.requiresScriptedObjectRefresh(source)).isFalse();
+  }
+
+  @Test
+  void toStringContainsScriptSourceLocator() {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js");
+    assertThat(factory.toString()).contains("test.js");
+  }
+
+  interface TestInterface {
+    String getValue();
+  }
+
+  @Test
+  void scriptFactoryWithInterfacesCreatesInstance() {
+    StandardScriptFactory factory = new StandardScriptFactory("test.js", TestInterface.class);
+    assertThat(factory.getScriptInterfaces()).containsExactly(TestInterface.class);
+  }
+
+  @Test
+  void scriptEngineEvaluationErrorWrapsException() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    when(source.getScriptAsString()).thenReturn("invalid javascript code");
+
+    assertThatExceptionOfType(ScriptCompilationException.class)
+            .isThrownBy(() -> factory.getScriptedObject(source));
+  }
+
+  @Test
+  void adaptToInterfacesWithNullScriptThrowsException() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    when(source.getScriptAsString()).thenReturn("null");
+
+    assertThatExceptionOfType(ScriptCompilationException.class)
+            .isThrownBy(() -> factory.getScriptedObject(source, TestInterface.class));
+  }
+
+  @Test
+  void getScriptedObjectWithMultipleInterfaces() throws IOException {
+    interface Interface1 {
+      String method1();
+    }
+    interface Interface2 {
+      void method2();
+    }
+
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    when(source.getScriptAsString()).thenReturn("var obj = { method1: function() { return 'test'; }, method2: function() {} };");
+
+    assertThatExceptionOfType(ScriptCompilationException.class)
+            .isThrownBy(() -> factory.getScriptedObject(source, Interface1.class, Interface2.class));
+  }
+
+  @Test
+  void scriptSourceReturningUndefinedThrowsException() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    when(source.getScriptAsString()).thenReturn("undefined");
+
+    assertThatExceptionOfType(ScriptCompilationException.class)
+            .isThrownBy(() -> factory.getScriptedObject(source, TestInterface.class));
+  }
+
+  @Test
+  void getScriptedObjectWithInvalidScriptSourceThrowsException() throws IOException {
+    StandardScriptFactory factory = new StandardScriptFactory("javascript", "test.js");
+    ScriptSource source = mock(ScriptSource.class);
+    when(source.getScriptAsString()).thenThrow(new IOException("Failed to read script"));
+
+    assertThatExceptionOfType(ScriptCompilationException.class)
+            .isThrownBy(() -> factory.getScriptedObject(source));
+  }
 
 }
