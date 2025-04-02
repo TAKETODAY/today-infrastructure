f18278015a60fdece1603567cb0a815ba4dbc394
海子 Yang <i.take.today@gmail.com>
2025-04-02 16:29:27 +0800

    :art:

--- today-context/src/main/java/infra/context/expression/CachedExpressionEvaluator.java
+++ today-context/src/main/java/infra/context/expression/CachedExpressionEvaluator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -103,6 +103,7 @@ public abstract class CachedExpressionEvaluator {
   protected static class ExpressionKey implements Comparable<ExpressionKey> {
 
     private final String expression;
+    
     private final AnnotatedElementKey element;
 
     protected ExpressionKey(AnnotatedElementKey element, String expression) {
