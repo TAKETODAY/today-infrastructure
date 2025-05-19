df989034a3af1191db8f668b3fda9f7bb12455a7
海子 Yang <i.take.today@gmail.com>
2025-05-19 17:45:29 +0800

    :fire: 移除多余的 `getMatrixVariable` 方法及优化 `toString` 实现

--- today-web/src/testFixtures/java/infra/web/bind/resolver/MockPathMatchInfo.java
+++ today-web/src/testFixtures/java/infra/web/bind/resolver/MockPathMatchInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -64,13 +64,6 @@ public class MockPathMatchInfo extends PathMatchInfo {
    */
   public Map<String, MultiValueMap<String, String>> getMatrixVariables() {
     return this.matrixVariables;
-  }
-
-  /**
-   * Return matrix variables
-   */
-  public MultiValueMap<String, String> getMatrixVariable(String name) {
-    return this.matrixVariables.get(name);
   }
 
 }
