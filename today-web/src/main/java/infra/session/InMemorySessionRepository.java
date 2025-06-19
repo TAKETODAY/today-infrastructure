07fcd57e160ffc166415a4d857d8d998842daaab
海子 Yang <i.take.today@gmail.com>
2025-06-19 20:53:00 +0800

    :art: Allow update of existing WebSession after max sessions limit is reached

--- today-web/src/main/java/infra/session/InMemorySessionRepository.java
+++ today-web/src/main/java/infra/session/InMemorySessionRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -390,9 +390,8 @@ public class InMemorySessionRepository implements SessionRepository {
     private void checkMaxSessionsLimit() {
       if (sessions.size() >= maxSessions) {
         expiredSessionChecker.removeExpiredSessions(clock.instant());
-        if (sessions.size() >= maxSessions) {
-          throw new TooManyActiveSessionsException(
-                  "Max sessions limit reached: " + sessions.size(), maxSessions);
+        if (sessions.size() >= maxSessions && !sessions.containsKey(this.id.get())) {
+          throw new TooManyActiveSessionsException("Max sessions limit reached: " + sessions.size(), maxSessions);
         }
       }
     }
