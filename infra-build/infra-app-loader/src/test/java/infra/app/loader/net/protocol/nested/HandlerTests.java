4f4b85f52ec0327a3faaf21ae5b4eb174e503aef
海子 Yang <i.take.today@gmail.com>
2025-07-05 14:22:03 +0800

    :art: Never attempt to resolve local address for nested URLs
    Update `nested:` Handler to always return `null` from `getHostAddress`
    in an attempt to improve performance on Windows.

--- infra-build/infra-app-loader/src/test/java/infra/app/loader/net/protocol/nested/HandlerTests.java
+++ infra-build/infra-app-loader/src/test/java/infra/app/loader/net/protocol/nested/HandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -67,6 +67,12 @@ class HandlerTests {
   void assertUrlIsNotMalformedWhenUrlIsValidDoesNotThrowException() {
     String url = "nested:" + this.temp.getAbsolutePath() + "/!nested.jar";
     assertThatNoException().isThrownBy(() -> Handler.assertUrlIsNotMalformed(url));
+  }
+
+  @Test
+  void getHostAddressIsNull() throws Exception {
+    String url = "nested:" + this.temp.getAbsolutePath() + "/!nested.jar";
+    assertThat(new Handler().getHostAddress(new URL(url))).isNull();
   }
 
 }
