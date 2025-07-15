c056697f401ece92926b133c4da5a67c78818acc
海子 Yang <i.take.today@gmail.com>
2025-07-15 14:42:35 +0800

    :art:

--- today-framework/src/main/java/infra/app/context/config/ConfigDataLoader.java
+++ today-framework/src/main/java/infra/app/context/config/ConfigDataLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ package infra.app.context.config;
 
 import java.io.IOException;
 
+import infra.app.BootstrapContext;
 import infra.app.BootstrapRegistry;
 import infra.app.ConfigurableBootstrapContext;
 import infra.lang.Nullable;
@@ -38,6 +39,7 @@ import infra.lang.Nullable;
  * @param <R> the resource type
  * @author Phillip Webb
  * @author Madhura Bhave
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
  * @since 4.0
  */
 public interface ConfigDataLoader<R extends ConfigDataResource> {
