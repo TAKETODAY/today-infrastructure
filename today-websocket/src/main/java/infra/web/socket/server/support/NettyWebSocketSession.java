2c8130022d2475929b50812ee22f315fec7d2cbe
海子 Yang <i.take.today@gmail.com>
2025-08-14 21:58:36 +0800

    :art: NettyWebSocketSession getLocalAddress

--- today-websocket/src/main/java/infra/web/socket/server/support/NettyWebSocketSession.java
+++ today-websocket/src/main/java/infra/web/socket/server/support/NettyWebSocketSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -138,6 +138,12 @@ public class NettyWebSocketSession extends WebSocketSession {
   @Override
   public InetSocketAddress getRemoteAddress() {
     return (InetSocketAddress) channel.remoteAddress();
+  }
+
+  @Nullable
+  @Override
+  public InetSocketAddress getLocalAddress() {
+    return (InetSocketAddress) channel.localAddress();
   }
 
   @Nullable
