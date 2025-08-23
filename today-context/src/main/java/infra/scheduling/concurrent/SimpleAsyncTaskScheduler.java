566d609129442779835c81d9b802014a86d4bdc4
海子 Yang <i.take.today@gmail.com>
2025-08-23 11:36:21 +0800

    :zap: Align task execution tracking and thread interruption on shutdown

--- today-context/src/main/java/infra/scheduling/concurrent/SimpleAsyncTaskScheduler.java
+++ today-context/src/main/java/infra/scheduling/concurrent/SimpleAsyncTaskScheduler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -365,7 +365,7 @@ public class SimpleAsyncTaskScheduler extends SimpleAsyncTaskExecutor implements
 
   @Override
   public boolean isRunning() {
-    return this.triggerLifecycle.isRunning();
+    return triggerLifecycle.isRunning() || fixedDelayLifecycle.isRunning();
   }
 
   @Override
--- today-core/src/main/java/infra/core/task/SimpleAsyncTaskExecutor.java
+++ today-core/src/main/java/infra/core/task/SimpleAsyncTaskExecutor.java
@@ -334,7 +334,6 @@ public class SimpleAsyncTaskExecutor extends CustomizableThreadCreator implement
       this.active = false;
       Set<Thread> threads = this.activeThreads;
       if (threads != null) {
-        threads.forEach(Thread::interrupt);
         synchronized(threads) {
           try {
             if (!threads.isEmpty()) {
@@ -345,6 +344,7 @@ public class SimpleAsyncTaskExecutor extends CustomizableThreadCreator implement
             Thread.currentThread().interrupt();
           }
         }
+        threads.forEach(Thread::interrupt);
       }
     }
   }
