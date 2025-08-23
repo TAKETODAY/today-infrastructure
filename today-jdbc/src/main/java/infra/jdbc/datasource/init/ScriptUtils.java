b03769ed4127fa7ee659ded1c4dbab09467e17e3
海子 Yang <i.take.today@gmail.com>
2025-08-23 14:46:10 +0800

    :sparkles: Support multiple result sets in ScriptUtils.executeSqlScript()
    Prior to this commit, ScriptUtils.executeSqlScript() treated every
    statement within the script as if it were a single insert/update/delete
    statement. This disregarded the fact that the execution of a JDBC
    Statement can result in multiple individual statements, some of which
    result in a ResultSet and others that result in an update count.
    
    For example, when executing a stored procedure on Sybase, ScriptUtils
    did not execute all statements within the stored procedure.
    
    To address that, this commit revises the implementation of
    ScriptUtils.executeSqlScript() so that it handles multiple results and
    differentiates between result sets and update counts.

--- today-jdbc/src/main/java/infra/jdbc/datasource/init/ScriptUtils.java
+++ today-jdbc/src/main/java/infra/jdbc/datasource/init/ScriptUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,12 +28,12 @@ import java.util.List;
 
 import infra.core.io.EncodedResource;
 import infra.core.io.Resource;
+import infra.jdbc.datasource.DataSourceUtils;
 import infra.lang.Assert;
 import infra.lang.Nullable;
 import infra.logging.Logger;
 import infra.logging.LoggerFactory;
 import infra.util.StringUtils;
-import infra.jdbc.datasource.DataSourceUtils;
 
 /**
  * Generic utility methods for working with SQL scripts in conjunction with JDBC.
@@ -50,6 +50,7 @@ import infra.jdbc.datasource.DataSourceUtils;
  * @author Chris Baldwin
  * @author Nicolas Debeissat
  * @author Phillip Webb
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
  * @since 4.0
  */
 public abstract class ScriptUtils {
@@ -224,8 +225,7 @@ public abstract class ScriptUtils {
    * @see DataSourceUtils#getConnection
    * @see DataSourceUtils#releaseConnection
    */
-  public static void executeSqlScript(
-          Connection connection, EncodedResource resource, boolean continueOnError,
+  public static void executeSqlScript(Connection connection, EncodedResource resource, boolean continueOnError,
           boolean ignoreFailedDrops, String[] commentPrefixes, @Nullable String separator,
           String blockCommentStartDelimiter, String blockCommentEndDelimiter) throws ScriptException {
 
@@ -262,17 +262,30 @@ public abstract class ScriptUtils {
         for (String statement : statements) {
           stmtNumber++;
           try {
-            stmt.execute(statement);
-            int rowsAffected = stmt.getUpdateCount();
+            boolean hasResultSet = stmt.execute(statement);
+            int updateCount = -1;
             if (logger.isDebugEnabled()) {
-              logger.debug("{} returned as update count for SQL: {}", rowsAffected, statement);
-              SQLWarning warningToLog = stmt.getWarnings();
-              while (warningToLog != null) {
-                logger.debug("SQLWarning ignored: SQL state '{}', error code '{}', message [{}]",
-                        warningToLog.getSQLState(), warningToLog.getErrorCode(), warningToLog.getMessage());
-                warningToLog = warningToLog.getNextWarning();
-              }
+              logSqlWarnings(stmt);
             }
+            do {
+              if (hasResultSet) {
+                // We invoke getResultSet() to ensure the JDBC driver processes
+                // it, but we intentionally ignore the returned ResultSet since
+                // we cannot do anything meaningful with it here.
+                stmt.getResultSet();
+                if (logger.isDebugEnabled()) {
+                  logger.debug("ResultSet returned for SQL: {}", statement);
+                }
+              }
+              else {
+                updateCount = stmt.getUpdateCount();
+                if (updateCount >= 0 && logger.isDebugEnabled()) {
+                  logger.debug("{} returned as update count for SQL: {}", updateCount, statement);
+                }
+              }
+              hasResultSet = stmt.getMoreResults();
+            }
+            while (hasResultSet || updateCount != -1);
           }
           catch (SQLException ex) {
             boolean dropStatement = StringUtils.startsWithIgnoreCase(statement.trim(), "drop");
@@ -307,6 +320,15 @@ public abstract class ScriptUtils {
       }
       throw new UncategorizedScriptException(
               "Failed to execute database script from resource [" + resource + "]", ex);
+    }
+  }
+
+  private static void logSqlWarnings(Statement stmt) throws SQLException {
+    SQLWarning warningToLog = stmt.getWarnings();
+    while (warningToLog != null) {
+      logger.debug("SQLWarning ignored: SQL state '{}', error code '{}', message [{}]",
+              warningToLog.getSQLState(), warningToLog.getErrorCode(), warningToLog.getMessage());
+      warningToLog = warningToLog.getNextWarning();
     }
   }
 
