a909132d5cfb9f68d3aae58f85cbc1f9ec21fb4a
海子 Yang <i.take.today@gmail.com>
2025-09-28 20:39:37 +0800

    :white_check_mark:

--- today-context/src/test/java/infra/context/annotation/AnnotationScopeMetadataResolverTests.java
+++ today-context/src/test/java/infra/context/annotation/AnnotationScopeMetadataResolverTests.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package infra.context.annotation;
@@ -29,6 +26,7 @@ import java.lang.annotation.RetentionPolicy;
 import infra.beans.factory.annotation.AnnotatedBeanDefinition;
 import infra.beans.factory.annotation.AnnotatedGenericBeanDefinition;
 import infra.beans.factory.config.BeanDefinition;
+import infra.core.annotation.AliasFor;
 import infra.core.type.classreading.MetadataReader;
 import infra.core.type.classreading.MetadataReaderFactory;
 import infra.core.type.classreading.SimpleMetadataReaderFactory;
@@ -141,6 +139,7 @@ public class AnnotationScopeMetadataResolverTests {
   @Scope("request")
   @interface CustomRequestScopeWithAttributeOverride {
 
+    @AliasFor(annotation = Scope.class)
     ScopedProxyMode proxyMode();
   }
 
