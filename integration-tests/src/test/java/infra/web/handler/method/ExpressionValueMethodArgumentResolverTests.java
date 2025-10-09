a072109f03bc2befe65ca3de3f565036c7ee4393
海子 Yang <i.take.today@gmail.com>
2025-10-09 22:12:58 +0800

    :white_check_mark:

--- integration-tests/src/test/java/infra/web/handler/method/ExpressionValueMethodArgumentResolverTests.java
+++ integration-tests/src/test/java/infra/web/handler/method/ExpressionValueMethodArgumentResolverTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,8 +26,10 @@ import java.lang.reflect.Method;
 import infra.beans.factory.annotation.Value;
 import infra.core.DefaultParameterNameDiscoverer;
 import infra.core.MethodParameter;
+import infra.core.ParameterNameDiscoverer;
 import infra.mock.web.HttpMockRequestImpl;
 import infra.mock.web.MockHttpResponseImpl;
+import infra.web.BindingContext;
 import infra.web.RequestContextHolder;
 import infra.web.bind.resolver.ExpressionValueMethodArgumentResolver;
 import infra.web.mock.MockRequestContext;
@@ -103,6 +105,87 @@ public class ExpressionValueMethodArgumentResolverTests {
     Object value = resolver.resolveArgument(webRequest, paramContextPath);
 
     assertThat(value).isEqualTo("");
+  }
+
+  @Test
+  public void supportsParameterWithoutValueAnnotation() throws Exception {
+    Method method = getClass().getMethod("params", int.class, String.class, String.class);
+    ResolvableMethodParameter param = new ResolvableMethodParameter(new MethodParameter(method, 2));
+    assertThat(resolver.supportsParameter(param)).isFalse();
+  }
+
+  @Test
+  public void resolveArgumentWithPlaceholder() throws Throwable {
+    System.setProperty("test.property", "test-value");
+
+    Method method = getClass().getMethod("methodWithPlaceholder", String.class);
+    MethodParameter parameter = new MethodParameter(method, 0);
+    parameter.initParameterNameDiscovery(ParameterNameDiscoverer.getSharedInstance());
+    ResolvableMethodParameter param = new ResolvableMethodParameter(parameter);
+
+    Object value = resolver.resolveArgument(webRequest, param);
+    System.clearProperty("test.property");
+
+    assertThat(value).isEqualTo("test-value");
+  }
+
+  @Test
+  public void resolveArgumentWithDefaultValue() throws Throwable {
+    Method method = getClass().getMethod("methodWithDefaultValue", String.class);
+    MethodParameter parameter = new MethodParameter(method, 0);
+    parameter.initParameterNameDiscovery(ParameterNameDiscoverer.getSharedInstance());
+    ResolvableMethodParameter param = new ResolvableMethodParameter(parameter);
+
+    Object value = resolver.resolveArgument(webRequest, param);
+
+    assertThat(value).isEqualTo("default");
+  }
+
+  @Test
+  public void resolveArgumentWithSpelExpression() throws Throwable {
+    request.setRequestURI("/test/path");
+
+    Method method = getClass().getMethod("methodWithSpelExpression", String.class);
+    MethodParameter parameter = new MethodParameter(method, 0);
+    parameter.initParameterNameDiscovery(ParameterNameDiscoverer.getSharedInstance());
+    ResolvableMethodParameter param = new ResolvableMethodParameter(parameter);
+
+    Object value = resolver.resolveArgument(webRequest, param);
+
+    assertThat(value).isEqualTo("/test/path");
+  }
+
+  @Test
+  public void resolveArgumentWithTypeConversion() throws Throwable {
+    System.setProperty("number.property", "123");
+
+    Method method = getClass().getMethod("methodWithNumber", int.class);
+    MethodParameter parameter = new MethodParameter(method, 0);
+    parameter.initParameterNameDiscovery(ParameterNameDiscoverer.getSharedInstance());
+    ResolvableMethodParameter param = new ResolvableMethodParameter(parameter);
+
+    Object value = resolver.resolveArgument(webRequest, param);
+    assertThat(value).isEqualTo("123");
+
+    webRequest.setBinding(new BindingContext());
+    value = resolver.resolveArgument(webRequest, param);
+
+    System.clearProperty("number.property");
+
+    assertThat(value).isEqualTo(123);
+  }
+
+  @SuppressWarnings("unused")
+  public void methodWithPlaceholder(@Value("${test.property}") String param) { }
+
+  @SuppressWarnings("unused")
+  public void methodWithDefaultValue(@Value("${nonexistent.property:default}") String param) { }
+
+  @SuppressWarnings("unused")
+  public void methodWithSpelExpression(@Value("#{request.requestURI}") String param) { }
+
+  @SuppressWarnings("unused")
+  public void methodWithNumber(@Value("${number.property}") int param) {
   }
 
   public void params(@Value("#{systemProperties.systemProperty}") int param1,
