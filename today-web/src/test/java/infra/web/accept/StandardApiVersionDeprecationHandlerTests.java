a752079b7b52233174a2b0a058d40fd38e6b6b92
海子 Yang <i.take.today@gmail.com>
2025-10-10 15:17:09 +0800

    :white_check_mark:

--- today-web/src/test/java/infra/web/accept/StandardApiVersionDeprecationHandlerTests.java
+++ today-web/src/test/java/infra/web/accept/StandardApiVersionDeprecationHandlerTests.java
@@ -23,6 +23,7 @@ import java.net.URI;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+import infra.http.MediaType;
 import infra.mock.api.http.HttpMockRequest;
 import infra.mock.api.http.HttpMockResponse;
 import infra.mock.web.HttpMockRequestImpl;
@@ -65,6 +66,147 @@ class StandardApiVersionDeprecationHandlerTests {
     assertThat(response.getHeaders("Link")).containsExactlyInAnyOrder(
             "<" + deprecationUrl + ">; rel=\"deprecation\"; type=\"text/html\"",
             "<" + sunsetUrl + ">; rel=\"sunset\"; type=\"text/html\""
+    );
+
+    assertThat(handler.toString()).startsWith("StandardApiVersionDeprecationHandler [VersionInfo[version=1.1,");
+  }
+
+  @Test
+  void handleVersionWithNoMatchDoesNotSetHeaders() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    handler.configureVersion("1.0")
+            .setDeprecationDate(getDate("Fri, 30 Jun 2023 23:59:59 GMT"));
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v2.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("2.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeader("Deprecation")).isNull();
+  }
+
+  @Test
+  void handleVersionWithPredicateMismatchDoesNotSetHeaders() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    handler.configureVersion("1.0")
+            .setRequestPredicate(request -> request.getRequestURL().toString().contains("internal"))
+            .setDeprecationDate(getDate("Fri, 30 Jun 2023 23:59:59 GMT"));
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v1.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("1.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeader("Deprecation")).isNull();
+  }
+
+  @Test
+  void setDeprecationLinkWithCustomMediaType() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    URI deprecationUri = URI.create("https://example.com/deprecation");
+    MediaType customMediaType = MediaType.APPLICATION_JSON;
+
+    handler.configureVersion("1.0")
+            .setDeprecationLink(deprecationUri, customMediaType);
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v1.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("1.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeaders("Link")).containsExactly(
+            "<" + deprecationUri + ">; rel=\"deprecation\"; type=\"" + customMediaType + "\""
+    );
+  }
+
+  @Test
+  void setSunsetLinkWithCustomMediaType() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    URI sunsetUri = URI.create("https://example.com/sunset");
+    MediaType customMediaType = MediaType.APPLICATION_JSON;
+
+    handler.configureVersion("1.0")
+            .setSunsetLink(sunsetUri, customMediaType);
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v1.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("1.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeaders("Link")).containsExactly(
+            "<" + sunsetUri + ">; rel=\"sunset\"; type=\"" + customMediaType + "\""
+    );
+  }
+
+  @Test
+  void handleVersionWithMultipleLinkHeaders() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    URI deprecationUri = URI.create("https://example.com/deprecation");
+    URI sunsetUri = URI.create("https://example.com/sunset");
+
+    handler.configureVersion("1.0")
+            .setDeprecationLink(deprecationUri)
+            .setSunsetLink(sunsetUri);
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v1.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("1.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeaders("Link")).containsExactlyInAnyOrder(
+            "<" + deprecationUri + ">; rel=\"deprecation\"; type=\"text/html\"",
+            "<" + sunsetUri + ">; rel=\"sunset\"; type=\"text/html\""
+    );
+  }
+
+  @Test
+  void handleVersionWithAllHeadersSet() {
+    ApiVersionParser<String> parser = version -> version;
+    StandardApiVersionDeprecationHandler handler = new StandardApiVersionDeprecationHandler(parser);
+
+    ZonedDateTime deprecationDate = getDate("Fri, 30 Jun 2023 23:59:59 GMT");
+    ZonedDateTime sunsetDate = getDate("Wed, 11 Nov 2026 11:11:11 GMT");
+    URI deprecationUri = URI.create("https://example.com/deprecation");
+    URI sunsetUri = URI.create("https://example.com/sunset");
+
+    handler.configureVersion("1.0")
+            .setDeprecationDate(deprecationDate)
+            .setDeprecationLink(deprecationUri)
+            .setSunsetDate(sunsetDate)
+            .setSunsetLink(sunsetUri);
+
+    HttpMockRequest request = new HttpMockRequestImpl("GET", "/api/v1.0/resource");
+    HttpMockResponse response = new MockHttpResponseImpl();
+    MockRequestContext context = new MockRequestContext(request, response);
+
+    handler.handleVersion("1.0", context);
+    context.requestCompleted();
+
+    assertThat(response.getHeader("Deprecation")).isEqualTo("@1688169599");
+    assertThat(response.getHeader("Sunset")).isEqualTo("Wed, 11 Nov 2026 11:11:11 GMT");
+    assertThat(response.getHeaders("Link")).containsExactlyInAnyOrder(
+            "<" + deprecationUri + ">; rel=\"deprecation\"; type=\"text/html\"",
+            "<" + sunsetUri + ">; rel=\"sunset\"; type=\"text/html\""
     );
   }
 
