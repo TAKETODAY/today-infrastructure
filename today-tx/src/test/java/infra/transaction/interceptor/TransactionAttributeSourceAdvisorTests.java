0f93ddc1ce433175a25695f0118f871deb9fa5d4
海子 Yang <i.take.today@gmail.com>
2025-10-12 22:40:34 +0800

    :white_check_mark:

--- today-tx/src/test/java/infra/transaction/interceptor/TransactionAttributeSourceAdvisorTests.java
+++ today-tx/src/test/java/infra/transaction/interceptor/TransactionAttributeSourceAdvisorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
 
+import infra.aop.ClassFilter;
+import infra.aop.Pointcut;
+import infra.aop.support.AbstractPointcutAdvisor;
 import infra.core.testfixture.io.SerializationTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author Rod Johnson
@@ -34,6 +40,82 @@ public class TransactionAttributeSourceAdvisorTests {
     ti.setTransactionAttributes(new Properties());
     TransactionAttributeSourceAdvisor tas = new TransactionAttributeSourceAdvisor(ti);
     SerializationTestUtils.serializeAndDeserialize(tas);
+  }
+
+  @Test
+  void constructorWithInterceptor() {
+    TransactionInterceptor interceptor = new TransactionInterceptor();
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor(interceptor);
+
+    assertThat(advisor).isNotNull();
+    assertThat(advisor.getAdvice()).isEqualTo(interceptor);
+  }
+
+  @Test
+  void setTransactionInterceptor() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+    TransactionInterceptor interceptor = new TransactionInterceptor();
+
+    advisor.setTransactionInterceptor(interceptor);
+
+    assertThat(advisor.getAdvice()).isEqualTo(interceptor);
+  }
+
+  @Test
+  void setTransactionInterceptorWithNullThrowsException() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+
+    assertThatThrownBy(() -> advisor.setTransactionInterceptor(null))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("TransactionInterceptor is required");
+  }
+
+  @Test
+  void getAdviceWithoutInterceptorThrowsException() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+
+    assertThatThrownBy(() -> advisor.getAdvice())
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("No TransactionInterceptor set");
+  }
+
+  @Test
+  void getPointcutReturnsTransactionAttributeSourcePointcut() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+
+    Pointcut pointcut = advisor.getPointcut();
+
+    assertThat(pointcut).isNotNull();
+    assertThat(pointcut).isInstanceOf(TransactionAttributeSourcePointcut.class);
+  }
+
+  @Test
+  void setClassFilterUpdatesPointcut() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+    ClassFilter classFilter = ClassFilter.TRUE;
+
+    advisor.setClassFilter(classFilter);
+
+    Pointcut pointcut = advisor.getPointcut();
+    assertThat(pointcut).isNotNull();
+  }
+
+  @Test
+  void advisorExtendsAbstractPointcutAdvisor() {
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor();
+    assertThat(advisor).isInstanceOf(AbstractPointcutAdvisor.class);
+  }
+
+  @Test
+  void serializabilityWithInterceptor() throws Exception {
+    TransactionInterceptor interceptor = new TransactionInterceptor();
+    NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+    source.addTransactionalMethod("test", new DefaultTransactionAttribute());
+    interceptor.setTransactionAttributeSource(source);
+
+    TransactionAttributeSourceAdvisor advisor = new TransactionAttributeSourceAdvisor(interceptor);
+
+    SerializationTestUtils.serializeAndDeserialize(advisor);
   }
 
 }
