ccd07b921b5df884b51010cbc9d8ce9243fcba03
海子 Yang <i.take.today@gmail.com>
2025-11-07 21:44:39 +0800

    :white_check_mark:

---
+++ today-core/src/test/java/infra/logging/SLF4JBridgeHandlerTests.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2017 - 2025 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+package infra.logging;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0 2025/11/7 21:42
+ */
+class SLF4JBridgeHandlerTests {
+
+  @Test
+  void shouldInstallSLF4JBridgeHandler() {
+    // First uninstall any existing handlers to have a clean state
+    SLF4JBridgeHandler.uninstall();
+
+    // Install the bridge handler
+    SLF4JBridgeHandler.install();
+
+    // Verify it's installed
+    assertThat(SLF4JBridgeHandler.isInstalled()).isTrue();
+  }
+
+  @Test
+  void shouldUninstallSLF4JBridgeHandler() {
+    // Ensure handler is installed first
+    SLF4JBridgeHandler.uninstall();
+    SLF4JBridgeHandler.install();
+
+    // Now uninstall it
+    SLF4JBridgeHandler.uninstall();
+
+    // Verify it's uninstalled
+    assertThat(SLF4JBridgeHandler.isInstalled()).isFalse();
+  }
+
+  @Test
+  void shouldReturnFalseWhenNotInstalled() {
+    SLF4JBridgeHandler.uninstall();
+
+    assertThat(SLF4JBridgeHandler.isInstalled()).isFalse();
+  }
+
+  @Test
+  void shouldPublishLogRecordToSLF4J() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Test message");
+    record.setLoggerName("test.logger");
+
+    // Should not throw any exception
+    handler.publish(record);
+  }
+
+  @Test
+  void shouldHandleNullLogRecord() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+
+    // Should not throw any exception when record is null
+    handler.publish(null);
+  }
+
+  @Test
+  void shouldHandleLogRecordWithNullMessage() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, null);
+    record.setLoggerName("test.logger");
+
+    // Should not throw any exception
+    handler.publish(record);
+  }
+
+  @Test
+  void shouldCloseAndFlushWithoutException() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+
+    // These should not throw exceptions
+    handler.close();
+    handler.flush();
+  }
+
+  @Test
+  void shouldGetSLF4JLogger() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Test message");
+    record.setLoggerName("test.slf4j.logger");
+
+    org.slf4j.Logger logger = handler.getSLF4JLogger(record);
+
+    assertThat(logger).isNotNull();
+    assertThat(logger.getName()).isEqualTo("test.slf4j.logger");
+  }
+
+  @Test
+  void shouldHandleLogRecordWithNullLoggerName() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Test message");
+    record.setLoggerName(null);
+
+    org.slf4j.Logger logger = handler.getSLF4JLogger(record);
+
+    assertThat(logger).isNotNull();
+    assertThat(logger.getName()).isEqualTo("unknown.jul.logger");
+  }
+
+  @Test
+  void shouldMapJULLevelsToSLF4JLevels() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+
+    // Test various JUL levels mapping to SLF4J levels
+    java.util.logging.LogRecord finestRecord = new java.util.logging.LogRecord(java.util.logging.Level.FINEST, "Trace message");
+    java.util.logging.LogRecord finerRecord = new java.util.logging.LogRecord(java.util.logging.Level.FINER, "Debug message");
+    java.util.logging.LogRecord fineRecord = new java.util.logging.LogRecord(java.util.logging.Level.FINE, "Debug message");
+    java.util.logging.LogRecord infoRecord = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Info message");
+    java.util.logging.LogRecord warningRecord = new java.util.logging.LogRecord(java.util.logging.Level.WARNING, "Warn message");
+    java.util.logging.LogRecord severeRecord = new java.util.logging.LogRecord(java.util.logging.Level.SEVERE, "Error message");
+
+    finestRecord.setLoggerName("test.level");
+    finerRecord.setLoggerName("test.level");
+    fineRecord.setLoggerName("test.level");
+    infoRecord.setLoggerName("test.level");
+    warningRecord.setLoggerName("test.level");
+    severeRecord.setLoggerName("test.level");
+
+    // Should not throw exceptions
+    handler.publish(finestRecord);
+    handler.publish(finerRecord);
+    handler.publish(fineRecord);
+    handler.publish(infoRecord);
+    handler.publish(warningRecord);
+    handler.publish(severeRecord);
+
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  void shouldHandleLogRecordWithParameters() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Test message with {0} and {1}");
+    record.setLoggerName("test.params");
+    record.setParameters(new Object[] { "param1", 42 });
+
+    handler.publish(record);
+
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  void shouldCallLocationAwareLogger() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Location aware message");
+    record.setLoggerName("test.location");
+
+    // This will call callLocationAwareLogger if the SLF4J logger implements LocationAwareLogger
+    handler.publish(record);
+
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  void shouldCallPlainSLF4JLogger() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Plain logger message");
+    record.setLoggerName("test.plain");
+
+    // This will call callPlainSLF4JLogger for regular SLF4J loggers
+    handler.publish(record);
+
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  void shouldHandleMalformedMessageFormat() {
+    SLF4JBridgeHandler handler = new SLF4JBridgeHandler();
+    java.util.logging.LogRecord record = new java.util.logging.LogRecord(java.util.logging.Level.INFO, "Test message with {0 and {1}");
+    record.setLoggerName("test.malformed");
+    record.setParameters(new Object[] { "param1", 42 });
+
+    handler.publish(record);
+
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  void shouldRemoveExistingHandlersDuringInstall() {
+    // First ensure clean state
+    SLF4JBridgeHandler.uninstall();
+
+    // Add a dummy handler to root logger
+    java.util.logging.Logger rootLogger = java.util.logging.LogManager.getLogManager().getLogger("");
+    java.util.logging.Handler dummyHandler = new java.util.logging.Handler() {
+      @Override
+      public void publish(java.util.logging.LogRecord record) { }
+
+      @Override
+      public void flush() { }
+
+      @Override
+      public void close() throws SecurityException { }
+    };
+    rootLogger.addHandler(dummyHandler);
+
+    // Install SLF4JBridgeHandler
+    SLF4JBridgeHandler.install();
+
+    // Verify our dummy handler was removed and SLF4JBridgeHandler was added
+    boolean hasSLF4JBridgeHandler = false;
+    for (java.util.logging.Handler handler : rootLogger.getHandlers()) {
+      if (handler instanceof SLF4JBridgeHandler) {
+        hasSLF4JBridgeHandler = true;
+        break;
+      }
+    }
+
+    assertThat(hasSLF4JBridgeHandler).isTrue();
+    assertThat(rootLogger.getHandlers()).hasSize(1);
+  }
+
+}
