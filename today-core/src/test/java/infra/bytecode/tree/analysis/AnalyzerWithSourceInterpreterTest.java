44c9b0cf9d94625da1d5ab2bc6f4d90290c28e2b
海子 Yang <i.take.today@gmail.com>
2025-11-15 19:05:40 +0800

    :bug: Fix DUP_X2 and DUP_X1 not copying values correctly

--- today-core/src/test/java/infra/bytecode/tree/analysis/AnalyzerWithSourceInterpreterTest.java
+++ today-core/src/test/java/infra/bytecode/tree/analysis/AnalyzerWithSourceInterpreterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,23 +20,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+
 import infra.bytecode.AsmTest;
 import infra.bytecode.ClassReader;
+import infra.bytecode.Label;
+import infra.bytecode.Opcodes;
+import infra.bytecode.tree.AbstractInsnNode;
 import infra.bytecode.tree.ClassNode;
 import infra.bytecode.tree.MethodNode;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit tests for {@link Analyzer}, when used with a {@link SourceInterpreter}.
  *
  * @author Eric Bruneton
  */
-public class AnalyzerWithSourceInterpreterTest extends AsmTest {
+class AnalyzerWithSourceInterpreterTest extends AsmTest {
 
   @Test
-  public void testConstructor() {
-    assertDoesNotThrow(() -> new SourceInterpreter());
+  void testConstructor() {
+    assertDoesNotThrow(SourceInterpreter::new);
   }
 
   /**
@@ -46,8 +53,8 @@ public class AnalyzerWithSourceInterpreterTest extends AsmTest {
    */
   @ParameterizedTest
   @MethodSource(ALL_CLASSES_AND_LATEST_API)
-  public void testAnalyze_sourceInterpreter(
-          final PrecompiledClass classParameter) {
+  void testAnalyze_sourceInterpreter(
+          final PrecompiledClass classParameter, final Api apiParameter) {
     ClassNode classNode = new ClassNode();
     new ClassReader(classParameter.getBytes()).accept(classNode, 0);
     Analyzer<SourceValue> analyzer = new Analyzer<>(new SourceInterpreter());
@@ -55,5 +62,57 @@ public class AnalyzerWithSourceInterpreterTest extends AsmTest {
     for (MethodNode methodNode : classNode.methods) {
       assertDoesNotThrow(() -> analyzer.analyze(classNode.name, methodNode));
     }
+  }
+
+  /** Checks if DUP_X2 producers are correct. */
+  @Test
+  void testAnalyze_dupx2Producers() throws AnalyzerException {
+    Label label0 = new Label();
+    Label label1 = new Label();
+    MethodNode methodNode =
+            new MethodNodeBuilder(4, 1)
+                    .push()
+                    .push()
+                    .iconst_0()
+                    .ifne(label0)
+                    // First case
+                    .insn(Opcodes.ICONST_M1)
+                    .go(label1)
+                    // Second case
+                    .label(label0)
+                    .iconst_0()
+                    // DUP_X2 value
+                    .label(label1)
+                    .insn(Opcodes.DUP_X2)
+                    .pop() // Point where the frame is checked
+                    .pop()
+                    .pop()
+                    .pop()
+                    .vreturn()
+                    .build();
+
+    Analyzer<SourceValue> analyzer = new Analyzer<>(new SourceInterpreter());
+    analyzer.analyze("C", methodNode);
+
+    AbstractInsnNode firstPop =
+            Arrays.stream(methodNode.instructions.toArray())
+                    .filter(insn -> insn.getOpcode() == Opcodes.POP)
+                    .findFirst()
+                    .get();
+    AbstractInsnNode dupx2 =
+            Arrays.stream(methodNode.instructions.toArray())
+                    .filter(insn -> insn.getOpcode() == Opcodes.DUP_X2)
+                    .findFirst()
+                    .get();
+    Frame<SourceValue> frame = analyzer.getFrames()[methodNode.instructions.indexOf(firstPop)];
+    // Check if all source values have the DUP_X2 as a producer
+    SourceValue sourceValue1 = frame.getStack(frame.getStackSize() - 4);
+    SourceValue sourceValue2 = frame.getStack(frame.getStackSize() - 3);
+    SourceValue sourceValue3 = frame.getStack(frame.getStackSize() - 2);
+    SourceValue sourceValue4 = frame.getStack(frame.getStackSize() - 1);
+    assertEquals(sourceValue1.insns.iterator().next(), dupx2);
+    assertEquals(sourceValue2.insns.iterator().next(), dupx2);
+    assertEquals(sourceValue3.insns.iterator().next(), dupx2);
+    assertEquals(sourceValue4.insns.iterator().next(), dupx2);
   }
 }
