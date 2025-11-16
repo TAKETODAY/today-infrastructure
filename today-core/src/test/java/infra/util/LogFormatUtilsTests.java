f521bbc77cd9cdeee1c3099fdbfb6e208ee0ade1
海子 Yang <i.take.today@gmail.com>
2025-11-16 22:34:27 +0800

    :white_check_mark:

---
+++ today-core/src/test/java/infra/util/LogFormatUtilsTests.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2017 - 2025 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+package infra.util;
+
+import org.junit.jupiter.api.Test;
+
+import infra.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0 2025/11/16 22:29
+ */
+class LogFormatUtilsTests {
+
+  @Test
+  void formatValueWithNullReturnsEmptyString() {
+    assertThat(LogFormatUtils.formatValue(null, true)).isEqualTo("");
+    assertThat(LogFormatUtils.formatValue(null, false)).isEqualTo("");
+  }
+
+  @Test
+  void formatValueWithStringUnderLimit() {
+    String value = "Hello World";
+    assertThat(LogFormatUtils.formatValue(value, true)).isEqualTo("\"Hello World\"");
+    assertThat(LogFormatUtils.formatValue(value, false)).isEqualTo("\"Hello World\"");
+  }
+
+  @Test
+  void formatValueWithNonCharSequenceUnderLimit() {
+    Integer value = 42;
+    assertThat(LogFormatUtils.formatValue(value, true)).isEqualTo("42");
+    assertThat(LogFormatUtils.formatValue(value, false)).isEqualTo("42");
+  }
+
+  @Test
+  void formatValueWithNonCharSequenceOverLimit() {
+    String value = "This is a very long string representation that exceeds the limit of 100 characters and should be truncated accordingly";
+    Object object = new Object() {
+      @Override
+      public String toString() {
+        return value;
+      }
+    };
+
+    String result = LogFormatUtils.formatValue(object, 50, false);
+    assertThat(result.length()).isEqualTo(50 + " (truncated)...".length());
+    assertThat(result).startsWith("This is a very long string representation");
+  }
+
+  @Test
+  void formatValueReplacesNewlinesAndControlCharacters() {
+    String value = "Line1\nLine2\rLine3\u0001";
+    String result = LogFormatUtils.formatValue(value, 100, true);
+    assertThat(result).isEqualTo("\"Line1<EOL>Line2<EOL>Line3?\"");
+  }
+
+  @Test
+  void formatValueHandlesExceptionInToString() {
+    Object value = new Object() {
+      @Override
+      public String toString() {
+        throw new RuntimeException("toString failed");
+      }
+    };
+
+    String result = LogFormatUtils.formatValue(value, true);
+    assertThat(result).contains("RuntimeException");
+  }
+
+  @Test
+  void formatValueWithMaxLengthNegativeOne() {
+    String value = "Some text";
+    String result = LogFormatUtils.formatValue(value, -1, false);
+    assertThat(result).isEqualTo("\"Some text\"");
+  }
+
+  @Test
+  void traceDebugLogsAtDebugLevelWhenTraceDisabled() {
+    Logger logger = mock(Logger.class);
+    when(logger.isDebugEnabled()).thenReturn(true);
+    when(logger.isTraceEnabled()).thenReturn(false);
+
+    LogFormatUtils.traceDebug(logger, isTrace -> isTrace ? "trace message" : "debug message");
+
+    verify(logger).debug("debug message");
+    verify(logger, never()).trace(anyString());
+  }
+
+  @Test
+  void traceDebugLogsAtTraceLevelWhenTraceEnabled() {
+    Logger logger = mock(Logger.class);
+    when(logger.isDebugEnabled()).thenReturn(true);
+    when(logger.isTraceEnabled()).thenReturn(true);
+
+    LogFormatUtils.traceDebug(logger, isTrace -> isTrace ? "trace message" : "debug message");
+
+    verify(logger).trace("trace message");
+    verify(logger, never()).debug(anyString());
+  }
+
+  @Test
+  void traceDebugDoesNothingWhenDebugEnabledReturnsFalse() {
+    Logger logger = mock(Logger.class);
+    when(logger.isDebugEnabled()).thenReturn(false);
+
+    LogFormatUtils.traceDebug(logger, isTrace -> { throw new AssertionError("Should not be called"); });
+
+    verify(logger, never()).isTraceEnabled();
+    verify(logger, never()).trace(anyString());
+    verify(logger, never()).debug(anyString());
+  }
+
+}
--- today-core/src/test/java/infra/util/UnmodifiableMultiValueMapTests.java
+++ today-core/src/test/java/infra/util/UnmodifiableMultiValueMapTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2024 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,8 +27,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Spliterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -177,8 +180,457 @@ class UnmodifiableMultiValueMapTests {
     assertThatUnsupportedOperationException().isThrownBy(() -> values.clear());
   }
 
+  @Test
+  void constructorWithNullDelegateThrowsException() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new UnmodifiableMultiValueMap<>(null));
+  }
+
+  @Test
+  void getReturnsUnmodifiableList() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value1");
+    delegate.add("key", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    List<String> result = unmodifiable.get("key");
+
+    assertThat(result).isNotNull().containsExactly("value1", "value2");
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> result.add("value3"));
+  }
+
+  @Test
+  void getReturnsNullForNonExistentKey() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.get("nonexistent")).isNull();
+  }
+
+  @Test
+  void getFirstReturnsCorrectValue() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "first");
+    delegate.add("key", "second");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    assertThat(unmodifiable.getFirst("key")).isEqualTo("first");
+  }
+
+  @Test
+  void getFirstReturnsNullForNonExistentKey() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.getFirst("nonexistent")).isNull();
+  }
+
+  @Test
+  void keySetIsUnmodifiable() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<String> keySet = unmodifiable.keySet();
+
+    assertThat(keySet).containsExactlyInAnyOrder("key1", "key2");
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> keySet.add("key3"));
+  }
+
+  @Test
+  void keySetIsCached() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    Set<String> keySet1 = unmodifiable.keySet();
+    Set<String> keySet2 = unmodifiable.keySet();
+
+    assertThat(keySet1).isSameAs(keySet2);
+  }
+
+  @Test
+  void valuesIsUnmodifiable() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    assertThat(values).hasSize(2);
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> values.add(List.of("value3")));
+  }
+
+  @Test
+  void valuesIsCached() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    Collection<List<String>> values1 = unmodifiable.values();
+    Collection<List<String>> values2 = unmodifiable.values();
+
+    assertThat(values1).isSameAs(values2);
+  }
+
+  @Test
+  void entrySetIteratorProvidesUnmodifiableEntries() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value1");
+    delegate.add("key", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    Iterator<Map.Entry<String, List<String>>> iterator = entrySet.iterator();
+    assertThat(iterator.hasNext()).isTrue();
+
+    Map.Entry<String, List<String>> entry = iterator.next();
+    assertThat(entry.getKey()).isEqualTo("key");
+    assertThat(entry.getValue()).containsExactly("value1", "value2");
+
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> entry.setValue(List.of("newvalue")));
+  }
+
+  @Test
+  void entrySetIsCached() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    Set<Map.Entry<String, List<String>>> entrySet1 = unmodifiable.entrySet();
+    Set<Map.Entry<String, List<String>>> entrySet2 = unmodifiable.entrySet();
+
+    assertThat(entrySet1).isSameAs(entrySet2);
+  }
+
+  @Test
+  void forEachIteratesOverEntries() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    List<String> collectedKeys = new ArrayList<>();
+    List<List<String>> collectedValues = new ArrayList<>();
+
+    unmodifiable.forEach((key, values) -> {
+      collectedKeys.add(key);
+      collectedValues.add(values);
+    });
+
+    assertThat(collectedKeys).containsExactlyInAnyOrder("key1", "key2");
+    assertThat(collectedValues).containsExactlyInAnyOrder(List.of("value1"), List.of("value2"));
+  }
+
+  @Test
+  void hashCodeEqualsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.hashCode()).isEqualTo(delegate.hashCode());
+  }
+
+  @Test
+  void equalsReturnsTrueForSameInstance() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.equals(unmodifiable)).isTrue();
+  }
+
+  @Test
+  void equalsReturnsTrueForEqualDelegate() {
+    MultiValueMap<String, String> delegate1 = new MappingMultiValueMap<>();
+    delegate1.add("key", "value");
+    MultiValueMap<String, String> delegate2 = new MappingMultiValueMap<>();
+    delegate2.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable1 = new UnmodifiableMultiValueMap<>(delegate1);
+    UnmodifiableMultiValueMap<String, String> unmodifiable2 = new UnmodifiableMultiValueMap<>(delegate2);
+
+    assertThat(unmodifiable1.equals(unmodifiable2)).isTrue();
+  }
+
+  @Test
+  void equalsReturnsFalseForDifferentDelegate() {
+    MultiValueMap<String, String> delegate1 = new MappingMultiValueMap<>();
+    delegate1.add("key1", "value1");
+    MultiValueMap<String, String> delegate2 = new MappingMultiValueMap<>();
+    delegate2.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable1 = new UnmodifiableMultiValueMap<>(delegate1);
+    UnmodifiableMultiValueMap<String, String> unmodifiable2 = new UnmodifiableMultiValueMap<>(delegate2);
+
+    assertThat(unmodifiable1.equals(unmodifiable2)).isFalse();
+  }
+
+  @Test
+  void toStringReturnsDelegateToString() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.toString()).isEqualTo(delegate.toString());
+  }
+
+  @Test
+  void asReadOnlyReturnsSelf() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.asReadOnly()).isSameAs(unmodifiable);
+  }
+
+  @Test
+  void asWritableReturnsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    assertThat(unmodifiable.asWritable()).isSameAs(delegate);
+  }
+
+  @Test
+  void getOrDefaultReturnsUnmodifiableList() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value1");
+    delegate.add("key", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    List<String> result = unmodifiable.getOrDefault("key", List.of("default"));
+
+    assertThat(result).isNotNull().containsExactly("value1", "value2");
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> result.add("value3"));
+  }
+
+  @Test
+  void getOrDefaultReturnsDefaultValueWhenKeyNotPresent() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+
+    List<String> defaultValue = List.of("default");
+    List<String> result = unmodifiable.getOrDefault("nonexistent", defaultValue);
+
+    assertThat(result).isSameAs(defaultValue);
+  }
+
+  @Test
+  void toSingleValueMapReturnsCorrectMap() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key1", "value2");
+    delegate.add("key2", "value3");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Map<String, String> singleValueMap = unmodifiable.toSingleValueMap();
+
+    assertThat(singleValueMap).containsExactly(Assertions.entry("key1", "value1"), Assertions.entry("key2", "value3"));
+  }
+
+  @Test
+  void entrySetToArrayReturnsUnmodifiableEntries() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value1");
+    delegate.add("key", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    Object[] array = entrySet.toArray();
+    assertThat(array).hasSize(1);
+
+    @SuppressWarnings("unchecked")
+    Map.Entry<String, List<String>> entry = (Map.Entry<String, List<String>>) array[0];
+    assertThat(entry.getKey()).isEqualTo("key");
+    assertThat(entry.getValue()).containsExactly("value1", "value2");
+
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> entry.setValue(List.of("newvalue")));
+  }
+
+  @Test
+  void entrySetToArrayWithTypedArrayReturnsUnmodifiableEntries() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value1");
+    delegate.add("key", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    Map.Entry<String, List<String>>[] array = entrySet.toArray(new Map.Entry[0]);
+    assertThat(array).hasSize(1);
+
+    Map.Entry<String, List<String>> entry = array[0];
+    assertThat(entry.getKey()).isEqualTo("key");
+    assertThat(entry.getValue()).containsExactly("value1", "value2");
+
+    assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> entry.setValue(List.of("newvalue")));
+  }
+
+  @Test
+  void valuesToArrayReturnsUnmodifiableLists() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key1", "value2");
+    delegate.add("key2", "value3");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    Object[] array = values.toArray();
+    assertThat(array).hasSize(2);
+
+    for (Object obj : array) {
+      @SuppressWarnings("unchecked")
+      List<String> list = (List<String>) obj;
+      assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> list.add("newvalue"));
+    }
+  }
+
+  @Test
+  void valuesToArrayWithTypedArrayReturnsUnmodifiableLists() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key1", "value2");
+    delegate.add("key2", "value3");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    List<String>[] array = values.toArray(new List[0]);
+    assertThat(array).hasSize(2);
+
+    for (List<String> list : array) {
+      assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> list.add("newvalue"));
+    }
+  }
+
+  @Test
+  void entrySetSpliteratorWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    Spliterator<Map.Entry<String, List<String>>> spliterator = entrySet.spliterator();
+    assertThat(spliterator).isNotNull();
+    assertThat(spliterator.estimateSize()).isEqualTo(2);
+  }
+
+  @Test
+  void valuesSpliteratorWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    Spliterator<List<String>> spliterator = values.spliterator();
+    assertThat(spliterator).isNotNull();
+    assertThat(spliterator.estimateSize()).isEqualTo(2);
+  }
+
+  @Test
+  void entrySetStreamWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    long count = entrySet.stream().count();
+    assertThat(count).isEqualTo(2);
+  }
+
+  @Test
+  void valuesStreamWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    long count = values.stream().count();
+    assertThat(count).isEqualTo(2);
+  }
+
+  @Test
+  void entrySetParallelStreamWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    long count = entrySet.parallelStream().count();
+    assertThat(count).isEqualTo(2);
+  }
+
+  @Test
+  void valuesParallelStreamWorksCorrectly() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key1", "value1");
+    delegate.add("key2", "value2");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    long count = values.parallelStream().count();
+    assertThat(count).isEqualTo(2);
+  }
+
+  @Test
+  void entrySetHashCodeEqualsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    assertThat(entrySet.hashCode()).isEqualTo(delegate.entrySet().hashCode());
+  }
+
+  @Test
+  void valuesHashCodeEqualsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    assertThat(values.hashCode()).isEqualTo(delegate.values().hashCode());
+  }
+
+  @Test
+  void entrySetToStringEqualsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Set<Map.Entry<String, List<String>>> entrySet = unmodifiable.entrySet();
+
+    assertThat(entrySet.toString()).isEqualTo(delegate.entrySet().toString());
+  }
+
+  @Test
+  void valuesToStringEqualsDelegate() {
+    MultiValueMap<String, String> delegate = new MappingMultiValueMap<>();
+    delegate.add("key", "value");
+
+    UnmodifiableMultiValueMap<String, String> unmodifiable = new UnmodifiableMultiValueMap<>(delegate);
+    Collection<List<String>> values = unmodifiable.values();
+
+    assertThat(values.toString()).isEqualTo(delegate.values().toString());
+  }
+
   private static ThrowableTypeAssert<UnsupportedOperationException> assertThatUnsupportedOperationException() {
-    return Assertions.assertThatExceptionOfType(UnsupportedOperationException.class);
+    return assertThatExceptionOfType(UnsupportedOperationException.class);
   }
 
 }
