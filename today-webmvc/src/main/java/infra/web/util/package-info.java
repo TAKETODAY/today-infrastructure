7c7c2b1aa6d08fdde8ff954bf94be5dd790877d9
海子 Yang <i.take.today@gmail.com>
2026-01-01 11:38:45 +0800

    :art:

---
+++ today-webmvc/src/main/java/infra/web/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Miscellaneous web utility classes, such as HTML escaping,
+ * Log4j initialization, and cookie handling.
+ */
+@NullMarked
+package infra.web.util;
+
+import org.jspecify.annotations.NullMarked;
