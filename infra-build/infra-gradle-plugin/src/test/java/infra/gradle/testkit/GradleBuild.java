253101d91b022d89dd3d810939a833be8fbeca70
海子 Yang <i.take.today@gmail.com>
2026-01-08 14:48:03 +0800

    :white_check_mark:
    Signed-off-by: 海子 Yang <i.take.today@gmail.com>

--- infra-build/infra-gradle-plugin/src/test/java/infra/gradle/testkit/GradleBuild.java
+++ infra-build/infra-gradle-plugin/src/test/java/infra/gradle/testkit/GradleBuild.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2026 the original author or authors.
+ * Copyright 2017 - 2025 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -125,7 +125,7 @@ public class GradleBuild {
             new File(pathOfJarContaining(HttpClientConnectionManager.class)),
             new File(pathOfJarContaining(HttpRequest.class)),
             new File(pathOfJarContaining(HttpVersionPolicy.class)),
-            new File(pathOfJarContaining(Module.class)),
+//            new File(pathOfJarContaining(Module.class)),
             new File(pathOfJarContaining(Versioned.class)),
             new File(pathOfJarContaining(JsonView.class)),
             new File(pathOfJarContaining(Platform.class)),
