f3eac4db1ec5a6b5b4c0f981b0b9ccc42564a3b7
海子 Yang <i.take.today@gmail.com>
2026-01-15 17:04:49 +0800

    :white_check_mark:

--- infra-app/src/test/java/infra/app/logging/logback/InfraJoranConfiguratorTests.java
+++ infra-app/src/test/java/infra/app/logging/logback/InfraJoranConfiguratorTests.java
@@ -23,6 +23,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
@@ -31,8 +36,11 @@ import infra.app.logging.LoggingStartupContext;
 import infra.app.test.system.CapturedOutput;
 import infra.app.test.system.OutputCaptureExtension;
 import infra.beans.factory.aot.BeanFactoryInitializationAotContribution;
+import infra.context.aot.AbstractAotProcessor;
 import infra.context.properties.source.ConfigurationPropertySources;
 import infra.mock.env.MockEnvironment;
+import infra.test.classpath.ClassPathExclusions;
+import infra.test.classpath.resources.WithResource;
 import infra.test.context.support.TestPropertySourceUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -72,11 +80,12 @@ class InfraJoranConfiguratorTests {
   @AfterEach
   void reset() {
     this.context.stop();
-    this.context.reset();
     new BasicConfigurator().configure((LoggerContext) LoggerFactory.getILoggerFactory());
+    this.context.start();
   }
 
   @Test
+  @WithProductionProfileXmlResource
   void profileActive() throws Exception {
     this.environment.setActiveProfiles("production");
     initialize("production-profile.xml");
@@ -85,6 +94,20 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithResource(name = "profile-in-include.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<include file="${resourceRoot}/include-with-profile.xml"/>
+          </configuration>
+          """)
+  @WithResource(name = "include-with-profile.xml", content = """
+          <included>
+          	<infra-profile name="production">
+          		<logger name="infra.app.logging.logback" level="TRACE"/>
+          	</infra-profile>
+          </included>
+          """)
   void profileInIncludeActive() throws Exception {
     this.environment.setActiveProfiles("production");
     initialize("profile-in-include.xml");
@@ -93,6 +116,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithMultiProfileNamesXmlResource
   void multipleNamesFirstProfileActive() throws Exception {
     this.environment.setActiveProfiles("production");
     initialize("multi-profile-names.xml");
@@ -101,6 +125,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithMultiProfileNamesXmlResource
   void multipleNamesSecondProfileActive() throws Exception {
     this.environment.setActiveProfiles("test");
     initialize("multi-profile-names.xml");
@@ -109,6 +134,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithProductionProfileXmlResource
   void profileNotActive() throws Exception {
     initialize("production-profile.xml");
     this.logger.trace("Hello");
@@ -116,6 +142,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithProfileExpressionXmlResource
   void profileExpressionMatchFirst() throws Exception {
     this.environment.setActiveProfiles("production");
     initialize("profile-expression.xml");
@@ -124,6 +151,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithProfileExpressionXmlResource
   void profileExpressionMatchSecond() throws Exception {
     this.environment.setActiveProfiles("test");
     initialize("profile-expression.xml");
@@ -132,6 +160,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithProfileExpressionXmlResource
   void profileExpressionNoMatch() throws Exception {
     this.environment.setActiveProfiles("development");
     initialize("profile-expression.xml");
@@ -140,26 +169,31 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithNestedXmlResource
   void profileNestedActiveActive() throws Exception {
     doTestNestedProfile(true, "outer", "inner");
   }
 
   @Test
+  @WithNestedXmlResource
   void profileNestedActiveNotActive() throws Exception {
     doTestNestedProfile(false, "outer");
   }
 
   @Test
+  @WithNestedXmlResource
   void profileNestedNotActiveActive() throws Exception {
     doTestNestedProfile(false, "inner");
   }
 
   @Test
+  @WithNestedXmlResource
   void profileNestedNotActiveNotActive() throws Exception {
     doTestNestedProfile(false);
   }
 
   @Test
+  @WithPropertyXmlResource
   void infraProperty() throws Exception {
     TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.environment, "my.example-property=test");
     initialize("property.xml");
@@ -167,6 +201,7 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithPropertyXmlResource
   void relaxedInfraProperty() throws Exception {
     TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.environment, "my.EXAMPLE_PROPERTY=test");
     ConfigurationPropertySources.attach(this.environment);
@@ -175,46 +210,63 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithPropertyXmlResource
   void infraPropertyNoValue() throws Exception {
     initialize("property.xml");
     assertThat(this.context.getProperty("SIMPLE")).isNull();
   }
 
   @Test
+  @WithPropertyXmlResource
   void relaxedInfraPropertyNoValue() throws Exception {
     initialize("property.xml");
     assertThat(this.context.getProperty("MINE")).isNull();
   }
 
   @Test
+  @WithPropertyDefaultValueXmlResource
   void infraPropertyWithDefaultValue() throws Exception {
     initialize("property-default-value.xml");
     assertThat(this.context.getProperty("SIMPLE")).isEqualTo("foo");
   }
 
   @Test
+  @WithPropertyDefaultValueXmlResource
   void relaxedInfraPropertyWithDefaultValue() throws Exception {
     initialize("property-default-value.xml");
     assertThat(this.context.getProperty("MINE")).isEqualTo("bar");
   }
 
   @Test
+  @WithPropertyInIfXmlResource
   void infraPropertyInIfWhenTrue() throws Exception {
-    TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-            environment, "my.example-property=true");
+    TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.environment, "my.example-property=true");
     initialize("property-in-if.xml");
     assertThat(this.context.getProperty("MYCHECK")).isEqualTo("i-was-included");
   }
 
   @Test
+  @WithPropertyInIfXmlResource
   void infraPropertyInIfWhenFalse() throws Exception {
-    TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-            environment, "my.example-property=false");
+    TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.environment, "my.example-property=false");
     initialize("property-in-if.xml");
     assertThat(this.context.getProperty("MYCHECK")).isNull();
   }
 
   @Test
+  @WithResource(name = "property-in-include.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<include file="${resourceRoot}/include-with-property.xml"/>
+          </configuration>
+          """)
+  @WithResource(name = "include-with-property.xml", content = """
+          <included>
+          	<infra-property scope="context" name="MINE" source="my.example-property" defaultValue="default-test"/>
+          </included>
+          """)
+  @ClassPathExclusions
   void infraPropertyInInclude() throws Exception {
     TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.environment, "my.example-property=test");
     initialize("property-in-include.xml");
@@ -222,8 +274,9 @@ class InfraJoranConfiguratorTests {
   }
 
   @Test
+  @WithPropertyXmlResource
   void addsAotContributionToContextDuringAotProcessing() throws Exception {
-    withSystemProperty("infra.aot.processing", "true", () -> {
+    withSystemProperty(AbstractAotProcessor.AOT_PROCESSING, "true", () -> {
       initialize("property.xml");
       Object contribution = this.context.getObject(BeanFactoryInitializationAotContribution.class.getName());
       assertThat(contribution).isNotNull();
@@ -255,12 +308,121 @@ class InfraJoranConfiguratorTests {
 
   private void initialize(String config) throws JoranException {
     this.configurator.setContext(this.context);
-    this.configurator.doConfigure(getClass().getResourceAsStream(config));
+    ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+    this.configurator.doConfigure(contextClassLoader.getResource(config));
   }
 
   private interface Action {
 
     void perform() throws Exception;
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "property-default-value.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-property scope="context" name="SIMPLE" source="testpropertyfoobar" defaultValue="foo"/>
+          	<infra-property scope="context" name="MINE" source="my.example-property" defaultValue="bar"/>
+          </configuration>
+          """)
+  private @interface WithPropertyDefaultValueXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "property-in-if.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-property scope="context" name="MINE" source="my.example-property"/>
+          	<if condition='property("MINE").contains("true")'>
+          		<then>
+          			<variable scope="context" name="MYCHECK" value="i-was-included"/>
+          		</then>
+          	</if>
+          </configuration>
+          """)
+  private @interface WithPropertyInIfXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "property.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-property scope="context" name="SIMPLE" source="testpropertyfoobar"/>
+          	<infra-property scope="context" name="MINE" source="my.example-property"/>
+          </configuration>
+          """)
+  private @interface WithPropertyXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "profile-expression.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-profile name="production | test">
+          		<logger name="infra.app.logging.logback" level="TRACE"/>
+          	</infra-profile>
+          </configuration>
+          """)
+  private @interface WithProfileExpressionXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "production-profile.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-profile name="production">
+          		<logger name="infra.app.logging.logback" level="TRACE"/>
+          	</infra-profile>
+          </configuration>
+          """)
+  private @interface WithProductionProfileXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "nested.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-profile name="outer">
+          		<infra-profile name="inner">
+          			<logger name="infra.app.logging.logback" level="TRACE"/>
+          		</infra-profile>
+          	</infra-profile>
+          </configuration>
+          """)
+  private @interface WithNestedXmlResource {
+
+  }
+
+  @Target(ElementType.METHOD)
+  @Retention(RetentionPolicy.RUNTIME)
+  @WithResource(name = "multi-profile-names.xml", content = """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <configuration>
+          	<include resource="infra/app/logging/logback/base.xml"/>
+          	<infra-profile name="production, test">
+          		<logger name="infra.app.logging.logback" level="TRACE"/>
+          	</infra-profile>
+          </configuration>
+          """)
+  private @interface WithMultiProfileNamesXmlResource {
 
   }
 
