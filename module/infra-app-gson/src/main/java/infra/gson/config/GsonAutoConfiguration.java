339e420ad6dcc25a18593b9546c427687ac3e298
海子 Yang <i.take.today@gmail.com>
2026-01-18 13:14:17 +0800

    :recycle: 重构 PropertyMapper API 实现并移除 alwaysApplyingWhenNonNull 方法

--- module/infra-app-gson/src/main/java/infra/gson/config/GsonAutoConfiguration.java
+++ module/infra-app-gson/src/main/java/infra/gson/config/GsonAutoConfiguration.java
@@ -83,7 +83,7 @@ public final class GsonAutoConfiguration {
     @SuppressWarnings("NullAway")
     public void customize(GsonBuilder builder) {
       GsonProperties properties = this.properties;
-      PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+      PropertyMapper map = PropertyMapper.get();
       map.from(properties::getGenerateNonExecutableJson).whenTrue().toCall(builder::generateNonExecutableJson);
       map.from(properties::getExcludeFieldsWithoutExposeAnnotation)
               .whenTrue()
