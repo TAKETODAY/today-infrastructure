28d33d0d44b7c577655ae501bfc0ac907a002023
海子 Yang <i.take.today@gmail.com>
2026-01-21 22:22:28 +0800

    :recycle: jackson 模块

---
+++ module/infra-jackson/src/main/java/infra/jackson/config/CborMapperBuilderCustomizer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Modifications Copyright 2017 - 2026 the TODAY authors.
+
+package infra.jackson.config;
+
+import tools.jackson.dataformat.cbor.CBORMapper;
+import tools.jackson.dataformat.cbor.CBORMapper.Builder;
+
+/**
+ * Callback interface that can be implemented by beans wishing to further customize the
+ * {@link CBORMapper} through {@link Builder CBORMapper.Builder} to fine-tune its
+ * auto-configuration.
+ *
+ * @author Andy Wilkinson
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0
+ */
+@FunctionalInterface
+public interface CborMapperBuilderCustomizer {
+
+  /**
+   * Customize the CBORMapper.Builder.
+   *
+   * @param cborMapperBuilder the builder to customize
+   */
+  void customize(CBORMapper.Builder cborMapperBuilder);
+
+}
---
+++ module/infra-jackson/src/main/java/infra/jackson/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Custom enhancements and support for the Jackson project.
+ */
+@NullMarked
+package infra.jackson;
+
+import org.jspecify.annotations.NullMarked;
