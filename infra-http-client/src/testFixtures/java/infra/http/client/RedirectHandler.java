2a0dde4369a2be72c8a9c5f2a7e315911fea6c95
海子 Yang <i.take.today@gmail.com>
2026-01-26 21:32:37 +0800

    :art:

--- infra-http-client/src/testFixtures/java/infra/http/client/RedirectHandler.java
+++ infra-http-client/src/testFixtures/java/infra/http/client/RedirectHandler.java
@@ -24,7 +24,6 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
