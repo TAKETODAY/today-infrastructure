6ad8c2273aa255756de01ab8c9431cef1e835e16
海子 Yang <i.take.today@gmail.com>
2026-03-01 00:23:55 +0800

    :art:

---
+++ infra-webmvc/src/main/java/infra/web/view/config/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Configuration package for web view components.
+ * This package contains classes and configurations related to web view setup and management.
+ */
+@NullMarked
+package infra.web.view.config;
+
+import org.jspecify.annotations.NullMarked;
