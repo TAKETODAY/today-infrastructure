3c89ee720921973e6a3318983170d0fea8516721
海子 Yang <i.take.today@gmail.com>
2026-04-23 22:16:35 +0800

    :sparkles: Add infrastructure for pluggable connection details factories

---
+++ infra-app/src/main/java/infra/app/service/connection/ConnectionDetailsFactoryNotFoundException.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Modifications Copyright 2017 - 2026 the TODAY authors.
+
+package infra.app.service.connection;
+
+/**
+ * {@link RuntimeException} thrown when a {@link ConnectionDetailsFactory} could not be
+ * found.
+ *
+ * @author Moritz Halbritter
+ * @author Andy Wilkinson
+ * @author Phillip Webb
+ * @since 5.0
+ */
+public class ConnectionDetailsFactoryNotFoundException extends RuntimeException {
+
+  <S> ConnectionDetailsFactoryNotFoundException(S source) {
+    this("No ConnectionDetailsFactory found for source '%s'".formatted(source));
+  }
+
+  public ConnectionDetailsFactoryNotFoundException(String message) {
+    super(message);
+  }
+
+  public ConnectionDetailsFactoryNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
---
+++ infra-app/src/main/java/infra/app/service/connection/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Support for service connections that affect auto-configuration.
+ */
+@NullMarked
+package infra.app.service.connection;
+
+import org.jspecify.annotations.NullMarked;
