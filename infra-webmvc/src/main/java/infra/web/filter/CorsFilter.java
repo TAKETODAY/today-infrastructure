9d959232410535cb6dc376813153c24eb1408271
海子 Yang <i.take.today@gmail.com>
2026-06-01 13:22:04 +0800

    :sparkles: CorsFilter

---
+++ infra-webmvc/src/main/java/infra/web/filter/CorsFilter.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2002-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package infra.web.filter;
+
+import infra.lang.Assert;
+import infra.web.FilterChain;
+import infra.web.RequestContext;
+import infra.web.cors.CorsConfiguration;
+import infra.web.cors.CorsConfigurationSource;
+import infra.web.cors.CorsProcessor;
+import infra.web.cors.DefaultCorsProcessor;
+import infra.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * {@link infra.web.Filter} to handle CORS pre-flight requests and intercept
+ * CORS simple and actual requests with a {@link CorsProcessor}, and to update
+ * the response, for example, with CORS response headers, based on the policy matched
+ * through the provided {@link CorsConfigurationSource}.
+ *
+ * <p>This is an alternative to configuring CORS in the Infra MVC Java config
+ * and the Infra MVC XML namespace. It is useful for applications depending
+ * only on infra-web (not on infra-webmvc) or for security constraints that
+ * require CORS checks to be performed at {@link infra.web.Filter} level.
+ *
+ * <p>This filter could be used in conjunction with {@link DelegatingFilterProxy}
+ * in order to help with its initialization.
+ *
+ * @author Sebastien Deleuze
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @see <a href="https://www.w3.org/TR/cors/">CORS W3C recommendation</a>
+ * @see UrlBasedCorsConfigurationSource
+ * @since 5.0
+ */
+public class CorsFilter extends GenericFilterBean {
+
+  private final CorsConfigurationSource configSource;
+
+  private CorsProcessor processor = new DefaultCorsProcessor();
+
+  /**
+   * Constructor accepting a {@link CorsConfigurationSource} used by the filter
+   * to find the {@link CorsConfiguration} to use for each incoming request.
+   *
+   * @see UrlBasedCorsConfigurationSource
+   */
+  public CorsFilter(CorsConfigurationSource configSource) {
+    Assert.notNull(configSource, "CorsConfigurationSource is required");
+    this.configSource = configSource;
+  }
+
+  /**
+   * Configure a custom {@link CorsProcessor} to use to apply the matched
+   * {@link CorsConfiguration} for a request.
+   * <p>By default {@link DefaultCorsProcessor} is used.
+   */
+  public void setCorsProcessor(CorsProcessor processor) {
+    Assert.notNull(processor, "CorsProcessor is required");
+    this.processor = processor;
+  }
+
+  @Override
+  public void doFilter(RequestContext request, FilterChain chain) throws Throwable {
+    CorsConfiguration corsConfiguration = this.configSource.getCorsConfiguration(request);
+    boolean isValid = this.processor.process(corsConfiguration, request);
+    if (!isValid || request.isPreFlightRequest()) {
+      return;
+    }
+    chain.doFilter(request);
+  }
+
+}
---
+++ infra-webmvc/src/test/java/infra/web/filter/CorsFilterTests.java
@@ -0,0 +1,140 @@
+package infra.web.filter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import infra.http.HttpHeaders;
+import infra.http.HttpMethod;
+import infra.mock.web.HttpMockRequestImpl;
+import infra.mock.web.MockHttpResponseImpl;
+import infra.web.FilterChain;
+import infra.web.cors.CorsConfiguration;
+import infra.web.cors.DefaultCorsProcessor;
+import infra.web.cors.UrlBasedCorsConfigurationSource;
+import infra.web.mock.MockRequestContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0 2026/6/1 12:40
+ */
+class CorsFilterTests {
+
+  private CorsFilter filter;
+
+  private final CorsConfiguration config = new CorsConfiguration();
+
+  @BeforeEach
+  void setup() {
+    config.setAllowedOrigins(Arrays.asList("https://domain1.com", "https://domain2.com"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST"));
+    config.setAllowedHeaders(Arrays.asList("header1", "header2"));
+    config.setExposedHeaders(Arrays.asList("header3", "header4"));
+    config.setMaxAge(123L);
+    config.setAllowCredentials(false);
+
+    UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+    configSource.registerCorsConfiguration("/**", config);
+
+    filter = new CorsFilter(configSource);
+    filter.setCorsProcessor(new DefaultCorsProcessor());
+  }
+
+  @Test
+  void nonCorsRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.GET.name(), "/test.html");
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) -> {
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isNull();
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS)).isNull();
+    };
+    filter.doFilter(new MockRequestContext(request), filterChain);
+  }
+
+  @Test
+  void sameOriginRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.GET.name(), "https://domain1.com/test.html");
+    request.addHeader(HttpHeaders.ORIGIN, "https://domain1.com");
+    request.setScheme("https");
+    request.setServerName("domain1.com");
+    request.setServerPort(443);
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) -> {
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isNull();
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS)).isNull();
+    };
+    filter.doFilter(new MockRequestContext(request), filterChain);
+  }
+
+  @Test
+  void validActualRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.GET.name(), "/test.html");
+    request.addHeader(HttpHeaders.ORIGIN, "https://domain2.com");
+    request.addHeader("header2", "foo");
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) -> {
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isEqualTo("https://domain2.com");
+      assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS)).isEqualTo("header3, header4");
+    };
+    filter.doFilter(new MockRequestContext(request, response), filterChain);
+  }
+
+  @Test
+  void invalidActualRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.DELETE.name(), "/test.html");
+    request.addHeader(HttpHeaders.ORIGIN, "https://domain2.com");
+    request.addHeader("header2", "foo");
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) ->
+            fail("Invalid requests must not be forwarded to the filter chain");
+    filter.doFilter(new MockRequestContext(request), filterChain);
+    assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isNull();
+  }
+
+  @Test
+  void validPreFlightRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.OPTIONS.name(), "/test.html");
+    request.addHeader(HttpHeaders.ORIGIN, "https://domain2.com");
+    request.addHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.GET.name());
+    request.addHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "header1, header2");
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) ->
+            fail("Preflight requests must not be forwarded to the filter chain");
+    filter.doFilter(new MockRequestContext(request, response), filterChain);
+    assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isEqualTo("https://domain2.com");
+    assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS)).isEqualTo("header1, header2");
+    assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS)).isEqualTo("header3, header4");
+    assertThat(Long.parseLong(response.getHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE))).isEqualTo(123L);
+  }
+
+  @Test
+  void invalidPreFlightRequest() throws Throwable {
+
+    HttpMockRequestImpl request = new HttpMockRequestImpl(HttpMethod.OPTIONS.name(), "/test.html");
+    request.addHeader(HttpHeaders.ORIGIN, "https://domain2.com");
+    request.addHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.DELETE.name());
+    request.addHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "header1, header2");
+    MockHttpResponseImpl response = new MockHttpResponseImpl();
+
+    FilterChain filterChain = (filterRequest) ->
+            fail("Preflight requests must not be forwarded to the filter chain");
+    filter.doFilter(new MockRequestContext(request), filterChain);
+
+    assertThat(response.getHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)).isNull();
+  }
+
+}
