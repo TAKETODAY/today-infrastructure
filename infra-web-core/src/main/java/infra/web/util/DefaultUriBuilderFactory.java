e1c425142111855531a72f9159dcf0653055596f
海子 Yang <i.take.today@gmail.com>
2026-07-17 09:09:22 +0800

    :recycle: Refactor map initialization using CollectionUtils

--- infra-web-core/src/main/java/infra/web/util/DefaultUriBuilderFactory.java
+++ infra-web-core/src/main/java/infra/web/util/DefaultUriBuilderFactory.java
@@ -441,7 +441,7 @@ public class DefaultUriBuilderFactory implements UriBuilderFactory {
     @Override
     public URI build(Map<String, ?> uriVars) {
       if (CollectionUtils.isNotEmpty(defaultUriVariables)) {
-        Map<String, Object> map = new HashMap<>(defaultUriVariables.size() + uriVars.size());
+        Map<String, Object> map = CollectionUtils.newHashMap(defaultUriVariables.size() + uriVars.size());
         map.putAll(defaultUriVariables);
         map.putAll(uriVars);
         uriVars = map;
