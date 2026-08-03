5f30d261cbaa706cc2f6c90aeb185bb3ddc36506
海子 Yang <i.take.today@gmail.com>
2026-08-03 17:43:37 +0800

    :recycle: PreFlightRequestFilter

---
+++ infra-webmvc/src/main/java/infra/web/filter/PreFlightRequestFilter.java
@@ -0,0 +1,37 @@
+package infra.web.filter;
+
+import infra.lang.Assert;
+import infra.web.Filter;
+import infra.web.FilterChain;
+import infra.web.HttpContext;
+import infra.web.cors.PreFlightRequestHandler;
+
+/**
+ * Filter that handles pre-flight requests through a
+ * {@link PreFlightRequestHandler} and bypasses the rest of the chain.
+ *
+ * <p>The {@code @EnableWebMvc} config declares a bean of type
+ * {@code PreFlightRequestHandler}.
+ *
+ * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
+ * @since 5.0 2026/5/26 08:45
+ */
+public class PreFlightRequestFilter implements Filter {
+
+  private final PreFlightRequestHandler handler;
+
+  public PreFlightRequestFilter(PreFlightRequestHandler handler) {
+    Assert.notNull(handler, "PreFlightRequestHandler is required");
+    this.handler = handler;
+  }
+
+  @Override
+  public void doFilter(HttpContext http, FilterChain chain) throws Exception {
+    if (http.isPreFlightRequest()) {
+      handler.handlePreFlight(http);
+    }
+    else {
+      chain.doFilter(http);
+    }
+  }
+}
