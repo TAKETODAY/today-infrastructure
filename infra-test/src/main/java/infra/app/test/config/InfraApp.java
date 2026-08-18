66d4c36636ce8220f1a6b4cff9d255ba60f24f59
海子 Yang <i.take.today@gmail.com>
2026-08-18 22:41:26 +0800

    :art:

--- infra-test/src/main/java/infra/app/test/config/InfraApp.java
+++ infra-test/src/main/java/infra/app/test/config/InfraApp.java
@@ -22,7 +22,7 @@ import infra.util.ClassUtils;
  * @author <a href="https://github.com/TAKETODAY">海子 Yang</a>
  * @since 1.0 2026/8/18 10:00
  */
-public class InfraApp {
+public abstract class InfraApp {
 
   public static final boolean isPresent = ClassUtils.isPresent("infra.app.Application");
 
