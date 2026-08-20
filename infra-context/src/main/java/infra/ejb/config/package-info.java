fc899862eeed5792ac9487b839ef3deed420f5c2
海子 Yang <i.take.today@gmail.com>
2026-08-20 12:25:53 +0800

    :fire: 删除 EJB/JEE 命名空间支持（infra.ejb）
    - 删除 infra.ejb 命名空间包及其 XSD/Tooling 注册
    - 删除 JeeNamespaceHandler 及 EJB/JNDI 解析器
    - 清理 spring.handlers / spring.schemas / spring.tooling 中的 jee 注册
    - 删除相关测试

--- infra-context/src/main/java/infra/ejb/config/package-info.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Support package for EJB/Jakarta EE-related configuration,
- * with XML schema being the primary configuration format.
- */
-@NullMarked
-package infra.ejb.config;
-
-import org.jspecify.annotations.NullMarked;
